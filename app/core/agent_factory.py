2620d11255cd4fdc3f6d858ecd82f8c03b9db74f
Michael Wiles <michaelwiles@afrozaar.com>
2021-06-04 16:46:25 +0200

    #9 initial commit on some refactorings for reference architecture

---
+++ app/core/agent_factory.py
@@ -0,0 +1,19 @@
+from distutils.util import strtobool
+
+import os
+
+import aries_cloudcontroller
+
+admin_url = os.getenv("ACAPY_ADMIN_URL")
+admin_port = os.getenv("ACAPY_ADMIN_PORT")
+admin_api_key = os.getenv("ACAPY_ADMIN_API_KEY")
+is_multitenant = strtobool(os.getenv("IS_MULTITENANT", "False"))
+ledger_url = os.getenv("LEDGER_NETWORK_URL")
+
+
+def create_aries_agentcontroller():
+    return aries_cloudcontroller.AriesAgentController(
+            admin_url=f"{admin_url}:{admin_port}",
+            api_key=admin_api_key,
+            is_multitenant=is_multitenant,
+    )
--- app/routers/wallet.py
+++ app/routers/wallet.py
@@ -4,7 +4,7 @@ import json
 import os
 import logging
 
-
+from core import wallet
 from schemas import LedgerRequest, DidCreationResponse, InitWalletRequest
 
 import aries_cloudcontroller
@@ -25,117 +25,7 @@ ledger_url = os.getenv("LEDGER_NETWORK_URL")
     "/create-pub-did", tags=["did"], response_model=DidCreationResponse
 )
 async def create_public_did():
-    """
-    Create a new public DID and
-    write it to the ledger and
-    receive its public info.
-
-    Returns:
-    * DID object (json)
-    * Issuer verkey (str)
-    * Issuer Endpoint (url)
-    """
-    # TODO Can we break down this endpoint into smaller functions?
-    # Because this really is too complex/too much happening at once.
-    # This way not really testible/robust
-    try:
-        aries_agent_controller = aries_cloudcontroller.AriesAgentController(
-            admin_url=f"{admin_url}:{admin_port}",
-            api_key=admin_api_key,
-            is_multitenant=is_multitenant,
-        )
-        # TODO: Should this come from env var or from the client request?
-        url = ledger_url
-        # Adding empty header as parameters are being sent in payload
-        generate_did_res = await aries_agent_controller.wallet.create_did()
-        if not generate_did_res["result"]:
-            raise HTTPException(
-                # TODO: Should this return HTTPException, if so which status code?
-                # Check same for occurences below
-                status_code=404,
-                detail=f"Something went wrong.\nCould not generate DID.\n{generate_did_res}",
-            )
-        did_object = generate_did_res["result"]
-        # TODO: Network and paymentaddr should be definable on the fly/via args/via request body
-        # TODO: Should this really be a schema or is using schema overkill here?
-        # If we leave it as schema like this I suppose it is at least usable elsewhere
-        payload = LedgerRequest(
-            network="stagingnet",
-            did=did_object["did"],
-            verkey=did_object["verkey"],
-            paymentaddr="",
-        ).dict()
-        r = requests.post(url, data=json.dumps(payload), headers={})
-        if r.status_code != 200:
-            error_json = r.json()
-            raise HTTPException(
-                status_code=r.status_code,
-                detail=f"Something went wrong.\nCould not write to StagingNet.\n{error_json}",
-            )
-        taa_response = await aries_agent_controller.ledger.get_taa()
-        logger.info(f"taa_response:\n{taa_response}")
-        if not taa_response["result"]:
-            error_json = taa_response.json()
-            raise HTTPException(
-                status_code=404,
-                detail=f"Something went wrong. Could not get TAA. {error_json}",
-            )
-        TAA = taa_response["result"]["taa_record"]
-        TAA["mechanism"] = "service_agreement"
-        accept_taa_response = await aries_agent_controller.ledger.accept_taa(TAA)
-        logger.info(f"accept_taa_response: {accept_taa_response}")
-        if accept_taa_response != {}:
-            error_json = accept_taa_response.json()
-            raise HTTPException(
-                status_code=404,
-                detail=f"Something went wrong. Could not accept TAA. {error_json}",
-            )
-        assign_pub_did_response = await aries_agent_controller.wallet.assign_public_did(
-            did_object["did"]
-        )
-        logger.info(f"assign_pub_did_response:\n{assign_pub_did_response}")
-        if (
-            not assign_pub_did_response["result"]
-            or assign_pub_did_response["result"] == {}
-        ):
-            error_json = assign_pub_did_response.json()
-            raise HTTPException(
-                status_code=500,
-                detail=f"Something went wrong.\nCould not assign DID. {error_json}",
-            )
-        get_pub_did_response = await aries_agent_controller.wallet.get_public_did()
-        logger.info(f"get_pub_did_response:\n{get_pub_did_response}")
-        if not get_pub_did_response["result"] or get_pub_did_response["result"] == {}:
-            error_json = get_pub_did_response.json()
-            raise HTTPException(
-                status_code=404,
-                detail=f"Something went wrong. Could not obtain public DID. {error_json}",
-            )
-        issuer_nym = get_pub_did_response["result"]["did"]
-        issuer_verkey = get_pub_did_response["result"]["verkey"]
-        issuer_endpoint = await aries_agent_controller.ledger.get_did_endpoint(
-            issuer_nym
-        )
-        if not issuer_endpoint:
-            raise HTTPException(
-                status_code=404,
-                detail="Something went wrong. Could not obtain issuer endpoint.",
-            )
-        issuer_endpoint_url = issuer_endpoint["endpoint"]
-        final_response = DidCreationResponse(
-            did_object=did_object,
-            issuer_verkey=issuer_verkey,
-            issuer_endpoint=issuer_endpoint_url,
-        )
-        await aries_agent_controller.terminate()
-        return final_response
-    except Exception as e:
-        await aries_agent_controller.terminate()
-        logger.error(f"The following error occured:\n{e!r}")
-        raise HTTPException(
-            status_code=500,
-            detail=f"Something went wrong: {e!r}",
-        )
+    return wallet.create_public_did()
 
 
 @router.get("/")

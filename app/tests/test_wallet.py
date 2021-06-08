8efcfcb4e5858890d4f8c2feb35d3fdd3c1babfc
Michael Wiles <michaelwiles@afrozaar.com>
2021-06-08 10:22:14 +0200

    adding code to extract the ip address of the required container

--- app/tests/test_wallet.py
+++ app/tests/test_wallet.py
@@ -1,3 +1,4 @@
+import docker
 import pytest
 
 import routers.governance
@@ -10,11 +11,27 @@ def test_foo():
 
 @pytest.mark.asyncio
 async def test_create_public_did():
-    agent_factory.admin_url = 'http://0.0.0.0'
-    agent_factory.admin_port = '3021'
-    agent_factory.admin_api_key = 'adminApiKey'
-    agent_factory.is_multitenant = False
+    setup_env()
 
     delegates.ledger_url = "https://selfserve.sovrin.org/nym"
     result = await wallet.create_public_did()
     print(result)
+
+
+def setup_env():
+    client = docker.from_env()
+    containers_list = client.containers.list()
+    print(containers_list)
+
+    container = next(iter(c for c in containers_list if 'aries-cloudapi-python_yoma-ga-agent:latest' in c.image.tags), None)
+
+    if container:
+        network = next(iter(container.attrs['NetworkSettings']['Networks'].values()), None)
+        ip_address=network['IPAddress']
+        agent_factory.admin_url = 'http://'+ip_address
+    else:
+        agent_factory.admin_url = 'http://localhost'
+    print(f'admin url  = {agent_factory.admin_url}')
+    agent_factory.admin_port = '3021'
+    agent_factory.admin_api_key = 'adminApiKey'
+    agent_factory.is_multitenant = False

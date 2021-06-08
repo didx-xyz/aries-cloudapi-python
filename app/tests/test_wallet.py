6fafed2c46f5f28bc9b0f3527938ad8a243ea27f
Michael Wiles <michaelwiles@afrozaar.com>
2021-06-08 11:14:41 +0200

    switched to using localhost (to check if it works on github actions)

--- app/tests/test_wallet.py
+++ app/tests/test_wallet.py
@@ -15,7 +15,7 @@ async def test_create_public_did():
 
     delegates.ledger_url = "https://selfserve.sovrin.org/nym"
     result = await wallet.create_public_did()
-    print(result)
+    # print(result)
 
 
 def setup_env():
@@ -23,15 +23,20 @@ def setup_env():
     containers_list = client.containers.list()
     print(containers_list)
 
-    container = next(iter(c for c in containers_list if 'aries-cloudapi-python_yoma-ga-agent:latest' in c.image.tags), None)
+    container = next(iter(c for c in containers_list if 'aries-cloudapi-python_yoma-ga-agent:latest' in c.image.tags),
+                     None)
 
-    if container:
-        network = next(iter(container.attrs['NetworkSettings']['Networks'].values()), None)
-        ip_address=network['IPAddress']
-        agent_factory.admin_url = 'http://'+ip_address
-    else:
-        agent_factory.admin_url = 'http://localhost'
+    # if container:
+    #     network = next(iter(container.attrs['NetworkSettings']['Networks'].values()), None)
+    #     ip_address = network['IPAddress']
+    #     print('ip address = ' + ip_address)
+    #     agent_factory.admin_url = 'http://' + ip_address
+    # else:
+    #     agent_factory.admin_url = 'http://localhost'
+    agent_factory.admin_url = 'http://localhost'
     print(f'admin url  = {agent_factory.admin_url}')
+
+
     agent_factory.admin_port = '3021'
     agent_factory.admin_api_key = 'adminApiKey'
     agent_factory.is_multitenant = False

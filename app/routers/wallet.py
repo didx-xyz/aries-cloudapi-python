3d845b3dbcef272ed3da5714070f3387a075f772
morrieinmaas <motechnix@gmail.com>
2021-06-15 19:17:36 +0000

    Revert "Merge pull request #19 from didx-xyz/feature/9-reference-arcthitecture"
    This reverts commit 88bcec0017ce564277b917ccc8c81a1ade2af85e, reversing
    changes made to e257778ff4a73a8cf88536d4c4f3eaf55058c5ee.

--- app/routers/wallet.py
+++ app/routers/wallet.py
@@ -1,17 +1,12 @@
+import logging
+import os
+import traceback
 from typing import Optional
 
-from fastapi import APIRouter, HTTPException, Header
-import requests
-import json
-import os
-import logging
-
-from core import wallet
-from facade import create_controller
-from schemas import LedgerRequest, DidCreationResponse, InitWalletRequest
-
-import aries_cloudcontroller
-
+from facade import (accept_taa, assign_pub_did, create_controller, create_did,
+                    get_did_endpoint, get_pub_did, get_taa, post_to_ledger)
+from fastapi import APIRouter, Header, HTTPException
+from schemas import DidCreationResponse, InitWalletRequest, LedgerRequest
 
 logger = logging.getLogger(__name__)
 
@@ -19,26 +14,86 @@ router = APIRouter(prefix="/wallets", tags=["wallets"])
 
 admin_url = os.getenv("ACAPY_ADMIN_URL")
 admin_port = os.getenv("ACAPY_ADMIN_PORT")
+# TODO Should the admin_api_key be a dummy variable so the controller doesn't function w/o providing it?
+# This all smells really - this has to be done in a better manner
 admin_api_key = os.getenv("ACAPY_ADMIN_API_KEY")
 is_multitenant = os.getenv("IS_MULTITENANT", False)
 ledger_url = os.getenv("LEDGER_NETWORK_URL")
 
 
-@router.get(
-    "/create-pub-did", tags=["did"], response_model=DidCreationResponse
-)
+@router.get("/create-pub-did", tags=["did"], response_model=DidCreationResponse)
 async def create_public_did(req_header: Optional[str] = Header(None)):
-    return wallet.create_public_did(req_header)
+    """
+    Create a new public DID and
+    write it to the ledger and
+    receive its public info.
+
+    Parameters:
+    -----------
+    req_header: Header
+        The request header object with (tenant_jwt, wallet_id) or api_key
+
+    Returns:
+    * DID object (json)
+    * Issuer verkey (str)
+    * Issuer Endpoint (url)
+    """
+    try:
+        async with create_controller(req_header) as controller:
+            # TODO: Should this come from env var or from the client request?
+            if "ledger_url" in req_header:
+                url = req_header["ledger_url"]
+            else:
+                url = ledger_url
+            # Adding empty header as parameters are being sent in payload
+            generate_did_res = await create_did(controller)
+            did_object = generate_did_res["result"]
+            # TODO: Network and paymentaddr should be definable on the fly/via args/via request body
+            # TODO: Should this really be a schema or is using schema overkill here?
+            # If we leave it as schema like this I suppose it is at least usable elsewhere
+            payload = LedgerRequest(
+                network="stagingnet",
+                did=did_object["did"],
+                verkey=did_object["verkey"],
+                paymentaddr="",
+            )
+
+            await post_to_ledger(url, payload)
+
+            TAA = await get_taa(controller)
+
+            await accept_taa(controller, TAA)
+
+            await assign_pub_did(controller, did_object)
+
+            get_pub_did_response = await get_pub_did(controller)
+            issuer_nym = get_pub_did_response["result"]["did"]
+            issuer_verkey = get_pub_did_response["result"]["verkey"]
+
+            issuer_endpoint = await get_did_endpoint(controller, issuer_nym)
+            issuer_endpoint_url = issuer_endpoint["endpoint"]
+
+            final_response = DidCreationResponse(
+                did_object=did_object,
+                issuer_verkey=issuer_verkey,
+                issuer_endpoint=issuer_endpoint_url,
+            )
+            return final_response
+    except Exception as e:
+        err_trace = traceback.print_exc()
+        logger.error(
+            f"Failed to create public DID. The following error occured:\n{e!r}\n{err_trace}"
+        )
+        raise e
 
 
 @router.get("/")
 async def wallets_root():
     """
     The default endpoints for wallets
-
-    TODO: Determine what this should return or
-    whether this should return anything at all
     """
+    # TODO: Determine what this should return or
+    # whether this should return anything at all
     return {
         "message": "Wallets endpoint. Please, visit /docs to consult the Swagger docs."
     }
--- app/tests/test_wallet.py
+++ app/tests/test_wallet.py
@@ -1,35 +1,9 @@
-import facade
-import ledger_facade
-import pytest
-from core import wallet
-from fastapi import HTTPException
+import routers.governance
 
 
-@pytest.fixture
-def setup_env():
-    facade.admin_url = "http://localhost"
-    facade.admin_port = "3021"
-    facade.is_multitenant = False
-    ledger_facade.ledger_url = "https://selfserve.sovrin.org/nym"
+def test_foo():
+    pass
 
 
-@pytest.mark.asyncio
-async def test_create_public_did(setup_env):
-    result = await wallet.create_public_did("{'api_key':'adminApiKey'}")
-
-    # TODO: validate in a more robust manner
-    assert result.did_object and result.did_object != {}
-    assert result.issuer_verkey and result.issuer_verkey != {}
-    assert result.issuer_endpoint and result.issuer_endpoint != {}
-
-
-@pytest.mark.asyncio
-async def test_create_public_did_no_api_key(setup_env):
-
-    with pytest.raises(HTTPException) as exc:
-        await wallet.create_public_did("")
-    assert exc.value.status_code == 400
-    assert (
-        exc.value.detail
-        == "Bad headers. Either provide an api_key or both wallet_id and tenant_jwt"
-    )
+def test_bar():
+    pass

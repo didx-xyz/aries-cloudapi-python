d3c20c165fb5af4f610466845cf0b26c52240efe
morrieinmaas <motechnix@gmail.com>
2021-06-15 15:15:25 +0000

    Improve unit tests:
    * Remove redundant new dependency assertpy
    * Use pute pytest instead
    * Use pytest fixture for test setup
    * test for actual HTTException and its expected value

--- app/tests/test_wallet.py
+++ app/tests/test_wallet.py
@@ -1,37 +1,35 @@
-
-import pytest
-from assertpy import assert_that
-from fastapi import HTTPException
-
 import facade
 import ledger_facade
+import pytest
 from core import wallet
+from fastapi import HTTPException
+
+
+@pytest.fixture
+def setup_env():
+    facade.admin_url = "http://localhost"
+    facade.admin_port = "3021"
+    facade.is_multitenant = False
+    ledger_facade.ledger_url = "https://selfserve.sovrin.org/nym"
 
 
 @pytest.mark.asyncio
-async def test_create_public_did():
-    setup_env()
-
+async def test_create_public_did(setup_env):
     result = await wallet.create_public_did("{'api_key':'adminApiKey'}")
 
     # TODO: validate in a more robust manner
-    assert_that(result.did_object).is_not_empty()
-    assert_that(result.issuer_verkey).is_not_empty()
-    assert_that(result.issuer_endpoint).is_not_empty()
+    assert result.did_object and result.did_object != {}
+    assert result.issuer_verkey and result.issuer_verkey != {}
+    assert result.issuer_endpoint and result.issuer_endpoint != {}
 
 
 @pytest.mark.asyncio
-async def test_create_public_did_no_api_key():
-    setup_env()
+async def test_create_public_did_no_api_key(setup_env):
 
-    async def wrap():
-        return await wallet.create_public_did("")
-    assert_that(wrap).raises(HTTPException)
-
-
-
-def setup_env():
-    facade.admin_url = 'http://localhost'
-    facade.admin_port = '3021'
-    facade.is_multitenant = False
-    ledger_facade.ledger_url = "https://selfserve.sovrin.org/nym"
+    with pytest.raises(HTTPException) as exc:
+        await wallet.create_public_did("")
+    assert exc.value.status_code == 400
+    assert (
+        exc.value.detail
+        == "Bad headers. Either provide an api_key or both wallet_id and tenant_jwt"
+    )

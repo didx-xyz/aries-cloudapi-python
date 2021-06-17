6271fb0d2ced4a40a457beb45c6fa48444d1b7c5
morrieinmaas <motechnix@gmail.com>
2021-06-17 20:55:14 +0000

    Try with retries for request session

--- app/tests/bdd/steps/test_001_wallet.py
+++ app/tests/bdd/steps/test_001_wallet.py
@@ -5,11 +5,34 @@ import socket
 import pytest
 import requests
 from pytest_bdd import given, scenario, then, when
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
+
+def requests_retry_session(
+    retries=5,
+    backoff_factor=0.3,
+    status_forcelist=(500, 502, 504),
+    session=None,
+):
+    session = session or requests.Session()
+    retry = Retry(
+        total=retries,
+        read=retries,
+        connect=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    return session
+
 
 hostname = socket.gethostname()
 ip_address = socket.gethostbyname(hostname)
 
-FASTAPI_URL = "http://127.0.0.1:8000"
+FASTAPI_URL = "http://localhost:8000"
 
 
 @scenario("001_wallet.feature", "Getting a public DID")
@@ -41,7 +64,10 @@ def test_gen_pub_did():
     endpoint = target_did_enpoint()
     url = FASTAPI_URL + endpoint
 
-    time.sleep(10)
+    time.sleep(90)
+    s = requests.Session()
+    s.headers.update(header)
+    result = requests_retry_session().get(url)
     result = requests.get(url, headers=header)
     time.sleep(10)
     res_dict = json.loads(result.content)
@@ -57,7 +83,9 @@ def test_gen_pub_did_no_key():
     url = FASTAPI_URL + endpoint
 
     time.sleep(10)
-    result = requests.get(url, headers=header)
+    s = requests.Session()
+    s.headers.update(header)
+    result = requests_retry_session().get(url)
     time.sleep(5)
     res_dict = json.loads(result.content)
     assert result.status_code == 400

5441e8ae794d60c26e323f8165aca05455aeed09
Michael Wiles <michaelwiles@afrozaar.com>
2021-06-21 10:55:26 +0200

    #32 upgraded tests after adding agent controller injection via fast api depends

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -6,6 +6,7 @@ from mockito import mock
 
 import ledger_facade
 import utils
+from facade import create_controller_dependency, create_controller
 
 
 @pytest.fixture
@@ -14,7 +15,8 @@ def setup_env():
     utils.admin_port = "3021"
     utils.is_multitenant = False
     ledger_facade.LEDGER_URL = "http://testnet.didx.xyz/register"
-    ledger_facade.LEDGER_TYPE = 'von'
+    ledger_facade.LEDGER_TYPE = "von"
+
 
 @pytest.fixture
 def mock_agent_controller():
@@ -22,3 +24,9 @@ def mock_agent_controller():
     controller.wallet = mock(WalletController)
     controller.ledger = mock(LedgerController)
     return controller
+
+
+@pytest.fixture
+async def agent_controller():
+    async with create_controller({"api_key": "adminApiKey"}) as c:
+        yield c
--- app/tests/test_via_web_wallet.py
+++ app/tests/test_via_web_wallet.py
@@ -26,3 +26,14 @@ async def test_get_pub_did_via_web(setup_env):
     assert len(found) == 1
     assert found[0]["verkey"] == result["did_object"]["verkey"]
     assert found[0]["posture"] == "public"
+
+
+@pytest.mark.asyncio
+async def test_get_pub_did_via_web_no_header(setup_env):
+    async with AsyncClient(app=app, base_url="http://localhost:8000") as ac:
+        response = await ac.get("/wallets/create-pub-did", headers={})
+    assert response.status_code == 400
+    assert (
+        response.text
+        == '{"detail":"Bad headers. Either provide an api_key or both wallet_id and tenant_jwt"}'
+    )

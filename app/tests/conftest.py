dc1d99201ccabc58c77b59ecde56b1129bb44a60
Michael Wiles <michaelwiles@afrozaar.com>
2021-07-19 12:22:30 +0200

    adding fixtures to create async clients with headers for both bob and alice

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,4 +1,7 @@
+import json
 from contextlib import asynccontextmanager
+from dataclasses import dataclass
+from typing import Dict
 
 import ledger_facade
 import pytest
@@ -10,6 +13,15 @@ from dependencies import member_admin_agent, yoma_agent
 from httpx import AsyncClient
 from main import app
 from mockito import mock
+
+from tests.test_dependencies import async_next
+from tests.utils_test import get_random_string
+
+DEFAULT_HEADERS = {
+    "content-type": "application/json",
+    "x-role": "member",
+    "x-api-key": "adminApiKey",
+}
 
 
 @pytest.fixture
@@ -50,3 +62,80 @@ async def async_client():
 async def member_admin_agent_mock():
     async with asynccontextmanager(member_admin_agent)(x_api_key="adminApiKey") as c:
         yield c
+
+
+@dataclass
+class AgentEntity:
+    headers: Dict[str, str]
+    wallet_id: str
+    did: str
+    verkey: str
+
+
+@pytest.fixture
+async def async_client_bob(async_client):
+    async with agent_client(async_client, "bob") as client:
+        yield client
+
+
+@pytest.fixture
+async def async_client_alice(async_client):
+    async with agent_client(async_client, "alice") as client:
+        yield client
+
+
+@asynccontextmanager
+async def agent_client(async_client, name):
+    agent = await async_next(create_wallet(async_client, name))
+    async with AsyncClient(
+        app=app, base_url="http://localhost:8000", headers=agent.headers
+    ) as ac:
+        yield ac
+
+
+@pytest.fixture
+async def member_bob(async_client):
+    return await async_next(create_wallet(async_client, "bob"))
+
+
+@pytest.fixture
+async def member_alice(async_client):
+    return await async_next(create_wallet(async_client, "alice"))
+
+
+async def create_wallet(async_client, key):
+    def create_wallet_payload(key):
+        return {
+            "image_url": "https://aries.ca/images/sample.png",
+            "label": f"{key}{get_random_string(3)}",
+            "wallet_key": "MySecretKey1234",
+            "wallet_name": f"{key}{get_random_string(3)}",
+        }
+
+    wallet_payload = create_wallet_payload(key)
+
+    wallet = (
+        await async_client.post(
+            "/admin/wallet-multitenant" + "/create-wallet",
+            headers=DEFAULT_HEADERS,
+            data=json.dumps(wallet_payload),
+        )
+    ).json()
+
+    local_did = (
+        await async_client.get(
+            "/wallet/create-local-did",
+            headers={**DEFAULT_HEADERS, "authorization": f"Bearer {wallet['token']}"},
+        )
+    ).json()
+
+    yield AgentEntity(
+        headers={**DEFAULT_HEADERS, "authorization": f'Bearer {wallet["token"]}'},
+        wallet_id=wallet["wallet_id"],
+        did=local_did["result"]["did"],
+        verkey=local_did["result"]["verkey"],
+    )
+    await async_client.delete(
+        f"/admin/wallet-multitenant/{wallet['wallet_id']}",
+        headers=DEFAULT_HEADERS,
+    )

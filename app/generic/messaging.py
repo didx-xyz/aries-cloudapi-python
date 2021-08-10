59984f214a308150a67e4415c047f5d4d13515a9
Vineeth Rajesh <vineethrajesh14@gmail.com>
2021-08-10 18:07:13 +0530

    WIP Messaging Endpoints *Started send message test

--- app/generic/messaging.py
+++ app/generic/messaging.py
@@ -1,27 +1,37 @@
 import logging
 from aries_cloudcontroller import AriesAgentControllerBase
 from dependencies import agent_selector
-from fastapi import APIRouter, Depends, Optional
+from fastapi import APIRouter, Depends
+from typing import Optional
+from pydantic import BaseModel
 
 logger = logging.getLogger(__name__)
 
 router = APIRouter(prefix="/generic/messaging", tags=["messaging"])
 
 
+class Message(BaseModel):
+    connection_id: str
+    msg: str
+
+
 @router.post("/send-message")
 async def send_messages(
-    connection_id: str,
-    msg: str,
+    message: Message,
     aries_controller: AriesAgentControllerBase = Depends(agent_selector),
 ):
     send = await aries_controller.messaging.send_message(
-        connection_id=connection_id, msg=msg
+        connection_id=message.connection_id, msg=message.msg
     )
     return send
 
 
 @router.post("/trust-ping")
-async def send_trust_ping(connection_id: str, comment_msg: Optional[str]):
+async def send_trust_ping(
+    connection_id: str,
+    comment_msg: Optional[str],
+    aries_controller: AriesAgentControllerBase = Depends(agent_selector),
+):
 
     response = await aries_controller.messaging.trust_ping(
         connection_id=connection_id, comment_msg=comment_msg
---
+++ app/tests/test_messaging.py
@@ -0,0 +1,102 @@
+import asyncio
+import json
+import time
+from assertpy import assert_that
+
+import pytest
+from generic.messaging import Message
+
+APPLICATION_JSON_CONTENT_TYPE = {"content-type": "application/json"}
+MESSAGE_PATH = "/generic/messaging"
+BASE_PATH_CON = "/generic/connections"
+
+
+@pytest.yield_fixture(scope="module")
+def event_loop(request):
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture(scope="module")
+@pytest.mark.asyncio
+async def create_bob_and_alice_connect(
+    async_client_bob_module_scope, async_client_alice_module_scope
+):
+    async_client_bob = async_client_bob_module_scope
+    async_client_alice = async_client_alice_module_scope
+    """This test validates that bob and alice connect successfully...
+
+    NB: it assumes you have all the "auto connection" settings flagged as on.
+
+    ACAPY_AUTO_ACCEPT_INVITES=true
+    ACAPY_AUTO_ACCEPT_REQUESTS=true
+    ACAPY_AUTO_PING_CONNECTION=true
+
+    """
+    # creaet invitation on bob side
+    invitation = (await async_client_bob.get(BASE_PATH_CON + "/create-invite")).json()
+    bob_connection_id = invitation["connection_id"]
+    connections = (await async_client_bob.get(BASE_PATH_CON)).json()
+    assert_that(connections["results"]).extracting("connection_id").contains_only(
+        bob_connection_id
+    )
+
+    # accept invitation on alice side
+    invite_response = (
+        await async_client_alice.post(
+            BASE_PATH_CON + "/accept-invite", data=json.dumps(invitation["invitation"])
+        )
+    ).json()
+    time.sleep(15)
+    alice_connection_id = invite_response["connection_id"]
+    # fetch and validate
+    # both connections should be active - we have waited long enough for events to be exchanged
+    # and we are running in "auto connect" mode.
+    bob_connections = (await async_client_bob.get(BASE_PATH_CON)).json()
+    alice_connections = (await async_client_alice.get(BASE_PATH_CON)).json()
+
+    assert_that(bob_connections["results"]).extracting("connection_id").contains(
+        bob_connection_id
+    )
+    bob_connection = [
+        c for c in bob_connections["results"] if c["connection_id"] == bob_connection_id
+    ][0]
+    assert_that(bob_connection).has_state("active")
+
+    assert_that(alice_connections["results"]).extracting("connection_id").contains(
+        alice_connection_id
+    )
+    alice_connection = [
+        c
+        for c in alice_connections["results"]
+        if c["connection_id"] == alice_connection_id
+    ][0]
+    assert_that(alice_connection).has_state("active")
+
+    return {
+        "alice_connection_id": alice_connection_id,
+        "bob_connection_id": bob_connection_id,
+    }
+
+
+@pytest.fixture(scope="module")
+def bob_connection_id(create_bob_and_alice_connect):
+    return create_bob_and_alice_connect["bob_connection_id"]
+
+
+@pytest.fixture(scope="module")
+def alice_connection_id(create_bob_and_alice_connect):
+    return create_bob_and_alice_connect["alice_connection_id"]
+
+
+@pytest.mark.asyncio(scope="module")
+async def test_send_messages(
+    bob_connection_id, async_client_alice_module_scope, async_client_bob_module_scope
+):
+
+    message = Message(connection_id=bob_connection_id, msg="Donda").json()
+    response = await async_client_alice_module_scope.post(
+        MESSAGE_PATH + "/send-message", data=message
+    )
+    assert response == ""

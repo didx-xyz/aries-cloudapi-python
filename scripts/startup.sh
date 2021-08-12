a5b47ee4baf49c5b539410ad183c546fbc94d1e2
morrieinmaas <motechnix@gmail.com>
2021-08-12 08:15:06 +0000

    Add sleep 10 to agent startup script.
    This fixes startup failures when von network notes are not yet ready

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Sleep 10 hack to wait for von network nodes to start up
+sleep 10
+
 aca-py start \
     -it http '0.0.0.0' "$HTTP_PORT" \
     -e "$AGENT_ENDPOINT" "${AGENT_ENDPOINT/http/ws}" \

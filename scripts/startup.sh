a8e9911596ab8ceae35ccd00c1a9d8769bcdc399
morrieinmaas <motechnix@gmail.com>
2021-08-16 12:39:38 +0000

    Do not use cached containers

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Sleep 15 hack to wait for von network nodes to start up
-sleep 15
+# Sleep 30 hack to wait for von network nodes to start up
+sleep 30
 
 aca-py start \
     -it http '0.0.0.0' "$HTTP_PORT" \

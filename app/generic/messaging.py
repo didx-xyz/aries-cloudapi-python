ab230f58331e722c6a92a36eb49d83b0570d6308
morrieinmaas <motechnix@gmail.com>
2021-09-01 13:44:31 +0000

    [WIP] [skip ci] Fix messaging tests for new controller

--- app/generic/messaging.py
+++ app/generic/messaging.py
@@ -1,5 +1,5 @@
 import logging
-from aries_cloudcontroller import AcaPyClient
+from aries_cloudcontroller import AcaPyClient, SendMessage, PingRequest
 from dependencies import agent_selector
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
@@ -10,13 +10,13 @@ router = APIRouter(prefix="/generic/messaging", tags=["messaging"])
 
 
 class Message(BaseModel):
-    connection_id: str
-    msg: str
+    conn_id: str
+    sendMessage: SendMessage
 
 
 class TrustPingMsg(BaseModel):
-    connection_id: str
-    comment_msg: str = None
+    conn_id: str
+    pingRequest: PingRequest
 
 
 @router.post("/send-message")
@@ -36,8 +36,8 @@ async def send_messages(
     ---------
     The response object obtained when sending a message.
     """
-    send = await aries_controller.messaging.send_message(
-        connection_id=message.connection_id, msg=message.msg
+    send = await aries_controller.basicmessage.send_message(
+        conn_id=message.conn_id, body=message.sendMessage
     )
     return send
 
@@ -59,7 +59,7 @@ async def send_trust_ping(
     --------
     The response object obtained when sending a trust ping.
     """
-    response = await aries_controller.messaging.trust_ping(
-        connection_id=trustping_msg.connection_id, comment_msg=trustping_msg.comment_msg
+    response = await aries_controller.trustping.send_ping(
+        conn_id=trustping_msg.conn_id, body=trustping_msg.pingRequest
     )
     return response

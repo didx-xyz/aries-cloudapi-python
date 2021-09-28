ccdb70c77caed8745f0496f054e688078427fbb1
morrieinmaas <motechnix@gmail.com>
2021-09-28 19:11:00 +0000

    Add trust registry MVP that reads and writes from file.
    See also https://hackmd.io/8E6pYb4NRWKJy42tWt7BTA

--- trustregistry/registry/actor.py
+++ trustregistry/registry/actor.py
@@ -1,10 +1,10 @@
-import logging
+import json
+
+from trustregistry.main import registry
 from fastapi import APIRouter
 from pydantic import BaseModel, validator
 from typing import List
 from slugify import Slugify
-
-logger = logging.getLogger(__name__)
 
 router = APIRouter(prefix="/registry/actor", tags=["actor"])
 
@@ -17,30 +17,43 @@ class ActorID(BaseModel):
 
 class ActorUpdateFields(BaseModel):
     name: str
-    slug: str = None
     roles: List[str] = ["issuer", "verifier"]
     didcomm_invitation: str = None
     did: str = None
-
-    @validator("slug", always=True)
-    def slugify_id_for_name(cls, value, values):
-        return custom_slugify(values["name"])
 
 
 class RegisterActor(ActorUpdateFields, ActorID):
     pass
 
 
-@router.post("")
-async def register_actor(actor: RegisterActor):
-    return actor.dict()
+@router.post("/")
+async def register_actor(new_actor: RegisterActor):
+    with open("./registryfiles/trustregistry.json", "r+") as tr:
+        registry_file = json.load(tr)
+        # We could check whether the actor (id) is already present and return an error
+        # This way this endpoint is an "update" in disguise - agreed it's fine for now though
+        registry_file["actors"].append(new_actor.dict())
+        tr.write(json.dumps(registry_file))
+    return {}
 
 
 @router.put("/{actor_id}")
-async def update_actor(actor_id: str, actor: ActorUpdateFields):
-    return actor.dict()
+async def update_actor(actor_id: str, new_actor: ActorUpdateFields):
+    with open("./registryfiles/trustregistry.json", "r+") as tr:
+        registry_file = json.load(tr)
+        for actor in registry_file["actors"]:
+            if actor["id"] == actor_id:
+                actor = new_actor.dict()
+        tr.write(json.dumps(registry_file))
+    return {}
 
 
 @router.delete("/{actor_id}")
 async def remove_actor(actor_id: str):
-    return actor_id
+    with open("./registryfiles/trustregistry.json", "r+") as tr:
+        registry_file = json.load(tr)
+        for i, actor in enumerate(registry_file["actors"]):
+            if actor["id"] == actor_id:
+                del registry_file["actors"][i]
+        tr.write(json.dumps(registry_file))
+    return {}
--- trustregistry/registry/schema.py
+++ trustregistry/registry/schema.py
@@ -1,9 +1,8 @@
-import logging
+import json
+
 from typing import Optional
 from fastapi import APIRouter
 from pydantic import BaseModel, validator
-
-logger = logging.getLogger(__name__)
 
 router = APIRouter(prefix="/registry/schema", tags=["schema"])
 
@@ -32,16 +31,34 @@ class SchemaRegister(SchemaUpdate, SchemaBase):
         return values["id"].split(":")[2]
 
 
-@router.post("")
-async def register_schema(schema: SchemaRegister):
-    return schema.dict()
+@router.post("/")
+async def register_schema(new_schema: SchemaRegister):
+    with open("./registryfiles/trustregistry.json", "r+") as tr:
+        registry_file = json.load(tr)
+        registry_file["schemas"].append(sum(new_schema.dict().values()))
+        tr.write(json.dumps(registry_file))
+    return {}
 
 
 @router.put("/{schema_id}")
-async def update_schema(schema_id: str, schema: SchemaUpdate):
-    return schema.dict(), schema_id
+async def update_schema(schema_id: str, new_schema: SchemaUpdate):
+    with open("./registryfiles/trustregistry.json", "r+") as tr:
+        registry_file = json.load(tr)
+        for schema in registry_file["schemas"]:
+            schema_id_old = schema.split(":")[0]
+            if schema_id == schema_id_old:
+                schema = new_schema
+        tr.write(json.dumps(registry_file))
+    return {}
 
 
 @router.delete("/{schema_id}")
 async def remove_schema(schema_id: str):
-    return schema_id
+    with open("./registryfiles/trustregistry.json", "r+") as tr:
+        registry_file = json.load(tr)
+        for i, schema in enumerate(registry_file["schemas"]):
+            schema_id_old = schema.split(":")[0]
+            if schema_id == schema_id_old:
+                del registry_file["schemas"][i]
+        tr.write(json.dumps(registry_file))
+    return {}

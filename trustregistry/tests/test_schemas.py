d77c4b5f15d78654dbb95c3d3a8c7474e5e7cdc1
morrieinmaas <motechnix@gmail.com>
2021-10-05 14:55:33 +0000

    Add tests for schemas

---
+++ trustregistry/tests/test_schemas.py
@@ -0,0 +1,25 @@
+import schemas
+
+
+def test_actor():
+    actor = schemas.Actor(
+        id="mickey-mouse",
+        name="Mickey Mouse",
+        roles="verifier, issuer",
+        didcomm_invitation="xyz",
+        did="abc",
+    )
+
+    assert actor.id == "mickey-mouse"
+    assert actor.name == "Mickey Mouse"
+    assert actor.roles == "verifier, issuer"
+    assert actor.didcomm_invitation == "xyz"
+    assert actor.did == "abc"
+
+
+def test_schema():
+    schema = schemas.Schema(did="abc", name="doubleaceschema", version="0.4.20")
+
+    assert schema.did == "abc"
+    assert schema.name == "doubleaceschema"
+    assert schema.version == "0.4.20"

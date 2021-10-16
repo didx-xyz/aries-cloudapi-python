544e0a287ffc3dba8413b5c38569211c0e426765
morrieinmaas <motechnix@gmail.com>
2021-10-16 13:16:12 +0000

    Address code review feedback

--- app/trust_registry_facade.py
+++ app/trust_registry_facade.py
@@ -1,6 +1,6 @@
 import logging
 import os
-from typing import Literal, Union, List
+from typing import Literal, List
 from fastapi.exceptions import HTTPException
 
 import requests
@@ -9,32 +9,27 @@ TRUST_REGISTRY_URL = os.getenv("TRUST_REGISTRY_URL", "http://localhost:8001/")
 
 logger = logging.getLogger(__name__)
 
+Role = Literal["issuer", "verifier"]
 
-async def actor_has_role(
-    actor_id: str, role: str = Union[Literal["issuer"], Literal["verifier"]]
-) -> bool:
+
+async def actor_has_role(actor_id: str, role: Role) -> bool:
     actor_res = requests.get(TRUST_REGISTRY_URL + f"/registry/actors/{actor_id}")
     if actor_res.status_code != 200:
         raise HTTPException(404, detail="Actor does not exist")
     return bool(role in actor_res.json()["roles"])
 
 
-async def actors_with_role(
-    role: str = Union[Literal["issuer"], Literal["verifier"]]
-) -> list:
+async def actors_with_role(role: str = Role) -> list:
     actors = requests.get(TRUST_REGISTRY_URL + "/registry/actors")
-    actors_with_role_list = []
     if actors.status_code != 200:
-        return actors_with_role_list
-    [
-        actors_with_role_list.append(actor)
-        for actor in actors.json()["actors"]
-        if role in actor["roles"]
+        return []
+    actors_with_role_list = [
+        actor for actor in actors.json()["actors"] if role in actor["roles"]
     ]
     return actors_with_role_list
 
 
-async def actor_has_schema(actor_id: str, schema_id: str) -> False:
+async def actor_has_schema(actor_id: str, schema_id: str) -> bool:
     actor_res = requests.get(TRUST_REGISTRY_URL + f"/registry/actors/{actor_id}")
     if actor_res.status_code != 200:
         return False

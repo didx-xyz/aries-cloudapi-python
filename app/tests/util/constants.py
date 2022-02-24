1e399ce4e7661183c41750705feae0ec00435d69
Timo Glastra <timo@animo.id>
2022-02-24 10:32:34 +0000

    test: use local ledger
    Signed-off-by: Timo Glastra <timo@animo.id>

--- app/tests/util/constants.py
+++ app/tests/util/constants.py
@@ -7,5 +7,5 @@ MEMBER_ACAPY_API_KEY = "adminApiKey"
 ECOSYSTEM_FASTAPI_ENDPOINT = "http://localhost:8100"
 ECOSYSTEM_ACAPY_API_KEY = "adminApiKey"
 
-LEDGER_REGISTRATION_URL = "http://testnet.didx.xyz/register"
+LEDGER_REGISTRATION_URL = "http://localhost:9000/register"
 LEDGER_TYPE: str = "von"

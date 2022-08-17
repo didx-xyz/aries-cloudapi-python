1a6a6c2ecac29c26e2f5d7ecd1acda368b5fbc23
lohanspies <lohan.spies@didx.co.za>
2022-08-17 19:38:57 +0200

    clean up aws ecr repo create script

--- create_ecr_repos.sh
+++ create_ecr_repos.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-#ECR_REGISTRY=""
-#
-## get parameters
-#while getopts v: flag
-#do
-#  case "${flag}" in
-#    v) ECR_REGISTRY=${OPTARG};;
-#  esac
-#done
-
 for r in $(grep 'container_name:' docker-compose.yaml | sed -e 's/^.*\///')
 do
   #echo "test $r"
   aws ecr create-repository --repository-name "$r"
 done
-
-#for r in $(grep 'image: \${ECR_REGISTRY}' docker-compose.yaml | sed -e 's/^.*\///')
-#do
-#  aws ecr create-repository --repository-name "$r"
-#done

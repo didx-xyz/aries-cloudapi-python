2416eaf2322cbf526aeab5cb38f00c854e9f4155
lohanspies <lohan.spies@didx.co.za>
2022-08-17 20:52:04 +0200

    fix aws ecr repo create script

--- create_ecr_repos.sh
+++ create_ecr_repos.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#for r in $(grep 'container_name:' docker-compose.yaml | sed -e 's/^.*\///')
-#do
-#  #echo "test $r"
-#  aws ecr create-repository --repository-name "$r"
-#done
-
-for r in $(grep 'image: \${ECR_REGISTRY}' docker-compose.yaml | sed -e 's/^.*\///' | sed 's/:.*//')
+for r in $(grep 'container_name: ' docker-compose.yaml | sed -e 's/^.*container_name\://')
 do
   #echo "$r"
   aws ecr create-repository --repository-name "$r"
 done
+#echo "XXXXX"
+#for r in $(grep 'image: \${ECR_REGISTRY}' docker-compose.yaml | sed -e 's/^.*\///' | sed 's/:.*//')
+#do
+#  #echo "$r"
+#  aws ecr create-repository --repository-name "$r"
+#done

404c5948f675e55ca6dc2daf5f98aa045c14d689
lohanspies <21362987+lohanspies@users.noreply.github.com>
2022-08-17 16:22:34 +0200

    Update create_ecr_repos.sh

--- scripts/create_ecr_repos.sh
+++ scripts/create_ecr_repos.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+ECR_REGISTRY=""
+
+# get parameters
+while getopts v: flag
+do
+  case "${flag}" in
+    v) ECR_REGISTRY=${OPTARG};;
+  esac
+done
+
+
 for r in $(grep 'image: \${ECR_REGISTRY}' docker-compose.yml | sed -e 's/^.*\///')
 do
   aws ecr create-repository --repository-name "$r"

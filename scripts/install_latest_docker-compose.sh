c8759c4aeacdb5b089b57883503191e7dba15978
lohanspies <lohan.spies@didx.co.za>
2022-08-17 17:46:48 +0200

    add shell script to install latest docker-compose

---
+++ scripts/install_latest_docker-compose.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## THIS THE THE OLD WAY
+## Nowadays, simply follow the Compose installation instructions in the official documentation: https://docs.docker.com/compose/install/
+
+# get latest docker compose released tag
+COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
+
+# Install docker-compose
+sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+chmod +x /usr/local/bin/docker-compose
+sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+
+# Output compose version
+docker-compose -v
+
+exit 0

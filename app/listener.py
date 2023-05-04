9dae964640e5b91a45bdbfb942c7f3eb4bca9940
ff137 <ff137@proton.me>
2023-05-04 15:33:11 +0200

    rename queue to unprocessed_queue; add list of _processed_events, because reading from queue discards elements

--- app/listener.py
+++ app/listener.py
@@ -14,14 +14,15 @@ class Listener:
     def __init__(self, topic: CloudApiTopics, wallet_id: str):
         self.topic = topic
         self.wallet_id = wallet_id
-        self.queue = asyncio.Queue()
+        self.unprocessed_queue = asyncio.Queue()
+        self._processed_events = []
 
     async def handle_webhook(self, data: Dict[str, Any]):
         """
         Process a webhook event and add it to the queue if the topic and wallet_id match.
         """
         if data["topic"] == self.topic and data["wallet_id"] == self.wallet_id:
-            await self.queue.put(data)
+            await self.unprocessed_queue.put(data)
 
     async def wait_for_filtered_event(self, filter_map: Dict[str, Any], timeout: Optional[float] = 180):
         """
@@ -42,13 +43,15 @@ class Listener:
             Search the queue for an event that matches the specified filter_map. If a matching event
             is found, return its payload. Otherwise, return None.
             """
-            while not self.queue.empty():
-                item = await self.queue.get()
+            while not self.unprocessed_queue.empty():
+                item = await self.unprocessed_queue.get()
 
                 payload = item["payload"]
 
                 if _payload_matches_filter(payload, filter_map):
                     return payload
+                else:
+                    self._processed_events.append(item)
 
             # Return None or raise an exception if no matching payload is found
             return None

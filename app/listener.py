8a08c8067d2984d1b1b2f67e5ea3b5218d21f502
ff137 <ff137@proton.me>
2023-05-04 14:01:52 +0200

    add docstrings to listener

--- app/listener.py
+++ app/listener.py
@@ -6,24 +6,34 @@ from shared_models import CloudApiTopics
 
 
 class Listener:
+    """
+    A class for listening to webhook events filtered by topic and wallet_id. Events that match the
+    given topic and wallet_id are added to a queue, allowing the caller to wait for specific events
+    """
+
     def __init__(self, topic: CloudApiTopics, wallet_id: str):
         self.topic = topic
         self.wallet_id = wallet_id
         self.queue = asyncio.Queue()
 
-    async def on_webhook(self, data: Dict[str, Any]):
+        """
+        Process a webhook event and add it to the queue if the topic and wallet_id match.
+        """
         if data["topic"] == self.topic and data["wallet_id"] == self.wallet_id:
             await self.queue.put(data)
 
-    def _payload_matches_filter(self, payload: Dict[str, Any], filter_map: Dict[str, Any]) -> bool:
-        return all(
-            key in payload and payload.get(key, None) == filter_value
-            for key, filter_value in filter_map.items()
-        )
+        """
+        Wait for an event that matches the specified filter_map within the given timeout period.
+        """
+            """
+            Check if the given payload matches the specified filter_map. A payload is considered a
+            match if all key-value pairs in the filter_map have the same values in the payload.
+            """
 
-    async def wait_for_event(self, filter_map: Dict[str, Any]) -> Dict[str, Any]:
-        while not self.queue.empty():
-            item = await self.queue.get()
+            """
+            Search the queue for an event that matches the specified filter_map. If a matching event
+            is found, return its payload. Otherwise, return None.
+            """
 
             payload = item["payload"]
 
@@ -43,7 +53,11 @@ class Listener:
             await self.stop()
 
     async def start(self):
-        await Webhooks.register_listener(self.on_webhook)
+        """
+        Start the listener by registering its callback with the Webhooks class.
+        """
 
     async def stop(self):
-        Webhooks.unregister_listener(self.on_webhook)
+        """
+        Stop the listener by unregistering its callback from the Webhooks class.
+        """

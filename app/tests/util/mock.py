0b1691e099a7da557f769000666ce52022197811
ff137 <ff137@proton.me>
2023-05-13 12:18:36 +0200

    add default None

--- app/tests/util/mock.py
+++ app/tests/util/mock.py
@@ -2,5 +2,5 @@ from typing import Any
 
 
 # This allows easy mocking of async functions, making `response` awaitable
-async def to_async(response: Any):
+async def to_async(response: Any = None):
     return response

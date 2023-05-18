7aaffd0f5efc85d75259b9a7a034e34fa2999d04
ff137 <ff137@proton.me>
2023-05-18 12:50:29 +0200

    :art:

--- scripts/indy_healthcheck.sh
+++ scripts/indy_healthcheck.sh
@@ -7,12 +7,12 @@ POOL_NAME="health"
 CLI_TMP_FILE="/tmp/indy_healthcheck.txn"
 
 # Write the commands to the temporary configuration file
-echo "pool create $POOL_NAME gen_txn_file=/home/indy/ledger/sandbox/pool_transactions_genesis" >$CLI_TMP_FILE
-echo "pool connect $POOL_NAME" >>$CLI_TMP_FILE
-echo "pool refresh" >>$CLI_TMP_FILE
-echo "pool disconnect" >>$CLI_TMP_FILE
-echo "pool delete $POOL_NAME" >>$CLI_TMP_FILE
-echo "exit" >>$CLI_TMP_FILE
+echo "pool create $POOL_NAME gen_txn_file=/home/indy/ledger/sandbox/pool_transactions_genesis" > $CLI_TMP_FILE
+echo "pool connect $POOL_NAME" >> $CLI_TMP_FILE
+echo "pool refresh" >> $CLI_TMP_FILE
+echo "pool disconnect" >> $CLI_TMP_FILE
+echo "pool delete $POOL_NAME" >> $CLI_TMP_FILE
+echo "exit" >> $CLI_TMP_FILE
 NODES_STATUS=$(indy-cli $CLI_TMP_FILE 2>&1 | grep -P 'Pool\s+"\w+"\s+has been deleted.')
 # Run indy-cli with the temporary configuration file and check if deleteion of pool was successful
 # If the health check fails, exit with a non-zero status code

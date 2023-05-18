a07dfb0bb6e029d096ca5863898c2a4c1269454f
ff137 <ff137@proton.me>
2023-05-18 22:12:05 +0200

    add assert check

--- app/tests/e2e/test_issuer.py
+++ app/tests/e2e/test_issuer.py
@@ -153,6 +153,8 @@ async def test_send_credential_oob_v2(
     )
     alice_credentials_listener.stop()
 
+    assert result["credential_id"]
+
 
 @pytest.mark.anyio
 async def test_send_credential(

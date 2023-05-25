748eac32963603b844af7457d2fe139b83ddddda
ff137 <ff137@proton.me>
2023-05-25 15:44:22 +0200

    :art:

--- app/tests/e2e/test_fixtures.py
+++ app/tests/e2e/test_fixtures.py
@@ -129,8 +129,7 @@ async def credential_exchange_id(
 
     response = await alice_member_client.get(
         BASE_PATH,
-        params={
-            "connection_id": faber_and_alice_connection["alice_connection_id"]},
+        params={"connection_id": faber_and_alice_connection["alice_connection_id"]},
     )
     response.raise_for_status()
     records = response.json()
@@ -154,9 +153,7 @@ async def issue_credential_to_alice(
         "attributes": {"speed": "10"},
     }
 
-    listener = Listener(
-        topic="credentials", wallet_id=alice_tenant["tenant_id"]
-    )
+    listener = Listener(topic="credentials", wallet_id=alice_tenant["tenant_id"])
 
     # create and send credential offer- issuer
     response = await faber_client.post(

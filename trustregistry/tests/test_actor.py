f57f463a36ac1bb8b829ca3f4774b46263ad10dd
ff137 <ff137@proton.me>
2023-05-26 18:06:37 +0200

    refactored tests to use AsyncClient, testing against TRUST_REGISTRY_URL

--- trustregistry/tests/test_actor.py
+++ trustregistry/tests/test_actor.py
@@ -1,8 +1,9 @@
 import json
 
-from trustregistry.tests import test_main
+import pytest
+from httpx import AsyncClient
 
-client = test_main.client
+from app.constants import TRUST_REGISTRY_URL
 
 new_actor = {
     "id": "darth-vader",
@@ -11,70 +12,76 @@ new_actor = {
     "didcomm_invitation": "string",
     "did": "did:key:string",
 }
+actor_id = new_actor["id"]
 
 
-def test_get_actors():
-    response = client.get("/registry/actors")
+@pytest.mark.anyio
+async def test_get_actors():
+    async with AsyncClient() as client:
+        response = await client.get(f"{TRUST_REGISTRY_URL}/registry/actors")
+
     assert response.status_code == 200
-    assert response.json() == {"actors": []}
+    assert "actors" in response.json()
 
 
-def test_register_actor():
+@pytest.mark.anyio
+async def test_register_actor():
     payload = json.dumps(new_actor)
-    response = client.post(
-        "/registry/actors",
-        headers={"content-type": "application/json", "accept": "application/json"},
-        data=payload,
-    )
-    assert response.json() == json.loads(payload)
-    assert response.status_code == 200
+    async with AsyncClient() as client:
+        response = await client.post(
+            f"{TRUST_REGISTRY_URL}/registry/actors",
+            content=payload,
+        )
+        assert response.json() == json.loads(payload)
+        assert response.status_code == 200
 
-    new_actor_resp = client.get("/registry/actors")
-    assert new_actor_resp.status_code == 200
-    new_actors = new_actor_resp.json()
-    assert new_actor["id"] in [actor["id"] for actor in new_actors["actors"]]
+        new_actor_resp = await client.get(f"{TRUST_REGISTRY_URL}/registry/actors")
+        assert new_actor_resp.status_code == 200
+        new_actors = new_actor_resp.json()
+        assert new_actor["id"] in [actor["id"] for actor in new_actors["actors"]]
 
-    response = client.post(
-        "/registry/actors",
-        headers={"content-type": "application/json", "accept": "application/json"},
-        data=payload,
-    )
-    assert response.status_code == 405
-    assert "Actor already exists" in response.json()["detail"]
-
-
-def test_update_actor():
-    response = client.post(
-        "/registry/actors/darth-vader",
-        headers={"content-type": "application/json", "accept": "application/json"},
-        json=new_actor,
-    )
-    assert response.status_code == 200
-    assert response.json() == new_actor
-
-    new_actors_resp = client.get("/registry/actors")
-    assert new_actors_resp.status_code == 200
-    new_actors_list = new_actors_resp.json()
-    assert new_actor in new_actors_list["actors"]
-
-    response = client.post(
-        "/registry/actors/idonotexist",
-        headers={"content-type": "application/json", "accept": "application/json"},
-        json=new_actor,
-    )
-    assert response.status_code == 404
-    assert "Actor not found" in response.json()["detail"]
+        response = await client.post(
+            f"{TRUST_REGISTRY_URL}/registry/actors",
+            content=payload,
+        )
+        assert response.status_code == 405
+        assert "Actor already exists" in response.json()["detail"]
 
 
-def test_update_actor_x():
+@pytest.mark.anyio
+async def test_update_actor():
+    async with AsyncClient() as client:
+        response = await client.post(
+            f"{TRUST_REGISTRY_URL}/registry/actors/{actor_id}",
+            json=new_actor,
+        )
+        assert response.status_code == 200
+        assert response.json() == new_actor
+
+        new_actors_resp = await client.get(f"{TRUST_REGISTRY_URL}/registry/actors")
+        assert new_actors_resp.status_code == 200
+        new_actors_list = new_actors_resp.json()
+        assert new_actor in new_actors_list["actors"]
+
+        response = await client.post(
+            f"{TRUST_REGISTRY_URL}/registry/actors/idonotexist",
+            json=new_actor,
+        )
+        assert response.status_code == 404
+        assert "Actor not found" in response.json()["detail"]
+
+
+@pytest.mark.anyio
+async def test_update_actor_x():
     updated_actor = new_actor.copy()
     updated_actor["did"] = None
 
-    response = client.post(
-        "/registry/actors/darth-vader",
-        headers={"content-type": "application/json", "accept": "application/json"},
-        json=updated_actor,
-    )
+    async with AsyncClient() as client:
+        response = await client.post(
+            f"{TRUST_REGISTRY_URL}/registry/actors/{actor_id}",
+            json=updated_actor,
+        )
+
     assert response.status_code == 422
     assert response.json() == {
         "detail": [
@@ -87,14 +94,17 @@ def test_update_actor_x():
     }
 
 
-def test_remove_schema():
-    response = client.delete("/registry/actors/darth-vader")
-    assert response.status_code == 204
-    assert response.json() is None
+@pytest.mark.anyio
+async def test_remove_schema():
+    async with AsyncClient() as client:
+        response = await client.delete(
+            f"{TRUST_REGISTRY_URL}/registry/actors/{actor_id}"
+        )
+        assert response.status_code == 204
+        assert not response.text
 
-    response = client.delete(
-        "/registry/actors/darth-vader",
-        headers={"content-type": "application/json", "accept": "application/json"},
-    )
-    assert response.status_code == 404
-    assert "Actor not found" in response.json()["detail"]
+        response = await client.delete(
+            f"{TRUST_REGISTRY_URL}/registry/actors/{actor_id}"
+        )
+        assert response.status_code == 404
+        assert "Actor not found" in response.json()["detail"]

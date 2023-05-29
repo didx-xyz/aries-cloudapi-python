4be1d41a5777a9123e26e67cb66072586c9a7fcf
ff137 <ff137@proton.me>
2023-05-29 22:58:01 +0200

    :art: deduplicate with def get_token

--- app/tests/util/member_acapy_clients.py
+++ app/tests/util/member_acapy_clients.py
@@ -24,11 +24,16 @@ async def tenant_admin_acapy_client():
     await acapy_client.close()
 
 
+def get_token(client):
+    # We extract the token from the x-api-key header as that's the easiest
+    # method to create an AcaPyClient from an AsyncClient
+    [_, token] = client.headers.get("x-api-key").split(".", maxsplit=1)
+    return token
+
+
 @pytest.fixture(scope="function")
 async def alice_acapy_client(alice_member_client: RichAsyncClient):
-    [_, token] = alice_member_client.headers.get("x-api-key").split(".", maxsplit=1)
-
-    acapy_client = get_tenant_acapy_client(token=token)
+    acapy_client = get_tenant_acapy_client(token=get_token(alice_member_client))
     yield acapy_client
 
     await acapy_client.close()
@@ -36,11 +41,9 @@ async def alice_acapy_client(alice_member_client: RichAsyncClient):
 
 @pytest.fixture(scope="function")
 async def bob_acapy_client(bob_member_client: RichAsyncClient):
-    # We extract the token from the x-api-key header as that's the easiest
-    # method to create an AcaPyClient from an AsyncClient
     [_, token] = bob_member_client.headers.get("x-api-key").split(".", maxsplit=1)
 
-    acapy_client = get_tenant_acapy_client(token=token)
+    acapy_client = get_tenant_acapy_client(token=get_token(bob_member_client))
     yield acapy_client
 
     await acapy_client.close()
@@ -48,23 +51,15 @@ async def bob_acapy_client(bob_member_client: RichAsyncClient):
 
 @pytest.fixture(scope="function")
 async def faber_acapy_client(faber_client: RichAsyncClient):
-    # We extract the token from the x-api-key header as that's the easiest
-    # method to create an AcaPyClient from an AsyncClient
-    [_, token] = faber_client.headers.get("x-api-key").split(".", maxsplit=1)
-
-    acapy_client = get_tenant_acapy_client(token=token)
+    acapy_client = get_tenant_acapy_client(token=get_token(faber_client))
     yield acapy_client
 
     await acapy_client.close()
 
 
 @pytest.fixture(scope="function")
-async def acme_acapy_client(faber_client: RichAsyncClient):
-    # We extract the token from the x-api-key header as that's the easiest
-    # method to create an AcaPyClient from an AsyncClient
-    [_, token] = faber_client.headers.get("x-api-key").split(".", maxsplit=1)
-
-    acapy_client = get_tenant_acapy_client(token=token)
+async def acme_acapy_client(acme_client: RichAsyncClient):
+    acapy_client = get_tenant_acapy_client(token=get_token(acme_client))
     yield acapy_client
 
     await acapy_client.close()

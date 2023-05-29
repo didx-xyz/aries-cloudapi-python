54c2eac3577de04f9e1cde109126bec42e86ff7b
ff137 <ff137@proton.me>
2023-05-29 17:33:06 +0200

    use new RichAsyncClient

--- app/tests/util/member_personas.py
+++ app/tests/util/member_personas.py
@@ -2,7 +2,6 @@ from typing import Any, Dict, TypedDict
 
 import pytest
 from aries_cloudcontroller import AcaPyClient
-from httpx import AsyncClient
 
 from app.generic.connections.connections import CreateInvitation
 from app.generic.verifier.verifier_utils import ed25519_verkey_to_did_key
@@ -15,6 +14,7 @@ from app.tests.util.ledger import create_public_did
 from app.tests.util.tenants import (create_issuer_tenant, create_tenant,
                                     delete_tenant)
 from app.tests.util.webhooks import check_webhook_state
+from app.util.rich_async_client import RichAsyncClient
 
 
 class BobAliceConnect(TypedDict):
@@ -60,7 +60,7 @@ async def alice_member_client(alice_tenant: Any):
 
 
 @pytest.fixture(scope="function")
-async def bob_acapy_client(bob_member_client: AsyncClient):
+async def bob_acapy_client(bob_member_client: RichAsyncClient):
     # We extract the token from the x-api-key header as that's the easiest
     # method to create an AcaPyClient from an AsyncClient
     [_, token] = bob_member_client.headers.get(
@@ -73,9 +73,7 @@ async def bob_acapy_client(bob_member_client: AsyncClient):
 
 
 @pytest.fixture(scope="function")
-async def alice_acapy_client(alice_member_client: AsyncClient):
-    [_, token] = alice_member_client.headers.get(
-        "x-api-key").split(".", maxsplit=1)
+async def alice_acapy_client(alice_member_client: RichAsyncClient):
 
     client = get_tenant_acapy_client(token=token)
     yield client
@@ -116,8 +114,8 @@ async def bob_and_alice_public_did(
 
 @pytest.fixture(scope="function")
 async def bob_and_alice_connection(
-    bob_member_client: AsyncClient,
-    alice_member_client: AsyncClient,
+    bob_member_client: RichAsyncClient,
+    alice_member_client: RichAsyncClient,
 ) -> BobAliceConnect:
     # create invitation on bob side
     invitation = (
@@ -181,7 +179,7 @@ class MultiInvite(TypedDict):
 
 @pytest.fixture(scope="function")
 async def bob_multi_use_invitation(
-    bob_member_client: AsyncClient,
+    bob_member_client: RichAsyncClient,
 ) -> MultiInvite:
     create_invite_json = CreateInvitation(
         alias=None,
@@ -208,8 +206,8 @@ async def bob_multi_use_invitation(
 
 @pytest.fixture(scope="function")
 async def alice_bob_connect_multi(
-    bob_member_client: AsyncClient,
-    alice_member_client: AsyncClient,
+    bob_member_client: RichAsyncClient,
+    alice_member_client: RichAsyncClient,
     bob_multi_use_invitation: MultiInvite,
 ) -> BobAliceConnect:
     invitation = bob_multi_use_invitation["multi_use_invitation"]["invitation"]

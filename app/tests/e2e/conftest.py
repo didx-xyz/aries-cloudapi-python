ddc12d2fbe4870f55438d45cd699f4f797e89d29
ff137 <ff137@proton.me>
2023-05-29 22:19:05 +0200

    governance_public_did fixture should return the did

--- app/tests/e2e/conftest.py
+++ app/tests/e2e/conftest.py
@@ -14,7 +14,9 @@ from app.tests.util.ledger import create_public_did, has_public_did
 
 
 # Governace should be provisioned with public did in all e2e tests
-@pytest.fixture(autouse=True, scope="module")
-async def governance_public_did(governance_acapy_client: AcaPyClient):
-    if not await has_public_did(governance_acapy_client):
-        await create_public_did(governance_acapy_client, set_public=True)
+async def governance_public_did(governance_acapy_client: AcaPyClient) -> str:
+    try:
+        did = await get_public_did(governance_acapy_client)
+    except:
+        did = await create_public_did(governance_acapy_client, set_public=True)
+    return did.did

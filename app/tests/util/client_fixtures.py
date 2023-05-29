6e7dbbe4ed31b40afb855afd15a6318c3867f023
ff137 <ff137@proton.me>
2023-05-29 15:54:06 +0200

    use new get_ methods and use clearer name for all the `client` instances

--- app/tests/util/client_fixtures.py
+++ app/tests/util/client_fixtures.py
@@ -1,41 +1,38 @@
 import pytest
 
 from app.tests.util.client import (
-    governance_client as _governance_client,
-    governance_acapy_client as _governance_acapy_client,
-    tenant_admin_client as _tenant_admin_client,
-    tenant_admin_acapy_client as _tenant_admin_acapy_client,
+    get_governance_acapy_client,
+    get_governance_client,
+    get_tenant_admin_acapy_client,
+    get_tenant_admin_client,
 )
 
-# governance
 
-
+# Governance
 @pytest.fixture(scope="session")
 async def governance_acapy_client():
-    client = _governance_acapy_client()
-    yield client
+    acapy_client = get_governance_acapy_client()
+    yield acapy_client()
 
-    await client.close()
+    await acapy_client.close()
 
 
 @pytest.fixture(scope="function")
 async def governance_client():
-    async with _governance_client() as client:
-        yield client
+    async with get_governance_client() as async_client:
+        yield async_client
 
 
-# TENANT ADMIN
-
-
+# Tenant Admin
 @pytest.fixture(scope="function")
 async def tenant_admin_client():
-    async with _tenant_admin_client() as client:
-        yield client
+    async with get_tenant_admin_client() as async_client:
+        yield async_client
 
 
 @pytest.fixture(scope="function")
 async def tenant_admin_acapy_client():
-    client = _tenant_admin_acapy_client()
-    yield client
+    acapy_client = get_tenant_admin_acapy_client()
+    yield acapy_client
 
-    await client.close()
+    await acapy_client.close()

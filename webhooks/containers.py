804520aaceaa2dc5c08bb1e02987a3058ceb4765
ff137 <ff137@proton.me>
2023-05-31 21:21:15 +0200

    :art: lint

--- webhooks/containers.py
+++ webhooks/containers.py
@@ -1,21 +1,15 @@
-"""Containers module."""
-
 from dependency_injector import containers, providers
 
-from webhooks import redis
-from webhooks import services
+from webhooks import redis, services
 
 
 class Container(containers.DeclarativeContainer):
-
     config = providers.Configuration()
-
     redis_pool = providers.Resource(
         redis.init_redis_pool,
         host=config.redis_host,
         password=config.redis_password,
     )
-
     service = providers.Factory(
         services.Service,
         redis=redis_pool,

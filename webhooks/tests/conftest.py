9039785f4a73e05437cc5b7a0469c92784d3b7f2
ff137 <ff137@proton.me>
2023-05-31 13:15:31 +0200

    :art: pylint disabler

--- webhooks/tests/conftest.py
+++ webhooks/tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+# pylint: disable=unused-import
 from app.tests.util.ecosystem_personas import acme_and_alice_connection, acme_tenant
 from app.tests.util.member_personas import alice_member_client, alice_tenant
 

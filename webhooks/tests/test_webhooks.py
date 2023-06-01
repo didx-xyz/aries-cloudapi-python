e32c85003c789e6880f66391386937a9b45d2625
ff137 <ff137@proton.me>
2023-06-01 17:09:58 +0200

    :art: use `async with` to open client

--- webhooks/tests/test_webhooks.py
+++ webhooks/tests/test_webhooks.py
@@ -12,14 +12,14 @@ from webhooks.main import app
 async def test_connection_webhooks(
     alice_member_client: RichAsyncClient, bob_and_alice_connection: BobAliceConnect
 ):
-    client = AsyncClient(app=app, base_url=WEBHOOKS_URL)
-    alice_wallet_id = get_wallet_id_from_async_client(alice_member_client)
-    alice_connection_id = bob_and_alice_connection.alice_connection_id
+    async with AsyncClient(app=app, base_url=WEBHOOKS_URL) as client:
+        alice_wallet_id = get_wallet_id_from_async_client(alice_member_client)
+        alice_connection_id = bob_and_alice_connection.alice_connection_id
 
-    response = await client.get(f"/webhooks/{alice_wallet_id}")
-    assert response.status_code == 200
+        response = await client.get(f"/webhooks/{alice_wallet_id}")
+        assert response.status_code == 200
 
-    response_text = response.text
-    assert '"topic":"connections"' in response_text
-    assert f'"connection_id":"{alice_connection_id}"' in response_text
-    assert f'"wallet_id":"{alice_wallet_id}"' in response_text
+        response_text = response.text
+        assert '"topic":"connections"' in response_text
+        assert f'"connection_id":"{alice_connection_id}"' in response_text
+        assert f'"wallet_id":"{alice_wallet_id}"' in response_text

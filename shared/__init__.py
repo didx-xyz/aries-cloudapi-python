11632f81d4c6e69d82c9b12a26ea121afcb5c8c2
ff137 <ff137@proton.me>
2023-06-03 00:28:35 +0200

    :art: import

--- shared/__init__.py
+++ shared/__init__.py
@@ -1,7 +1,8 @@
+from shared.cloud_api_error import CloudApiException  # pylint: disable=unused-import
 from shared.models.conversion import *  # pylint: disable=wildcard-import
 from shared.models.protocol import *
 from shared.models.topics import *
 from shared.models.topics.base import *
-from shared.util.api_router import APIRouter  # pylint: disable=unused-import
+from shared.util.api_router import APIRouter
 from shared.util.rich_async_client import RichAsyncClient
 from shared.util.rich_parsing import parse_with_error_handling

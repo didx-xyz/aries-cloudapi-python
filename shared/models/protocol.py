99ca9dcf368f96787923f260bc5bc3f4c0e9d413
ff137 <ff137@proton.me>
2023-06-02 23:20:00 +0200

    :truck: move protocol version defs

---
+++ shared/models/protocol.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+
+class PresentProofProtocolVersion(Enum):
+    v1: str = "v1"
+    v2: str = "v2"
+
+
+class IssueCredentialProtocolVersion(Enum):
+    v1: str = "v1"
+    v2: str = "v2"
+
+
+def pres_id_no_version(proof_id: str) -> str:
+    if proof_id.startswith("v2-") or proof_id.startswith("v1-"):
+        return proof_id[3:]
+    else:
+        raise ValueError("proof_id must start with prefix v1- or v2-")

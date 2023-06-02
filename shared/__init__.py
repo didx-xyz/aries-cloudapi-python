54d83f05fa2552f917aac15d289b20412100f4b9
ff137 <ff137@proton.me>
2023-06-03 00:06:05 +0200

    :art: pylint

--- shared/__init__.py
+++ shared/__init__.py
@@ -1,8 +1,7 @@
-# pylint: disable=unused-import,wildcard-import
-from shared.models.conversion import *
+from shared.models.conversion import *  # pylint: disable=wildcard-import
 from shared.models.protocol import *
 from shared.models.topics import *
 from shared.models.topics.base import *
-from shared.util.api_router import APIRouter
+from shared.util.api_router import APIRouter  # pylint: disable=unused-import
 from shared.util.rich_async_client import RichAsyncClient
 from shared.util.rich_parsing import parse_with_error_handling

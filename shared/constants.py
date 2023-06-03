77e59638631f109b6f791e75a31fa599ec1cbfd8
ff137 <ff137@proton.me>
2023-06-03 14:58:42 +0200

    add constants from app.util.tests -- some duplication of env variables here

--- shared/constants.py
+++ shared/constants.py
@@ -4,9 +4,17 @@ host = "localhost"
 url = f"http://{host}"
 adminApiKey = "adminApiKey"
 
+
 GOVERNANCE_AGENT_URL = os.getenv("ACAPY_GOVERNANCE_AGENT_URL", f"{url}:3021")
-MULTITENANT_AGENT_URL = os.getenv("ACAPY_TENANT_AGENT_URL", f"{url}:4021")
 GOVERNANCE_AGENT_API_KEY = os.getenv("ACAPY_GOVERNANCE_AGENT_API_KEY", adminApiKey)
+
+GOVERNANCE_FASTAPI_ENDPOINT = os.getenv("GOVERNANCE_FASTAPI_ENDPOINT", f"{url}:8100")
+GOVERNANCE_ACAPY_API_KEY = os.getenv("GOVERNANCE_ACAPY_API_KEY", adminApiKey)
+
+TENANT_FASTAPI_ENDPOINT = os.getenv("TENANT_FASTAPI_ENDPOINT", f"{url}:8100")
+TENANT_ACAPY_API_KEY = os.getenv("TENANT_ACAPY_API_KEY", adminApiKey)
+
+MULTITENANT_AGENT_URL = os.getenv("ACAPY_TENANT_AGENT_URL", f"{url}:4021")
 
 TENANT_AGENT_URL = os.getenv("ACAPY_TENANT_AGENT_URL", f"{url}:4021")
 TENANT_AGENT_API_KEY = os.getenv("ACAPY_TENANT_AGENT_API_KEY", adminApiKey)
@@ -21,3 +29,7 @@ ACAPY_ENDORSER_ALIAS = os.getenv("ACAPY_ENDORSER_ALIAS", "endorser")
 
 CLOUDAPI_URL = os.getenv("CLOUDAPI_URL", f"{url}:8000")
 ACAPY_TAILS_SERVER_BASE_URL = os.getenv("ACAPY_TAILS_SERVER_BASE_URL", f"{url}:6543")
+
+# For testing ledger
+LEDGER_TYPE: str = "von"
+LEDGER_REGISTRATION_URL = os.getenv("LEDGER_REGISTRATION_URL", f"{url}:9000/register")

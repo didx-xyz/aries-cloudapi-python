a8dba9fbb4cd32d2e686a17582e000c074ec437d
ff137 <ff137@proton.me>
2023-06-03 13:56:58 +0200

    add WEBHOOKS_PUBSUB_URL

--- shared/constants.py
+++ shared/constants.py
@@ -10,6 +10,7 @@ TENANT_AGENT_API_KEY = os.getenv("ACAPY_TENANT_AGENT_API_KEY", "adminApiKey")
 TRUST_REGISTRY_URL = os.getenv("TRUST_REGISTRY_URL", "http://localhost:8001")
 
 WEBHOOKS_URL = os.getenv("WEBHOOKS_URL", "http://localhost:3010")
+WEBHOOKS_PUBSUB_URL = os.getenv("WEBHOOKS_PUBSUB_URL", "ws://localhost:3010/pubsub")
 
 ACAPY_MULTITENANT_JWT_SECRET = os.getenv("ACAPY_MULTITENANT_JWT_SECRET", "jwtSecret")
 ACAPY_ENDORSER_ALIAS = os.getenv("ACAPY_ENDORSER_ALIAS", "endorser")

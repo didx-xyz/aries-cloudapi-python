97450c1db5a284c71d78aa4f3cdcab638dc33038
ff137 <ff137@proton.me>
2023-06-03 14:42:48 +0200

    :art: parameterise host url

--- shared/constants.py
+++ shared/constants.py
@@ -1,19 +1,23 @@
 import os
 
-GOVERNANCE_AGENT_URL = os.getenv("ACAPY_GOVERNANCE_AGENT_URL", "http://localhost:3021")
-MULTITENANT_AGENT_URL = os.getenv("ACAPY_TENANT_AGENT_URL", "http://localhost:4021")
-GOVERNANCE_AGENT_API_KEY = os.getenv("ACAPY_GOVERNANCE_AGENT_API_KEY", "adminApiKey")
+host = "localhost"
+url = f"http://{host}"
+adminApiKey = "adminApiKey"
 
-TENANT_AGENT_URL = os.getenv("ACAPY_TENANT_AGENT_URL", "http://localhost:4021")
-TENANT_AGENT_API_KEY = os.getenv("ACAPY_TENANT_AGENT_API_KEY", "adminApiKey")
+GOVERNANCE_AGENT_URL = os.getenv("ACAPY_GOVERNANCE_AGENT_URL", f"{url}:3021")
+MULTITENANT_AGENT_URL = os.getenv("ACAPY_TENANT_AGENT_URL", f"{url}:4021")
+GOVERNANCE_AGENT_API_KEY = os.getenv("ACAPY_GOVERNANCE_AGENT_API_KEY", adminApiKey)
 
-TRUST_REGISTRY_URL = os.getenv("TRUST_REGISTRY_URL", "http://localhost:8001")
+TENANT_AGENT_URL = os.getenv("ACAPY_TENANT_AGENT_URL", f"{url}:4021")
+TENANT_AGENT_API_KEY = os.getenv("ACAPY_TENANT_AGENT_API_KEY", adminApiKey)
 
-WEBHOOKS_URL = os.getenv("WEBHOOKS_URL", "http://localhost:3010")
-WEBHOOKS_PUBSUB_URL = os.getenv("WEBHOOKS_PUBSUB_URL", "ws://localhost:3010/pubsub")
+TRUST_REGISTRY_URL = os.getenv("TRUST_REGISTRY_URL", f"{url}:8001")
+
+WEBHOOKS_URL = os.getenv("WEBHOOKS_URL", f"{url}:3010")
+WEBHOOKS_PUBSUB_URL = os.getenv("WEBHOOKS_PUBSUB_URL", f"ws://{host}:3010/pubsub")
 
 ACAPY_MULTITENANT_JWT_SECRET = os.getenv("ACAPY_MULTITENANT_JWT_SECRET", "jwtSecret")
 ACAPY_ENDORSER_ALIAS = os.getenv("ACAPY_ENDORSER_ALIAS", "endorser")
 
-CLOUDAPI_URL = os.getenv("CLOUDAPI_URL", "http://localhost:8000")
-ACAPY_TAILS_SERVER_BASE_URL = os.getenv("ACAPY_TAILS_SERVER_BASE_URL", "http://localhost:6543")
+CLOUDAPI_URL = os.getenv("CLOUDAPI_URL", f"{url}:8000")
+ACAPY_TAILS_SERVER_BASE_URL = os.getenv("ACAPY_TAILS_SERVER_BASE_URL", f"{url}:6543")

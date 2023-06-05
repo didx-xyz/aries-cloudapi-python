50b2c8e6b07ccdcaaa3440df83a18c0a1c08aae0
ff137 <ff137@proton.me>
2023-06-05 13:29:21 +0200

    :art: moved tenant definitions for bob and faber (previously done separately in `member_async_clients`)

--- app/tests/util/member_wallets.py
+++ app/tests/util/member_wallets.py
@@ -1,13 +1,28 @@
 import pytest
 
 from app.tests.util.client import get_tenant_admin_client
-from app.tests.util.tenants import create_tenant, create_verifier_tenant, delete_tenant
+from app.tests.util.tenants import (
+    create_issuer_tenant,
+    create_tenant,
+    create_verifier_tenant,
+    delete_tenant,
+)
 
 
 @pytest.fixture(scope="function")
 async def alice_tenant():
     async with get_tenant_admin_client() as admin_client:
         tenant = await create_tenant(admin_client, "alice")
+
+        yield tenant
+
+        await delete_tenant(admin_client, tenant.tenant_id)
+
+
+@pytest.fixture(scope="function")
+async def bob_tenant():
+    async with get_tenant_admin_client() as admin_client:
+        tenant = await create_tenant(admin_client, "bob")
 
         yield tenant
 
@@ -22,3 +37,13 @@ async def acme_verifier():
         yield verifier_tenant
 
         await delete_tenant(admin_client, verifier_tenant.tenant_id)
+
+
+@pytest.fixture(scope="function")
+async def faber_issuer():
+    async with get_tenant_admin_client() as admin_client:
+        issuer_tenant = await create_issuer_tenant(admin_client, "faber")
+
+        yield issuer_tenant
+
+        await delete_tenant(admin_client, issuer_tenant.tenant_id)

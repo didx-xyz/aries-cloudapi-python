c933ec5aa60dcc46a115e9f76495046b794862a0
ff137 <ff137@proton.me>
2023-06-05 13:28:27 +0200

    :art: standardise each fixture to use `async with` pattern. move all tenant client creation to `member_wallets`

--- app/tests/util/member_async_clients.py
+++ app/tests/util/member_async_clients.py
@@ -6,59 +6,39 @@ from app.tests.util.client import (
     get_tenant_admin_client,
     get_tenant_client,
 )
-from app.tests.util.tenants import create_issuer_tenant, delete_tenant
 
 
 @pytest.fixture(scope="session")
 async def governance_client():
-    async with get_governance_client() as async_client:
-        yield async_client
+    async with get_governance_client() as gov_async_client:
+        yield gov_async_client
 
 
 @pytest.fixture(scope="function")
 async def tenant_admin_client():
-    async with get_tenant_admin_client() as async_client:
-        yield async_client
+    async with get_tenant_admin_client() as admin_async_client:
+        yield admin_async_client
 
 
 @pytest.fixture(scope="function")
 async def alice_member_client(alice_tenant: CreateTenantResponse):
-    alice_client = get_tenant_client(token=alice_tenant.access_token)
-
-    yield alice_client
-
-    await alice_client.aclose()
+    async with get_tenant_client(token=alice_tenant.access_token) as alice_async_client:
+        yield alice_async_client
 
 
 @pytest.fixture(scope="function")
-async def bob_member_client():
-    async with get_tenant_admin_client() as admin_async_client:
-        tenant = await create_issuer_tenant(admin_async_client, "bob")
-
-        bob_client = get_tenant_client(token=tenant.access_token)
-        yield bob_client
-
-        await bob_client.aclose()
-
-        await delete_tenant(admin_async_client, tenant.tenant_id)
+async def bob_member_client(bob_tenant: CreateTenantResponse):
+    async with get_tenant_client(token=bob_tenant.access_token) as bob_async_client:
+        yield bob_async_client
 
 
 @pytest.fixture(scope="function")
-async def faber_client():
-    async with get_tenant_admin_client() as admin_async_client:
-        faber_issuer = await create_issuer_tenant(admin_async_client, "faber")
-
-        faber_async_client = get_tenant_client(token=faber_issuer.access_token)
+async def faber_client(faber_issuer: CreateTenantResponse):
+    async with get_tenant_client(token=faber_issuer.access_token) as faber_async_client:
         yield faber_async_client
 
-        await faber_async_client.aclose()
-
-        await delete_tenant(admin_async_client, faber_issuer.tenant_id)
-
 
 @pytest.fixture(scope="function")
-async def acme_client(acme_tenant: CreateTenantResponse):
-    acme_async_client = get_tenant_client(token=acme_tenant.access_token)
-    yield acme_async_client
-
-    await acme_async_client.aclose()
+async def acme_client(acme_verifier: CreateTenantResponse):
+    async with get_tenant_client(token=acme_verifier.access_token) as acme_async_client:
+        yield acme_async_client

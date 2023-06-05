ee7d4a631544f5b0dd7e7ae574c060fcd2df520b
ff137 <ff137@proton.me>
2023-06-05 13:27:00 +0200

    add unstub mockito fixture

--- endorser/tests/conftest.py
+++ endorser/tests/conftest.py
@@ -1,3 +1,4 @@
+import mockito
 import pytest
 
 # pylint: disable=unused-import
@@ -7,3 +8,16 @@ from tests.fixtures import mock_agent_controller  # noqa: F401
 @pytest.fixture(scope="function")
 def anyio_backend():
     return "asyncio"
+
+
+@pytest.fixture(autouse=True)
+def unstub_mockito():
+    """
+    Automatically unstub all stubbed methods after each test.
+    """
+    # Setup phase: No setup needed in this fixture
+
+    yield
+
+    # Teardown phase: After each test, unstub all stubbed methods
+    mockito.unstub()

49ef86c3b5da04f7ab623f8d2c9ac462471aeee6
ff137 <ff137@proton.me>
2023-06-06 15:09:08 +0200

    add sse route for subscribing to desired state, regardless of other fields

--- webhooks/routers/sse.py
+++ webhooks/routers/sse.py
@@ -113,6 +113,43 @@ async def sse_subscribe(
 
 
 @router.get(
+    "/{wallet_id}/{topic}/{desired_state}",
+    response_class=EventSourceResponse,
+    summary="Wait for a desired state to be reached. "
+    "`desired_state` may be `offer-received`, `transaction-acked`, or `done`.",
+)
+@inject
+async def sse_subscribe_desired_state(
+    request: Request,
+    topic: str,
+    wallet_id: str,
+    desired_state: str,
+    sse_manager: SSEManager = Depends(get_sse_manager),
+):
+    async def event_stream(duration=150):
+        async with sse_manager.sse_event_stream(wallet_id, topic) as queue:
+            start_time = time.time()
+            while time.time() - start_time < duration:
+                # If client closes connection, stop sending events
+                if await request.is_disconnected():
+                    LOGGER.debug("SSE event_stream: client disconnected")
+                    break
+                try:
+                    event: TopicItem = queue.get_nowait()
+                    payload = dict(event.payload)  # to check if keys exist in payload
+                    if "state" in payload and payload["state"] == desired_state:
+                        yield event.json()  # Send the event
+                        break  # End the generator
+                except asyncio.QueueEmpty:
+                    await asyncio.sleep(0.2)
+                except asyncio.CancelledError:
+                    # This exception is thrown when the client disconnects.
+                    break
+
+    return EventSourceResponse(event_stream())
+
+
+@router.get(
     "/{wallet_id}/{topic}/{field}/{field_id}/{desired_state}",
     response_class=EventSourceResponse,
     summary="Wait for a desired state to be reached. "

581795f4fd4e596d3416ab836a120a14708e35b6
ff137 <ff137@proton.me>
2023-06-08 16:15:32 +0200

    set member fixtures to function scope

--- app/tests/util/member_async_clients.py
+++ app/tests/util/member_async_clients.py
@@ -14,31 +14,31 @@ async def governance_client():
         yield gov_async_client
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 async def tenant_admin_client():
     async with get_tenant_admin_client() as admin_async_client:
         yield admin_async_client
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 async def alice_member_client(alice_tenant: CreateTenantResponse):
     async with get_tenant_client(token=alice_tenant.access_token) as alice_async_client:
         yield alice_async_client
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 async def bob_member_client(bob_tenant: CreateTenantResponse):
     async with get_tenant_client(token=bob_tenant.access_token) as bob_async_client:
         yield bob_async_client
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 async def faber_client(faber_issuer: CreateTenantResponse):
     async with get_tenant_client(token=faber_issuer.access_token) as faber_async_client:
         yield faber_async_client
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 async def acme_client(acme_verifier: CreateTenantResponse):
     async with get_tenant_client(token=acme_verifier.access_token) as acme_async_client:
         yield acme_async_client

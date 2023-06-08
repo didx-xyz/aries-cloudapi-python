141db550fc3f1e2749282ee3f0be2eabb3c5117e
ff137 <ff137@proton.me>
2023-06-08 18:12:08 +0200

    increase default timeouts to 2min

--- app/event_handling/sse_listener.py
+++ app/event_handling/sse_listener.py
@@ -23,7 +23,7 @@ class SseListener:
         self.wallet_id = wallet_id
         self.topic = topic
 
-    async def wait_for_state(self, desired_state, timeout: int = 60):
+    async def wait_for_state(self, desired_state, timeout: int = 120):
         """
         Start listening for SSE events. When an event is received that matches the specified parameters.
         """
@@ -43,7 +43,7 @@ class SseListener:
 
         raise SseListenerTimeout("Event with request state was not returned by server")
 
-    async def wait_for_event(self, field, field_id, desired_state, timeout: int = 60):
+    async def wait_for_event(self, field, field_id, desired_state, timeout: int = 120):
         """
         Start listening for SSE events. When an event is received that matches the specified parameters.
         """

687ab2137788a9304046e683984c7855f37bee02
ff137 <ff137@proton.me>
2023-06-27 10:22:08 +0200

    move issuer models to correct module

--- app/generic/issuer/models.py
+++ app/generic/issuer/models.py
@@ -20,3 +20,50 @@ class Credential(BaseModel):
 class CredentialNoConnection(BaseModel):
     cred_def_id: str
     attributes: Dict[str, str]
+
+
+class IssueCredentialFacades(Enum):
+    v1 = IssuerV1
+    v2 = IssuerV2
+
+
+class ProblemReportExplanation(TypedDict):
+    description: str
+
+
+class CredentialBase(BaseModel):
+    protocol_version: IssueCredentialProtocolVersion
+    credential_definition_id: str
+    attributes: Dict[str, str]
+
+
+class RevokeCredential(BaseModel):
+    credential_definition_id: str = ""
+    auto_publish_on_ledger: Optional[bool] = False
+    credential_exchange_id: str = ""
+
+
+class SendCredential(CredentialBase):
+    connection_id: str
+
+
+class CreateOffer(CredentialBase):
+    pass
+
+
+def issuer_from_id(id: str) -> Issuer:
+    if id.startswith("v1-"):
+        return IssueCredentialFacades.v1.value
+
+    elif id.startswith("v2-"):
+        return IssueCredentialFacades.v2.value
+
+    raise CloudApiException(
+        "Unknown version. ID is expected to contain protocol version", 400
+    )
+
+
+def issuer_from_protocol_version(version: IssueCredentialProtocolVersion) -> Issuer:
+    facade = IssueCredentialFacades[version.name]
+
+    return facade.value

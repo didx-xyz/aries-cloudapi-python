75ef45a1efe327b3a9575974861d5c0cbebc688b
ff137 <ff137@proton.me>
2023-06-30 13:17:13 +0200

    :art: add info and exception logs

--- app/facades/webhooks.py
+++ app/facades/webhooks.py
@@ -9,10 +9,13 @@ logger = get_logger(__name__)
 
 
 def get_hooks_for_wallet_by_topic(wallet_id: str, topic: CloudApiTopics) -> List:
+    bound_logger = logger.bind(wallet_id=wallet_id, topic=topic)
+    bound_logger.info("Fetching webhooks events from /webhooks/wallet_id/topic")
     try:
         hooks = (get(f"{WEBHOOKS_URL}/webhooks/{wallet_id}/{topic}")).json()
         return hooks if hooks else []
     except HTTPError as e:
+        bound_logger.exception("HTTP Error caught when fetching webhooks.")
         raise e from e
 
 
@@ -20,8 +23,11 @@ def get_hooks_for_wallet(wallet_id: str) -> List:
     """
     Gets webhooks for wallet. Only return the first 100 hooks to not overload OpenAPI interface
     """
+    bound_logger = logger.bind(wallet_id=wallet_id)
+    bound_logger.info("Fetching webhooks events from /webhooks/wallet_id")
     try:
         hooks = (get(f"{WEBHOOKS_URL}/webhooks/{wallet_id}")).json()
         return hooks if hooks else []
     except HTTPError as e:
+        bound_logger.exception("HTTP Error caught when fetching webhooks.")
         raise e from e

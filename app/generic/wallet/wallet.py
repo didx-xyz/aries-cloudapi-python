9a588dc005287a4a95ee058a36a7db92f2801183
ff137 <ff137@proton.me>
2023-07-03 12:20:04 +0200

    :art: add info and debug logs

--- app/generic/wallet/wallet.py
+++ app/generic/wallet/wallet.py
@@ -19,10 +19,13 @@ async def create_did(
     auth: AcaPyAuth = Depends(acapy_auth),
 ):
     """Create Local DID."""
+    logger.info("POST request received: Create DID")
 
     async with client_from_auth(auth) as aries_controller:
+        logger.debug("Creating DID")
         result = await acapy_wallet.create_did(aries_controller)
 
+    logger.info("Successfully created DID.")
     return result
 
 
@@ -33,13 +36,17 @@ async def list_dids(
     """
     Retrieve list of DIDs.
     """
+    logger.info("GET request received: Retreive list of DIDs")
 
     async with client_from_auth(auth) as aries_controller:
+        logger.debug("Fetching DIDs")
         did_result = await aries_controller.wallet.get_dids()
 
     if not did_result.results:
+        logger.info("No DIDs returned.")
         return []
 
+    logger.info("Successfully fetched list of DIDs.")
     return did_result.results
 
 
@@ -50,12 +57,17 @@ async def get_public_did(
     """
     Fetch the current public DID.
     """
+    logger.info("GET request received: Fetch public DID")
+
     async with client_from_auth(auth) as aries_controller:
+        logger.debug("Fetching public DID")
         result = await aries_controller.wallet.get_public_did()
 
     if not result.result:
+        logger.info("Bad request: no public DID found.")
         raise CloudApiException("No public did found", 404)
 
+    logger.info("Successfully fetched public DID.")
     return result.result
 
 
@@ -65,10 +77,13 @@ async def set_public_did(
     auth: AcaPyAuth = Depends(acapy_auth),
 ):
     """Set the current public DID."""
+    logger.info("PUT request received: Set public DID")
 
     async with client_from_auth(auth) as aries_controller:
+        logger.debug("Setting public DID")
         result = await acapy_wallet.set_public_did(aries_controller, did)
 
+    logger.info("Successfully set public DID.")
     return result
 
 
@@ -77,8 +92,13 @@ async def rotate_keypair(
     did: str,
     auth: AcaPyAuth = Depends(acapy_auth),
 ):
+    bound_logger = logger.bind(body={"did": did})
+    bound_logger.info("PATCH request received: Rotate keypair for DID")
     async with client_from_auth(auth) as aries_controller:
+        bound_logger.debug("Rotating keypair")
         await aries_controller.wallet.rotate_keypair(did=did)
+
+    bound_logger.info("Successfully rotated keypair.")
 
 
 @router.get("/{did}/endpoint", response_model=DIDEndpoint)
@@ -87,9 +107,13 @@ async def get_did_endpoint(
     auth: AcaPyAuth = Depends(acapy_auth),
 ):
     """Get DID endpoint."""
+    bound_logger = logger.bind(body={"did": did})
+    bound_logger.info("GET request received: Get endpoint for DID")
     async with client_from_auth(auth) as aries_controller:
+        bound_logger.debug("Fetching DID endpoint")
         result = await aries_controller.wallet.get_did_endpoint(did=did)
 
+    bound_logger.info("Successfully fetched DID endpoint.")
     return result
 
 
@@ -102,11 +126,17 @@ async def set_did_endpoint(
     """Update Endpoint in wallet and on ledger if posted to it."""
 
     # "Endpoint" type is for making connections using public indy DIDs
+    bound_logger = logger.bind(body={"did": did, "body": body})
+    bound_logger.info("POST request received: Get endpoint for DID")
+
     endpoint_type = "Endpoint"
 
     async with client_from_auth(auth) as aries_controller:
+        bound_logger.debug("Setting DID endpoint")
         await aries_controller.wallet.set_did_endpoint(
             body=DIDEndpointWithType(
                 did=did, endpoint=body.endpoint, endpoint_type=endpoint_type
             )
         )
+
+    bound_logger.info("Successfully set DID endpoint.")

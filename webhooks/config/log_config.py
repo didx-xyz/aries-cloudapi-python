8e11453aedf649c52a27076795be086ee99adc93
ff137 <ff137@proton.me>
2023-07-04 11:05:26 +0200

    define custom logger for webhooks

---
+++ webhooks/config/log_config.py
@@ -0,0 +1,41 @@
+import os
+import sys
+
+from loguru import logger
+
+STDOUT_LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
+FILE_LOG_LEVEL = "DEBUG"
+
+# Remove default handler
+logger.remove()
+
+# Define custom formatter
+formatter = (
+    "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
+    "<level>{level: <8}</level> | "
+    "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> | "
+    "<level>{message}</level> - "
+    "{extra[body]}"
+)
+logger.configure(extra={"body": ""})  # Default values for extra args
+
+# Log to stdout
+logger.add(sys.stdout, level=STDOUT_LOG_LEVEL, format=formatter, colorize=True)
+
+# Log to a file
+logger.add(
+    "./logs/webhooks/{time}.log",
+    rotation="00:00",  # new file is created at midnight
+    retention="7 days",  # keep logs for up to 7 days
+    enqueue=True,  # asynchronous
+    level=FILE_LOG_LEVEL,
+    format=formatter,
+)
+
+# Configure email notifications
+# logger.add("smtp+ssl://username:password@host:port", level="CRITICAL")
+
+
+# Export this logger
+def get_logger(name: str):
+    return logger.bind(name=name)

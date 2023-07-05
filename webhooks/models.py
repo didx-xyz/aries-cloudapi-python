eed2769d9ac6029efeea0413f2cc09a622463e37
ff137 <ff137@proton.me>
2023-07-05 16:00:40 +0200

    :art: use pydantic model keys instead of typed dict keys

--- webhooks/models.py
+++ webhooks/models.py
@@ -21,12 +21,12 @@ from shared import (
 
 def to_endorsement_model(item: RedisItem) -> Endorsement:
     if item["payload"]["state"]:
-        item["payload"]["state"] = item["payload"]["state"].replace("_", "-")
-    return Endorsement(**item["payload"])
+        item.payload["state"] = item.payload["state"].replace("_", "-")
+    return Endorsement(**item.payload)
 
 
 def to_connections_model(item: RedisItem) -> Connection:
-    conn_record = ConnRecord(**item["payload"])
+    conn_record = ConnRecord(**item.payload)
     conn_record = conn_record_to_connection(connection_record=conn_record)
 
     return conn_record
@@ -34,15 +34,15 @@ def to_connections_model(item: RedisItem) -> Connection:
 
 def to_proof_hook_model(item: RedisItem) -> PresentationExchange:
     # v1
-    if item["acapy_topic"] == "present_proof":
-        presentation_exchange = V10PresentationExchange(**item["payload"])
+    if item.acapy_topic == "present_proof":
+        presentation_exchange = V10PresentationExchange(**item.payload)
         presentation_exchange = presentation_record_to_model(presentation_exchange)
     # v2
-    elif item["acapy_topic"] == "present_proof_v2_0":
-        presentation_exchange = V20PresExRecord(**item["payload"])
+    elif item.acapy_topic == "present_proof_v2_0":
+        presentation_exchange = V20PresExRecord(**item.payload)
         presentation_exchange = presentation_record_to_model(presentation_exchange)
     else:
-        topic = item["acapy_topic"]
+        topic = item.acapy_topic
         raise Exception(f"Unsupported proof acapy topic: {topic}")
 
     return presentation_exchange
@@ -50,15 +50,15 @@ def to_proof_hook_model(item: RedisItem) -> PresentationExchange:
 
 def to_credential_hook_model(item: RedisItem) -> CredentialExchange:
     # v1
-    if item["acapy_topic"] == "issue_credential":
-        cred_exchange = V10CredentialExchange(**item["payload"])
+    if item.acapy_topic == "issue_credential":
+        cred_exchange = V10CredentialExchange(**item.payload)
         cred_model = credential_record_to_model_v1(cred_exchange)
     # v2
-    elif item["acapy_topic"] == "issue_credential_v2_0":
-        cred_exchange = V20CredExRecord(**item["payload"])
+    elif item.acapy_topic == "issue_credential_v2_0":
+        cred_exchange = V20CredExRecord(**item.payload)
         cred_model = credential_record_to_model_v2(cred_exchange)
     else:
-        topic = item["acapy_topic"]
+        topic = item.acapy_topic
         raise Exception(f"Unsupported issue credential acapy topic: {topic}")
 
     return cred_model

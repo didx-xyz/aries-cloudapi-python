e0842f428322de3ce50ae58d049801640eab8608
ff137 <ff137@proton.me>
2023-07-05 15:57:17 +0200

    :art:

--- webhooks/dependencies/service.py
+++ webhooks/dependencies/service.py
@@ -1,7 +1,6 @@
 """Services module."""
 
-import json
-from typing import Any, List
+from typing import List, Optional
 
 from aioredis import Redis
 from aries_cloudcontroller.model import OobRecord
@@ -11,6 +10,7 @@ from shared import (
     BasicMessage,
     Connection,
     CredentialExchange,
+    Endorsement,
     PayloadType,
     PresentationExchange,
     RedisItem,
@@ -28,16 +28,8 @@ logger = get_logger(__name__)
 
 
 class Service:
-    def __init__(self, redis: Redis) -> None:
+    def __init__(self, redis: Redis):
         self._redis = redis
-
-    def _is_proof(self, topic: str) -> bool:
-        proofs_identifiers = ["present_proof", "present_proof_v2_0"]
-        return topic in proofs_identifiers
-
-    def _is_credential(self, topic: str) -> bool:
-        credential_identifiers = ["issue_credential", "issue_credential_v2_0"]
-        return topic in credential_identifiers
 
     def _proof_hook_versioned(self, item: RedisItem) -> PresentationExchange:
         return to_proof_hook_model(item=item)
@@ -48,45 +40,43 @@ class Service:
     def _version_connections(self, item: RedisItem) -> Connection:
         return to_connections_model(item=item)
 
-    def _basic_messages(self, item: RedisItem) -> BasicMessage:
-        return BasicMessage(**item["payload"])
-
-    def _endorsements(self, item: RedisItem):
+    def _endorsements(self, item: RedisItem) -> Endorsement:
         return to_endorsement_model(item=item)
 
-    def _oob(self, item: RedisItem):
-        return OobRecord(**item["payload"])
+    def _basic_messages(self, item: RedisItem) -> BasicMessage:
+        return BasicMessage(**item.payload)
+
+    def _oob(self, item: RedisItem) -> OobRecord:
+        return OobRecord(**item.payload)
 
     def _to_item(self, data: RedisItem):
         item = None
-
+        topic = data.topic
         # Transform the data to the appropriate model
-        if data["topic"] == "proofs":
+        if topic == "proofs":
             item = self._proof_hook_versioned(data)
-        elif data["topic"] == "credentials":
+        elif topic == "credentials":
             item = self._credential_hook_versioned(data)
-        elif data["topic"] == "connections":
+        elif topic == "connections":
             item = self._version_connections(data)
-        elif data["topic"] == "basic-messages":
+        elif topic == "basic-messages":
             item = self._basic_messages(data)
-        elif data["topic"] == "endorsements":
+        elif topic == "endorsements":
             item = self._endorsements(data)
-        elif data["topic"] == "oob":
+        elif topic == "oob":
             item = self._oob(data)
 
         return item
 
-    def _to_topic_item(
-        self, data: RedisItem, payload: PayloadType
-    ) -> TopicItem[PayloadType]:
-        return TopicItem[PayloadType](
-            topic=data["topic"],
-            wallet_id=data["wallet_id"],
-            origin=data["origin"],
+    def _to_topic_item(self, data: RedisItem, payload: PayloadType) -> TopicItem:
+        return TopicItem(
+            topic=data.topic,
+            wallet_id=data.wallet_id,
+            origin=data.origin,
             payload=payload,
         )
 
-    async def transform_topic_entry(self, data: RedisItem):
+    def transform_topic_entry(self, data: RedisItem) -> List[TopicItem]:
         """Transforms an entry from the redis cache into model."""
         payload = self._to_item(data=data)
 
@@ -116,35 +106,18 @@ class Service:
 
         return data_list
 
+    async def get_all_by_wallet(self, wallet_id: str) -> List[TopicItem]:
+        entries = await self._redis.smembers(wallet_id)
+        return self._transform_redis_entries(entries)
+
     async def get_all_for_topic_by_wallet_id(
         self, topic: str, wallet_id: str
-    ) -> List[TopicItem[Any]]:
-        data_list: List[TopicItem[Any]] = []
+    ) -> List[TopicItem]:
         entries = await self._redis.smembers(wallet_id)
+        return self._transform_redis_entries(entries, topic)
 
-        for entry in entries:
-            try:
-                data: RedisItem = json.loads(entry)
-                # Only take current topic
-                if data["topic"] != topic:
-                    continue
-
-                webhook = await self.transform_topic_entry(data)
-
-                if webhook:
-                    data_list.append(webhook)
-            # Log the data failing to create webhook, skip appending
-            # anything to the list, and continue to next item in entries
-            except (ValidationError, json.JSONDecodeError) as e:
-                logger.error("Error creating formatted webhook for\n%s\n%r", data, e)
-            # Catch the general case if sth else/unknown occurs:
-            except Exception as e:
-                logger.error("Unknown exception occurred:\n%r", e)
-
-        return data_list
-
-    async def add_wallet_entry(self, wallet_id: str, hook: str):
-        await self._redis.sadd(wallet_id, hook)
+    async def add_wallet_entry(self, wallet_id: str, event: str) -> None:
+        await self._redis.sadd(wallet_id, event)
 
     async def store_undelivered_message(self, topic: str, event: str) -> None:
         await self._redis.rpush(f"undelivered:{topic}", event)

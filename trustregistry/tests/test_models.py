57b7cba7ebf20f7cf3b8716c5c03379854682e34
cl0ete <cloete.dupreez@gmail.com>
2023-07-05 16:56:19 +0200

    moved model parsing tests to test_models

--- trustregistry/tests/test_models.py
+++ trustregistry/tests/test_models.py
@@ -1,7 +1,42 @@
+import pytest
+
+from trustregistry.schemas import Actor, Schema
 from trustregistry.models import DB_Actor, DB_Schema
 
-
 def test_actor():
+    actor = Actor(
+        id="mickey-mouse",
+        name="Mickey Mouse",
+        roles=["verifier", "issuer"],
+        didcomm_invitation="xyz",
+        did="did:key:abc",
+    )
+
+    assert actor.id == "mickey-mouse"
+    assert actor.name == "Mickey Mouse"
+    assert actor.roles == ["verifier", "issuer"]
+    assert actor.didcomm_invitation == "xyz"
+    assert actor.did == "did:key:abc"
+
+
+def test_schema():
+    schema = Schema(did="abc", name="doubleaceschema", version="0.4.20")
+
+    assert schema.did == "abc"
+    assert schema.name == "doubleaceschema"
+    assert schema.version == "0.4.20"
+    assert schema.id == "abc:2:doubleaceschema:0.4.20"
+
+    with pytest.raises(ValueError):
+        Schema(did="abc:def", name="doubleaceschema", version="0.4.20")
+
+    with pytest.raises(ValueError):
+        Schema(did="abc", name="double:ace:schema", version="0.4.20")
+
+    with pytest.raises(ValueError):
+        Schema(did="abc", name="doubleaceschema", version="0:4:20")
+
+def test_db_actor():
     actor = DB_Actor(
         id="mickey-mouse",
         name="Mickey Mouse",
@@ -17,7 +52,7 @@ def test_actor():
     assert actor.did == "abc"
 
 
-def test_schema():
+def test_db_schema():
     schema = DB_Schema(did="abc", name="doubleaceschema", version="0.4.20")
 
     assert schema.did == "abc"

ab0c231edddc38e4b2c690104b0f4e54c37365d6
ff137 <ff137@proton.me>
2023-07-06 11:01:30 +0200

    define method to create main logger once and export if already created

--- webhooks/config/log_config.py
+++ webhooks/config/log_config.py
@@ -35,24 +35,41 @@ def get_log_file_path() -> str:
     return os.path.join(LOG_DIR, "{time:YYYY-MM-DD}.log")
 
 
-
-# Log to stdout
-logger.add(sys.stdout, level=STDOUT_LOG_LEVEL, format=formatter, colorize=True)
-
-# Log to a file
-logger.add(
-    "./logs/webhooks/{time}.log",
-    rotation="00:00",  # new file is created at midnight
-    retention="7 days",  # keep logs for up to 7 days
-    enqueue=True,  # asynchronous
-    level=FILE_LOG_LEVEL,
-    format=formatter,
-)
-
-# Configure email notifications
-# logger.add("smtp+ssl://username:password@host:port", level="CRITICAL")
-
-
 # Export this logger
 def get_logger(name: str):
-    return logger.bind(name=name)
+    # Get the main module name
+    main_module_name = name.split(".")[0]
+
+    # Check if a logger for this name already exists
+    if main_module_name in loggers:
+        return loggers[main_module_name].bind(name=name)
+
+    # Remove default handler from global logger
+    logger.remove()
+
+    # Make a deep copy of the global logger
+    logger_ = copy.deepcopy(logger)
+
+    logger_.configure(extra={"body": ""})  # Default values for extra args
+
+    # Log to stdout
+    logger_.add(sys.stdout, level=STDOUT_LOG_LEVEL, format=formatter, colorize=True)
+
+    # Log to a file
+    logger_.add(
+        get_log_file_path(),
+        rotation="00:00",  # new file is created at midnight
+        retention="7 days",  # keep logs for up to 7 days
+        enqueue=True,  # asynchronous
+        level=FILE_LOG_LEVEL,
+        format=formatter,
+    )
+
+    # Configure email notifications
+    # logger_.add("smtp+ssl://username:password@host:port", level="CRITICAL")
+
+    # Store the logger in the dictionary
+    loggers[main_module_name] = logger_
+
+    # Return a logger bound with the full name including the submodule
+    return logger_.bind(name=name)

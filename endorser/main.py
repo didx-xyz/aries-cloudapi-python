2d0dcc8c511724decedd4dff81674268c5ba4bc5
ff137 <ff137@proton.me>
2023-07-06 13:01:31 +0200

    :art: override logger name (as __name__ provides  `__main__` due to how it is instantiated)

--- endorser/main.py
+++ endorser/main.py
@@ -3,7 +3,7 @@ import asyncio
 from endorser.endorser_processor import listen_endorsement_events
 from shared.log_config import get_logger
 
-logger = get_logger(__name__)
+logger = get_logger("endorser.main")  # override as __name__ gets passed as __main__
 
 if __name__ == "__main__":
     logger.info("Starting endorser service")

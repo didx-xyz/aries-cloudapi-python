7bfaf9ad9ae31ca6db59940d0bfdd124b31e97b7
cl0ete <cloete.dupreez@gmail.com>
2023-07-10 11:55:07 +0200

    update exception type

--- app/facades/trust_registry.py
+++ app/facades/trust_registry.py
@@ -253,7 +253,7 @@ async def registry_has_schema(schema_id: str) -> bool:
             bound_logger.debug("Fetch schema from trust registry")
             schema_res = await client.get(f"{TRUST_REGISTRY_URL}/registry/schemas/{schema_id}")
             schema_res.raise_for_status()
-    except httpx.HTTPError as http_err:
+    except httpx.HTTPStatusError as http_err:
         if http_err.status_code == 404:
             bound_logger.info("Schema id not registered in trust registry.")
             return False

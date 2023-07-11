e11f719ef5f848fd3734020bf111b4d2241340eb
cl0ete <cloete.dupreez@gmail.com>
2023-07-11 13:40:01 +0200

    renamed db to db_session

--- trustregistry/main.py
+++ trustregistry/main.py
@@ -43,15 +43,15 @@ async def startup_event():
 
 
 @app.get("/")
-async def root(db: Session = Depends(get_db)):
+async def root(db_session: Session = Depends(get_db)):
     logger.info("GET request received: Fetch actors and schemas from registry")
-    db_schemas = crud.get_schemas(db)
-    db_actors = crud.get_actors(db)
+    db_schemas = crud.get_schemas(db_session)
+    db_actors = crud.get_actors(db_session)
     schemas_repr = [schema.id for schema in db_schemas]
     logger.info("Successfully fetched actors and schemas from registry.")
     return {"actors": db_actors, "schemas": schemas_repr}
 
 
 @app.get("/registry")
-async def registry(db: Session = Depends(get_db)):
-    return await root(db)
+async def registry(db_session: Session = Depends(get_db)):
+    return await root(db_session)

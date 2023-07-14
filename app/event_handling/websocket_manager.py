df0e42e9d9d9437186a63f0a71d57de33a9c6457
ff137 <ff137@proton.me>
2023-07-14 11:07:26 +0200

    add debug logs

--- app/event_handling/websocket_manager.py
+++ app/event_handling/websocket_manager.py
@@ -45,11 +45,16 @@ class WebsocketManager:
             await websocket.send_text(data)
 
         client = PubSubClient()
+        logger.debug("Subscribing PubSubClient to {}", subscribed_topic)
+
         client.subscribe(subscribed_topic, callback)
         await WebsocketManager.start_pubsub_client(client)
 
         uuid = uuid4().hex
         WebsocketManager._clients[uuid] = client
+        logger.debug(
+            "Successfully started PubSubClient, listening to {}", subscribed_topic
+        )
 
         return uuid
 

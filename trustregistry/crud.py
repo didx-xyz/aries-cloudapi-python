21b18e2d3b9234e59fb26584df211e1699ac8d15
ff137 <ff137@proton.me>
2023-07-17 15:35:13 +0200

    :art: organise imports

--- trustregistry/crud.py
+++ trustregistry/crud.py
@@ -1,11 +1,11 @@
 from typing import List
 
-from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import Session
 
 from shared.log_config import get_logger
-from trustregistry import db
 from shared.models.trustregistry import Actor, Schema
+from trustregistry import db
 
 logger = get_logger(__name__)
 

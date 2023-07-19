4fed90d802fe78528750c463788db6f77d4dbf5b
cl0ete <cloete.dupreez@gmail.com>
2023-07-19 17:03:51 +0200

    declaritive_base moved to orm

--- trustregistry/database.py
+++ trustregistry/database.py
@@ -1,8 +1,7 @@
 import os
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 POSTGRES_DATABASE_URL = os.getenv(
     "POSTGRES_DATABASE_URL",

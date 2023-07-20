c4f04fb0377a403f38edf3657bc1c8cf3e9dcfeb
cl0ete <cloete.dupreez@gmail.com>
2023-07-20 14:09:59 +0200

    test get actor by name

--- trustregistry/tests/test_actor.py
+++ trustregistry/tests/test_actor.py
@@ -128,6 +128,21 @@ async def test_get_actor():
         )
 
         assert not_actor_response.status_code == 404
+
+        #test by name
+        response = await client.get(
+            f"{TRUST_REGISTRY_URL}/registry/actors/name/{actor_name}"
+        )
+
+        assert response.status_code == 200
+        assert response.json() == new_actor
+
+        not_actor_response = await client.get(
+            f"{TRUST_REGISTRY_URL}/registry/actors/not_a_actor"
+        )
+
+        assert not_actor_response.status_code == 404
+
 @pytest.mark.anyio
 async def test_update_actor():
     async with AsyncClient() as client:

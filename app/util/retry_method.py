cccb06f87d537abfbb326fcb1fe148d91ef04a75
ff137 <ff137@proton.me>
2023-07-26 15:28:00 +0200

    :sparkles: method to await coroutine with retry logic

---
+++ app/util/retry_method.py
@@ -0,0 +1,22 @@
+import asyncio
+from typing import Awaitable
+
+from loguru import Logger
+
+
+async def coroutine_with_retry(
+    coroutine: Awaitable, logger: Logger, max_attempts=5, retry_delay=0.5
+):
+    for attempt in range(max_attempts):
+        try:
+            await coroutine
+            break
+        except Exception as e:
+            if attempt + 1 == max_attempts:
+                logger.error("Maximum number of retries exceeded. Failing.")
+                raise e  # Re-raise the exception if max attempts exceeded
+
+            logger.warning(
+                f"Failed to run coroutine (attempt {attempt + 1}). Retrying in {retry_delay} seconds..."
+            )
+            await asyncio.sleep(retry_delay)

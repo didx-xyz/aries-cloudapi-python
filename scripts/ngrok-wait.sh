66e247bb066ed788cc219a6b60928284a8d8971f
cl0ete <cloete.dupreez@gmail.com>
2023-08-03 16:52:22 +0200

    add double quote to $NGROK_NAME

--- scripts/ngrok-wait.sh
+++ scripts/ngrok-wait.sh
@@ -8,7 +8,7 @@ AGENT_ENDPOINT=null
 while [ -z "$AGENT_ENDPOINT" ] || [ "$AGENT_ENDPOINT" = "null" ]
 do
     echo "Fetching end point from ngrok service"
-    AGENT_ENDPOINT=$(curl --silent $NGROK_NAME:4040/api/tunnels | ./jq -r '.tunnels[0].public_url')
+    AGENT_ENDPOINT=$(curl --silent "$NGROK_NAME":4040/api/tunnels | ./jq -r '.tunnels[0].public_url')
     echo "ngrok end point [$AGENT_ENDPOINT]"
     if [ -z "$AGENT_ENDPOINT" ] || [ "$AGENT_ENDPOINT" = "null" ]; then
         echo "ngrok not ready, sleeping 5 seconds...."

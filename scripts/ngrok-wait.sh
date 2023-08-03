af4dfe4c56443d90f433a012702d281a2bbc274e
cl0ete <cloete.dupreez@gmail.com>
2023-08-03 16:52:34 +0200

    :art:

--- scripts/ngrok-wait.sh
+++ scripts/ngrok-wait.sh
@@ -5,8 +5,7 @@ NGROK_NAME=${NGROK_NAME:-ngrok}
 echo "ngrok end point [$NGROK_NAME]"
 
 AGENT_ENDPOINT=null
-while [ -z "$AGENT_ENDPOINT" ] || [ "$AGENT_ENDPOINT" = "null" ]
-do
+while [ -z "$AGENT_ENDPOINT" ] || [ "$AGENT_ENDPOINT" = "null" ]; do
     echo "Fetching end point from ngrok service"
     AGENT_ENDPOINT=$(curl --silent "$NGROK_NAME":4040/api/tunnels | ./jq -r '.tunnels[0].public_url')
     echo "ngrok end point [$AGENT_ENDPOINT]"

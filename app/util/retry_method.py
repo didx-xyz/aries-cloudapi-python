32902014010ab4faf1f4c26dae8a8ba8649e5ce4
ff137 <ff137@proton.me>
2023-08-07 16:00:49 +0200

    increase default retry duration to 2 seconds

--- app/util/retry_method.py
+++ app/util/retry_method.py
@@ -4,7 +4,7 @@ from typing import Any, Callable, Tuple
 
 
 async def coroutine_with_retry(
-    coroutine_func: Callable, args: Tuple, logger: Logger, max_attempts=5, retry_delay=1
+    coroutine_func: Callable, args: Tuple, logger: Logger, max_attempts=5, retry_delay=2
 ) -> Any:
     result = None
     for attempt in range(max_attempts):

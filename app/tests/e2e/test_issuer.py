9d8c3dbbf7697fe0b547af2550a22b96375d7802
cl0ete <cloete.dupreez@gmail.com>
2023-08-10 15:54:19 +0200

    :art:

--- app/tests/e2e/test_issuer.py
+++ app/tests/e2e/test_issuer.py
@@ -378,7 +378,7 @@ async def test_revoke_credential(
 
     alice_credential_id = payload["credential_id"]
 
-    # send credential request - holder
+    # send credential request: holder
     response = await alice_member_client.post(
         f"{CREDENTIALS_BASE_PATH}/{alice_credential_id}/request", json={}
     )

5e47ef53d7ae0cd85be2abba853d45539cece6b1
ff137 <ff137@proton.me>
2023-08-10 15:54:42 +0200

    :art: remove custom JsonLdCredential

--- app/models/issuer.py
+++ app/models/issuer.py
@@ -1,8 +1,7 @@
 from enum import Enum
 from typing import Dict, Optional
 
-from aries_cloudcontroller import Credential as AcaCredential
-from aries_cloudcontroller import LDProofVCDetail, LDProofVCDetailOptions
+from aries_cloudcontroller import LDProofVCDetail
 from pydantic import BaseModel
 
 from shared.models.protocol import IssueCredentialProtocolVersion
@@ -45,9 +44,3 @@ class RevokeCredential(BaseModel):
     credential_exchange_id: str
     credential_definition_id: Optional[str] = None
     auto_publish_on_ledger: Optional[bool] = False
-
-
-class JsonLdCredential(BaseModel):
-    connection_id: str
-    credential: AcaCredential
-    options: LDProofVCDetailOptions

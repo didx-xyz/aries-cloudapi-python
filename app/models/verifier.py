53e9427867feed05c449c52f98edd6432eb96d98
ff137 <ff137@proton.me>
2023-08-14 16:44:08 +0200

    :art: refactor models with ProofRequestType

--- app/models/verifier.py
+++ app/models/verifier.py
@@ -1,17 +1,33 @@
+from enum import Enum
 from typing import Optional
 
-from aries_cloudcontroller import IndyPresSpec, IndyProofRequest
+from aries_cloudcontroller import DIFProofRequest, IndyPresSpec, IndyProofRequest
 from pydantic import BaseModel
 
 from shared.models.protocol import PresentProofProtocolVersion
 
 
-class CreateProofRequest(BaseModel):
-    proof_request: IndyProofRequest
+class ProofRequestType(Enum):
+    INDY = "indy"
+    JWT = "jwt"
+    LD_PROOF = "ld_proof"
+
+
+class ProofRequestBase(BaseModel):
+    type: ProofRequestType = ProofRequestType.INDY
+    indy_proof_request: Optional[IndyProofRequest]
+    ld_proof_request: Optional[DIFProofRequest]
+
+
+class ProofRequestMetadata(BaseModel):
+    protocol_version: PresentProofProtocolVersion
     auto_verify: Optional[bool] = None
     comment: Optional[str] = None
     trace: Optional[bool] = None
-    protocol_version: PresentProofProtocolVersion
+
+
+class CreateProofRequest(ProofRequestBase, ProofRequestMetadata):
+    pass
 
 
 class SendProofRequest(CreateProofRequest):

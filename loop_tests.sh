49fbbba61a2d433dc9d794ca4300e7a7e9b939e3
cl0ete <cloete.dupreez@gmail.com>
2023-08-16 16:46:17 +0200

    make sure passed var is pos int

--- loop_tests.sh
+++ loop_tests.sh
@@ -4,6 +4,16 @@ nr=1
 # Use the provided loop count argument or set a default value of 100
 if [ $# -eq 0 ]; then
     loop_count=100
+else
+    loop_count=$1
+
+    # Check if the input is a valid positive integer
+    re='^[0-9]+$'
+    if ! [[ $loop_count =~ $re ]]; then
+        echo "Error: Please enter a valid positive integer."
+        exit 1
+    fi
+fi
     ./manage tests
     ./manage down
     ((nr=nr+1))

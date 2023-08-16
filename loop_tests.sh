8f3d94463d1d33dace638a4af9504c3b28683be3
cl0ete <cloete.dupreez@gmail.com>
2023-08-16 16:45:23 +0200

    if no var passed default is 100

--- loop_tests.sh
+++ loop_tests.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 nr=1
-while true; do
-    echo "this is run ${nr}"
+
+# Use the provided loop count argument or set a default value of 100
+if [ $# -eq 0 ]; then
+    loop_count=100
     ./manage tests
     ./manage down
     ((nr=nr+1))

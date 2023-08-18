41aeb27c38193fbc1bc59d08b0ed4fa7e01e34e0
cl0ete <cloete.dupreez@gmail.com>
2023-08-18 16:32:21 +0200

    assert credentail data

--- app/tests/e2e/test_issuer/test_issuer_v2_indy.py
+++ app/tests/e2e/test_issuer/test_issuer_v2_indy.py
@@ -110,3 +110,9 @@ async def test_send_credential(
         json=credential,
     )
 
+    data = response.json()
+    assert_that(data).contains("credential_id")
+    assert_that(data).has_state("offer-sent")
+    assert_that(data).has_protocol_version("v2")
+    assert_that(data).has_attributes({"speed": "10"})
+    assert_that(data).has_schema_id(schema_definition.id)

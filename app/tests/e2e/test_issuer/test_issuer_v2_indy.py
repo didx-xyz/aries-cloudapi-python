b1268f820240227243169ba7bb0fde6eea7cc2d3
cl0ete <cloete.dupreez@gmail.com>
2023-08-18 16:27:32 +0200

    assert offer data

--- app/tests/e2e/test_issuer/test_issuer_v2_indy.py
+++ app/tests/e2e/test_issuer/test_issuer_v2_indy.py
@@ -37,3 +37,10 @@ async def test_send_credential_oob_v2(
         CREDENTIALS_BASE_PATH + "/create-offer",
         json=credential,
     )
+
+    data = create_offer_response.json()
+    assert_that(data).contains("credential_id")
+    assert_that(data).has_state("offer-sent")
+    assert_that(data).has_protocol_version("v2")
+    assert_that(data).has_attributes({"speed": "10"})
+    assert_that(data).has_schema_id(schema_definition.id)

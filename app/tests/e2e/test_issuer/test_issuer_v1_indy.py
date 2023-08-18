40aec0b9f39010ceb59baa09aec3b55326a8e195
cl0ete <cloete.dupreez@gmail.com>
2023-08-18 16:20:01 +0200

    define credential

--- app/tests/e2e/test_issuer/test_issuer_v1_indy.py
+++ app/tests/e2e/test_issuer/test_issuer_v1_indy.py
@@ -241,3 +241,11 @@ async def test_revoke_credential(
 ):
     faber_connection_id = faber_and_alice_connection.faber_connection_id
 
+    credential = {
+        "protocol_version": "v1",
+        "connection_id": faber_connection_id,
+        "indy_credential_detail": {
+            "credential_definition_id": credential_definition_id_revocable,
+            "attributes": {"speed": "10"},
+        },
+    }

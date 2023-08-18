97d798f7092c46ae3f47731140aff81e785f4932
cl0ete <cloete.dupreez@gmail.com>
2023-08-18 16:02:47 +0200

    assert alice has no issued credentials

--- app/tests/e2e/test_issuer/test_issuer_v1_indy.py
+++ app/tests/e2e/test_issuer/test_issuer_v1_indy.py
@@ -30,3 +30,12 @@ async def test_send_credential_oob_v1(
             "attributes": {"speed": "10"},
         },
     }
+
+    response = await alice_member_client.get(
+        CREDENTIALS_BASE_PATH,
+        params={"connection_id": faber_and_alice_connection.alice_connection_id},
+    )
+    records = response.json()
+
+    # nothing currently in alice's records
+    assert len(records) == 0

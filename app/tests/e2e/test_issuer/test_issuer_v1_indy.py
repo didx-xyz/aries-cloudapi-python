0ab609482533bd885f73a2e9571d297d0f6b0944
cl0ete <cloete.dupreez@gmail.com>
2023-08-18 16:01:08 +0200

    routes

--- app/tests/e2e/test_issuer/test_issuer_v1_indy.py
+++ app/tests/e2e/test_issuer/test_issuer_v1_indy.py
@@ -11,3 +11,6 @@ from app.tests.util.ecosystem_connections import FaberAliceConnect
 from app.tests.util.webhooks import check_webhook_state, get_wallet_id_from_async_client
 from app.util.credentials import cred_id_no_version
 from shared import RichAsyncClient
+
+CREDENTIALS_BASE_PATH = issuer_router.prefix
+OOB_BASE_PATH = oob_router.prefix

e3f3634b0ff97bc439a474c23252f82b86755b52
cl0ete <cloete.dupreez@gmail.com>
2023-08-22 15:23:22 +0200

    test send jsonld with did:sov

--- app/tests/e2e/test_issuer/test_issuer_ld_ed25519.py
+++ app/tests/e2e/test_issuer/test_issuer_ld_ed25519.py
@@ -43,6 +43,13 @@ credential = {
         },
     }
 
+@pytest.mark.anyio
+async def test_send_jsonld_credential_sov(
+    faber_client: RichAsyncClient,
+    faber_acapy_client: AcaPyClient,
+    faber_and_alice_connection: FaberAliceConnect,
+    alice_member_client: RichAsyncClient,
+):
     # Send credential
     response = await faber_client.post(
         CREDENTIALS_BASE_PATH,

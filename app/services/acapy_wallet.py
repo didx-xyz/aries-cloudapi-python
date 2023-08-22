5a62b293684314fbb81b427613064b2a8c44feb3
cl0ete <cloete.dupreez@gmail.com>
2023-08-22 11:24:44 +0200

    add optional typing to create did func

--- app/services/acapy_wallet.py
+++ app/services/acapy_wallet.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from aries_cloudcontroller import AcaPyClient
 from aries_cloudcontroller.model.did import DID
 from aries_cloudcontroller.model.did_create import DIDCreate

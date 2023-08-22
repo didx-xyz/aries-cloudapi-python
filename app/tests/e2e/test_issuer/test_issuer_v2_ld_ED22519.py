dec70ad3b09a4de09b35784920f8b2f760d30a45
cl0ete <cloete.dupreez@gmail.com>
2023-08-22 11:33:53 +0200

    add wallet router needed to make did:key

--- app/tests/e2e/test_issuer/test_issuer_v2_ld_ED22519.py
+++ app/tests/e2e/test_issuer/test_issuer_v2_ld_ED22519.py
@@ -7,6 +7,7 @@ from app.models.tenants import CreateTenantResponse
 from app.routes.definitions import CredentialSchema
 from app.routes.issuer import router as issuer_router
 from app.routes.oob import router as oob_router
+from app.routes.wallet import router as wallet_router
 from app.tests.util.ecosystem_connections import FaberAliceConnect
 from app.tests.util.webhooks import check_webhook_state, get_wallet_id_from_async_client
 from app.util.credentials import cred_id_no_version
@@ -14,6 +15,7 @@ from shared import RichAsyncClient
 
 CREDENTIALS_BASE_PATH = issuer_router.prefix
 OOB_BASE_PATH = oob_router.prefix
+WALLET = wallet_router.prefix
 
 
 @pytest.mark.anyio

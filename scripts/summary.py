c75bb5d76feec62b9e81f734acca95b3a6e11364
cl0ete <cloete.dupreez@gmail.com>
2023-08-23 15:01:36 +0200

    read files one at a time

--- scripts/summary.py
+++ scripts/summary.py
@@ -7,3 +7,6 @@ xml_files = [filename for filename in os.listdir(dir) if filename.endswith(".xml
 failures = 0
 errors = 0
 
+for xml_file in xml_files:
+    file_path = os.path.join(dir, xml_file)
+    with open(file_path,"r") as file_read:

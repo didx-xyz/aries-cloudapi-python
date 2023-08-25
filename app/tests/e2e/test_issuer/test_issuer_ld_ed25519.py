39a4e4e854e39cb68b9f8b39593eed0777c753ec
cl0ete <cloete.dupreez@gmail.com>
2023-08-25 15:57:33 +0200

    remove unused fixtures

--- app/tests/e2e/test_issuer/test_issuer_ld_ed25519.py
+++ app/tests/e2e/test_issuer/test_issuer_ld_ed25519.py
@@ -200,9 +200,7 @@ async def test_send_jsonld_key_ed25519(
 @pytest.mark.anyio
 async def test_send_jsonld_mismatch_ed_bbs(
     faber_client: RichAsyncClient,
-    faber_acapy_client: AcaPyClient,
     faber_and_alice_connection: FaberAliceConnect,
-    alice_member_client: RichAsyncClient,
 ):
     faber_connection_id = faber_and_alice_connection.faber_connection_id
 

37abec87a0e64601b354132defd73f20e5b1e765
cl0ete <cloete.dupreez@gmail.com>
2023-08-29 11:22:56 +0200

    add to did to trust registry

--- app/tests/e2e/test_issuer/test_issuer_ld_ed25519.py
+++ app/tests/e2e/test_issuer/test_issuer_ld_ed25519.py
@@ -143,10 +143,7 @@ async def test_send_jsonld_key_ed25519(
     alice_connection_id = faber_and_alice_connection.alice_connection_id
     faber_connection_id = faber_and_alice_connection.faber_connection_id
 
-    did_create_options = {"method": "key", "options": {"key_type": "ed25519"}}
-    wallet_response = (await faber_client.post(WALLET, json=did_create_options)).json()
-    did = wallet_response["did"]
-    faber_key_id = await register_key_issuer(did=did)
+    did = register_key_issuer
 
     # Updating JSON-LD credential did:key with proofType ed25519
     credential["connection_id"] = faber_connection_id
@@ -202,10 +199,7 @@ async def test_send_jsonld_mismatch_ed_bbs(
 ):
     faber_connection_id = faber_and_alice_connection.faber_connection_id
 
-    did_create_options = {"method": "key", "options": {"key_type": "ed25519"}}
-    wallet_response = (await faber_client.post(WALLET, json=did_create_options)).json()
-    did = wallet_response["did"]
-    faber_key_id = await register_key_issuer(did=did)
+    did = register_key_issuer
 
     # Creating JSON-LD credential did:sov with proofType: BbsBlsSignature2020
     credential["connection_id"] = faber_connection_id
@@ -308,10 +302,7 @@ async def test_send_jsonld_request(
     alice_connection_id = faber_and_alice_connection.alice_connection_id
     faber_connection_id = faber_and_alice_connection.faber_connection_id
 
-    did_create_options = {"method": "key", "options": {"key_type": "ed25519"}}
-    wallet_response = (await faber_client.post(WALLET, json=did_create_options)).json()
-    did = wallet_response["did"]
-    faber_key_id = await register_key_issuer(did=did)
+    did = register_key_issuer
 
     # Updating JSON-LD credential did:key with proofType ed25519
     credential["connection_id"] = faber_connection_id

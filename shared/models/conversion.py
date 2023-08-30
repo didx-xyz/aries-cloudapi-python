7362f829280fde018ec1c3954882932e5f8fe77c
cl0ete <cloete.dupreez@gmail.com>
2023-08-30 13:22:04 +0200

    type is now required, set to default value

--- shared/models/conversion.py
+++ shared/models/conversion.py
@@ -198,7 +198,7 @@ def credential_record_to_model_v2(record: V20CredExRecord) -> CredentialExchange
         state=record.state,
         thread_id=record.thread_id,
         updated_at=record.updated_at,
-        type=list(record.by_format.cred_offer.keys())[0] if record.by_format else None,
+        type=list(record.by_format.cred_offer.keys())[0] if record.by_format else "indy",
     )
 
 

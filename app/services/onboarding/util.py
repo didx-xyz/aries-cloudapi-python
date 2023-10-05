8a9caffe3a4304c4ebb55850ab934f1f49022e93
ff137 <ff137@proton.me>
2023-10-05 20:39:59 +0300

    :truck::art: move and rename util module. refactor with logging

--- app/services/onboarding/util.py
+++ app/services/onboarding/util.py
@@ -1,4 +1,5 @@
 import asyncio
+from logging import Logger
 from typing import Callable
 
 from aiohttp import ClientResponseError
@@ -6,7 +7,7 @@ from aries_cloudcontroller import AcaPyClient
 
 from app.exceptions.cloud_api_error import CloudApiException
 
-DEFAULT_RETRIES = 10
+DEFAULT_NUM_TRIES = 10
 DEFAULT_DELAY = 0.2
 
 
@@ -14,7 +15,8 @@ async def assert_metadata_set(
     controller: AcaPyClient,
     conn_id: str,
     check_fn: Callable,
-    retries=DEFAULT_RETRIES,
+    logger: Logger,
+    num_tries=DEFAULT_NUM_TRIES,
     delay=DEFAULT_DELAY,
 ):
     """Checks if connection record metadata has been set according to a custom check function.
@@ -23,42 +25,51 @@ async def assert_metadata_set(
         controller: The AcaPyClient instance for the respective agent
         conn_id: Connection id of the connection you're interested in
         check_fn: A function that takes the metadata and returns True if it meets the desired condition
-        retries: Number of retries before failing
+        logger: A logger instance
+        num_tries: Number of num_tries before failing
         delay: Delay in seconds between each retry
 
     Returns:
         True if condition is met, raises an exception otherwise.
     """
-    for _ in range(retries):
+    for _ in range(num_tries):
         # Delay is placed at the start to avoid race condition in ACA-Py, where reading metadata causes duplicate
         # record error if metadata is still due to be updated
+        logger.debug(f"Sleep {delay}s before trying to fetch metadata")
         await asyncio.sleep(delay)
         try:
+            logger.debug("Fetching connection metadata")
             connection_metadata = await controller.connection.get_metadata(
                 conn_id=conn_id
             )
+            logger.debug("Successfully fetched metadata")
             metadata_dict = connection_metadata.results
             if check_fn(metadata_dict):
                 return True
-        except ClientResponseError:
-            # A duplicate record error (aries_cloudagent.storage.error.StorageDuplicateError) may occur in ACA-Py
-            # if we fetch metadata while it's being updated
+        except ClientResponseError as e:
+            logger.error("Exception occurred when getting metadata: {}", e)
             pass
 
     raise CloudApiException(
-        f"Failed to assert that metadata meets the desired condition after {retries} attempts."
+        f"Failed to assert that metadata meets the desired condition after {num_tries} attempts."
     )
 
 
 async def assert_endorser_role_set(
-    controller, conn_id, retries=DEFAULT_RETRIES, delay=DEFAULT_DELAY
+    controller: AcaPyClient,
+    conn_id: str,
+    logger: Logger,
+    num_tries=1,
+    delay=DEFAULT_DELAY,
 ):
     check_fn = (
         lambda metadata: metadata.get("transaction_jobs", {}).get("transaction_my_job")
         == "TRANSACTION_ENDORSER"
     )
     try:
-        await assert_metadata_set(controller, conn_id, check_fn, retries, delay)
+        await assert_metadata_set(
+            controller, conn_id, check_fn, logger, num_tries, delay
+        )
     except Exception as e:
         raise CloudApiException(
             "Failed to assert that the endorser role has been set in the connection metadata."
@@ -66,7 +77,11 @@ async def assert_endorser_role_set(
 
 
 async def assert_author_role_set(
-    controller, conn_id, retries=DEFAULT_RETRIES, delay=DEFAULT_DELAY
+    controller: AcaPyClient,
+    conn_id: str,
+    logger: Logger,
+    num_tries=1,
+    delay=DEFAULT_DELAY,
 ):
     check_fn = (
         lambda metadata: metadata.get("transaction_jobs", {}).get("transaction_my_job")
@@ -75,7 +90,9 @@ async def assert_author_role_set(
         == "TRANSACTION_ENDORSER"
     )
     try:
-        await assert_metadata_set(controller, conn_id, check_fn, retries, delay)
+        await assert_metadata_set(
+            controller, conn_id, check_fn, logger, num_tries, delay
+        )
     except Exception as e:
         raise CloudApiException(
             "Failed to assert that the author role has been set in the connection metadata."
@@ -83,7 +100,12 @@ async def assert_author_role_set(
 
 
 async def assert_endorser_info_set(
-    controller, conn_id, endorser_did, retries=DEFAULT_RETRIES, delay=DEFAULT_DELAY
+    controller: AcaPyClient,
+    conn_id: str,
+    endorser_did: str,
+    logger: Logger,
+    num_tries=1,
+    delay=DEFAULT_DELAY,
 ):
     check_fn = (
         lambda metadata: metadata.get("transaction_jobs", {}).get("transaction_my_job")
@@ -93,7 +115,9 @@ async def assert_endorser_info_set(
         and metadata.get("endorser_info", {}).get("endorser_did") == endorser_did
     )
     try:
-        await assert_metadata_set(controller, conn_id, check_fn, retries, delay)
+        await assert_metadata_set(
+            controller, conn_id, check_fn, logger, num_tries, delay
+        )
     except Exception as e:
         raise CloudApiException(
             "Failed to assert that the endorser info has been set in the connection metadata."

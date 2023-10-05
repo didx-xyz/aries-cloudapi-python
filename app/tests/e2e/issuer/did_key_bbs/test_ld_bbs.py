da9bad78b2084b15f3b6c8e0942ae612254db69e
cl0ete <cloete.dupreez@gmail.com>
2023-10-05 10:46:04 +0200

    :art:

--- app/tests/e2e/issuer/did_key_bbs/test_ld_bbs.py
+++ app/tests/e2e/issuer/did_key_bbs/test_ld_bbs.py
@@ -148,7 +148,7 @@ async def test_send_jsonld_bbs_oob(
     )
 
     oob_record = accept_response.json()
-    alice_connection_id = oob_record['connection_id']
+    alice_connection_id = oob_record["connection_id"]
 
     assert await check_webhook_state(
         client=alice_member_client,
@@ -158,19 +158,16 @@ async def test_send_jsonld_bbs_oob(
             "connection_id": alice_connection_id,
         },
     )
-    
+
     assert_that(accept_response.status_code).is_equal_to(200)
     assert_that(oob_record).contains("created_at", "oob_id", "invitation")
 
-    faber_con = await faber_client.get(
-        CON
-    )
+    faber_con = await faber_client.get(CON)
 
     faber_connections = faber_con.json()
     for con in faber_connections:
-
-        if con['invitation_msg_id'] == invitation['@id']:
-            faber_connection_id = con['connection_id']
+        if con["invitation_msg_id"] == invitation["@id"]:
+            faber_connection_id = con["connection_id"]
 
     # Updating JSON-LD credential did:key (bbs)
     credential = deepcopy(credential_)

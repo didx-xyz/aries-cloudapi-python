0c35ddcde304a7e1fd07d2faaaa4b8f79624407c
ff137 <ff137@proton.me>
2023-10-06 12:21:25 +0300

    :bug::art: remove usage of retry + assert logic and opt for simpler solution: 1 try with longer delays. Mention "known bug" in exception messages.

--- app/services/onboarding/util/set_endorser_metadata.py
+++ app/services/onboarding/util/set_endorser_metadata.py
@@ -7,95 +7,48 @@ from aries_cloudcontroller import AcaPyClient
 
 from app.exceptions.cloud_api_error import CloudApiException
 
-DEFAULT_NUM_TRIES = 5
+DEFAULT_NUM_TRIES = 1
 DEFAULT_DELAY = 0.2
 
 
 async def set_endorser_role(
     endorser_controller: AcaPyClient, endorser_connection_id: str, logger: Logger
 ):
-    delay = DEFAULT_DELAY
-    for n in range(DEFAULT_NUM_TRIES):
-        try:
-            logger.debug(
-                f"Setting roles for endorser on endorser-issuer connection. Try: {n}"
-            )
-            await endorser_controller.endorse_transaction.set_endorser_role(
-                conn_id=endorser_connection_id,
-                transaction_my_job="TRANSACTION_ENDORSER",
-            )
-
-            # Try assert that it's done. Checking too soon may raise ACA-Py error (bug).
-            # So if it fails, retry with backing-off delay
-            logger.debug("Assert that the endorser role is set before continuing")
-            await assert_endorser_role_set(
-                endorser_controller,
-                endorser_connection_id,
-                logger,
-                num_tries=1,
-                delay=delay,
-            )
-            logger.debug(f"Successfully set endorser role on try: {n}")
-            return True  # success, exit retries
-        except (SettingMetadataException, ClientResponseError) as e:
-            if n == DEFAULT_NUM_TRIES:
-                logger.error(
-                    "Failed to set endorser role after {} retries.", DEFAULT_NUM_TRIES
-                )
-                raise CloudApiException(
-                    "Failed to set the endorser role in the endorser-issuer connection, "
-                    f"with connection id {endorser_connection_id}."
-                ) from e
-        logger.warning(
-            f"Setting endorser role has failed on try {n} with delay {delay}s"
+    try:
+        logger.debug("Setting roles for endorser on endorser-issuer connection.")
+        await endorser_controller.endorse_transaction.set_endorser_role(
+            conn_id=endorser_connection_id,
+            transaction_my_job="TRANSACTION_ENDORSER",
         )
-        await asyncio.sleep(delay)  # Secondary delay. Primary occurs in assert_ method
-
-        delay *= 2
-        logger.info(f"Retry setting of endorser role with increased delay: {delay}s")
+        logger.debug("Successfully set endorser role.")
+        await asyncio.sleep(DEFAULT_DELAY)  # Allow ACA-Py records to update
+    except ClientResponseError as e:
+        logger.error("Failed to set endorser role: {}.", e)
+        raise CloudApiException(
+            "Failed to set the endorser role in the endorser-issuer connection, "
+            f"with connection id {endorser_connection_id}. "
+            "This is a known bug in ACA-Py. Please retry."
+        ) from e
 
 
 async def set_author_role(
     issuer_controller: AcaPyClient, issuer_connection_id: str, logger: Logger
 ):
-    delay = DEFAULT_DELAY
-    for n in range(DEFAULT_NUM_TRIES):
-        try:
-            logger.debug(
-                f"Setting roles for author on issuer-endorser connection. Try: {n}"
-            )
-            await issuer_controller.endorse_transaction.set_endorser_role(
-                conn_id=issuer_connection_id,
-                transaction_my_job="TRANSACTION_AUTHOR",
-            )
-
-            # Try assert that it's done. Checking too soon may raise ACA-Py error (bug).
-            # So if it fails, retry with backing-off delay
-            logger.debug("Assert that the author role is set before continuing")
-
-            await assert_author_role_set(
-                issuer_controller,
-                issuer_connection_id,
-                logger,
-                num_tries=1,
-                delay=delay,
-            )
-            logger.debug(f"Successfully set author role on try: {n}")
-            return True  # success, exit retries
-        except (SettingMetadataException, ClientResponseError) as e:
-            if n == DEFAULT_NUM_TRIES:
-                logger.error(
-                    "Failed to set author role after {} retries.", DEFAULT_NUM_TRIES
-                )
-                raise CloudApiException(
-                    "Failed to set the author role in the issuer-endorser connection, "
-                    f"with connection id {issuer_connection_id}."
-                ) from e
-        logger.warning(f"Setting author role has failed on try {n} with delay {delay}s")
-        await asyncio.sleep(delay)  # Secondary delay. Primary occurs in assert_ method
-
-        delay *= 2
-        logger.info(f"Retry setting of author role with increased delay: {delay}s")
+    try:
+        logger.debug("Setting roles for author on issuer-endorser connection")
+        await issuer_controller.endorse_transaction.set_endorser_role(
+            conn_id=issuer_connection_id,
+            transaction_my_job="TRANSACTION_AUTHOR",
+        )
+        logger.debug("Successfully set author role.")
+        await asyncio.sleep(DEFAULT_DELAY)  # Allow ACA-Py records to update
+    except ClientResponseError as e:
+        logger.error("Failed to set author role: {}.", e)
+        raise CloudApiException(
+            "Failed to set the author role in the issuer-endorser connection, "
+            f"with connection id {issuer_connection_id}. "
+            "This is a known bug in ACA-Py. Please retry."
+        ) from e
 
 
 async def set_endorser_info(
@@ -104,47 +57,24 @@ async def set_endorser_info(
     endorser_did: str,
     logger: Logger,
 ):
-    delay = DEFAULT_DELAY
-    for n in range(DEFAULT_NUM_TRIES):
-        try:
-            logger.debug(
-                f"Setting endorser info on issuer-endorser connection. Try: {n}"
-            )
-            await issuer_controller.endorse_transaction.set_endorser_info(
-                conn_id=issuer_connection_id,
-                endorser_did=endorser_did,
-            )
-
-            # Try assert that it's done. Checking too soon may raise ACA-Py error (bug).
-            # So if it fails, retry with backing-off delay
-            logger.debug("Assert that the endorser info is set before continuing")
-
-            await assert_endorser_info_set(
-                issuer_controller,
-                issuer_connection_id,
-                endorser_did,
-                logger,
-                num_tries=1,
-                delay=delay,
-            )
-            logger.debug(f"Successfully set author role on try: {n}")
-            return True  # success, exit retries
-        except (SettingMetadataException, ClientResponseError) as e:
-            if n == DEFAULT_NUM_TRIES:
-                logger.error(
-                    "Failed to set endorser info after {} retries.", DEFAULT_NUM_TRIES
-                )
-                raise CloudApiException(
-                    "Failed to set the endorser info in the issuer-endorser connection, "
-                    f"with connection id {issuer_connection_id}."
-                ) from e
-        logger.warning(
-            f"Setting endorser info has failed on try {n} with delay {delay}s"
+    try:
+        logger.debug(f"Setting endorser info on issuer-endorser connection")
+        await issuer_controller.endorse_transaction.set_endorser_info(
+            conn_id=issuer_connection_id,
+            endorser_did=endorser_did,
         )
-        await asyncio.sleep(delay)  # Secondary delay. Primary occurs in assert_ method
+        logger.debug(f"Successfully set endorser info.")
+        await asyncio.sleep(DEFAULT_DELAY)  # Allow ACA-Py records to update
+    except ClientResponseError as e:
+        logger.error("Failed to set endorser info: {}.", e)
+        raise CloudApiException(
+            "Failed to set the endorser info in the issuer-endorser connection, "
+            f"with connection id {issuer_connection_id}. "
+            "This is a known bug in ACA-Py. Please retry."
+        ) from e
 
-        delay *= 2
-        logger.info(f"Retry setting of endorser info with increased delay: {delay}s")
+
+# Unused code at the moment: may be useful in avoiding ACA-Py delays resulting in duplicate record bug
 
 
 async def assert_metadata_set(

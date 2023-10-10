e4d7cabd017207153196f967989b86c5530ffb20
ff137 <ff137@proton.me>
2023-10-10 23:22:27 +0300

    :sparkles: connect route to service method and add response models

--- app/routes/trust_registry.py
+++ app/routes/trust_registry.py
@@ -1,6 +1,9 @@
+from typing import List
 from fastapi import APIRouter
+from app.models.trust_registry import Actor
 
-import app.services.trust_registry as trust_registry_facade
+import app.services.trustregistry.actors as registry_actors
+import app.services.trustregistry.schemas as registry_schemas
 from shared.log_config import get_logger
 
 logger = get_logger(__name__)
@@ -8,7 +11,7 @@ logger = get_logger(__name__)
 router = APIRouter(prefix="/trust-registry", tags=["trust-registry"])
 
 
-@router.get("/schemas", response_model=trust_registry_facade.Schemas)
+@router.get("/schemas", response_model=List[str])
 async def get_schemas():
     """
     Get only the schemas from the trust registry.
@@ -18,13 +21,13 @@ async def get_schemas():
     Only the schemas from the trust registry
     """
     logger.info("GET request received: Get only the schemas from the trust registry")
-    schemas = await trust_registry_facade.get_schemas()
+    schemas = await registry_schemas.get_trust_registry_schemas()
 
     logger.info("Successfully retrieved schemas.")
     return schemas
 
 
-@router.get("/actors", response_model=trust_registry_facade.Actors)
+@router.get("/actors", response_model=List[Actor])
 async def get_actors():
     """
     Get all actors from the trust registry.
@@ -34,39 +37,39 @@ async def get_actors():
     All actors from the trust registry
     """
     logger.info("GET request received: Get all actors from the trust registry")
-    actors = await trust_registry_facade.get_actors()
+    actors = await registry_actors.actors_with_role("")
 
     logger.info("Successfully retrieved actors.")
     return actors
 
 
-@router.get("/actors/issuers", response_model=trust_registry_facade.Issuers)
+@router.get("/actors/issuers", response_model=List[Actor])
 async def get_issuers():
     """
     Get only the issuers from the trust registry.
 
     Returns:
     ---------
-    Only the issuers from the trust registry
+    List of actor models, representing the issuers from the trust registry
     """
     logger.info("GET request received: Get only the issuers from the trust registry")
-    issuers = await trust_registry_facade.get_issuers()
+    issuers = await registry_actors.actors_with_role("issuer")
 
     logger.info("Successfully retrieved issuers.")
     return issuers
 
 
-@router.get("/actors/verifiers", response_model=trust_registry_facade.Verifiers)
+@router.get("/actors/verifiers", response_model=List[Actor])
 async def get_verifiers():
     """
     Get only the verifiers from the trust registry.
 
     Returns:
     ---------
-    Only the verifiers from the trust registry
+    List of actor models, representing only the verifiers from the trust registry
     """
     logger.info("GET request received: Get only the verifiers from the trust registry")
-    verifiers = await trust_registry_facade.get_verifiers()
+    verifiers = await registry_actors.actors_with_role("verifier")
 
     logger.info("Successfully retrieved verifiers.")
     return verifiers

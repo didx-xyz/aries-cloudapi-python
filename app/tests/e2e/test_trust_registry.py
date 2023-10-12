4f8157999dfbcdfd229ba9fec7387c8ea0314570
cl0ete <cloete.dupreez@gmail.com>
2023-10-12 14:16:11 +0200

    test get actor by name

--- app/tests/e2e/test_trust_registry.py
+++ app/tests/e2e/test_trust_registry.py
@@ -71,3 +71,11 @@ async def test_get_actors(faber_issuer: CreateTenantResponse):
         assert_that(actors_by_did.json()[0]).contains(
             "id", "name", "roles", "did", "didcomm_invitation"
         )
+
+        actors_by_name = await client.get(
+            f"{CLOUDAPI_URL}{TRUST_REGISTRY}/actors?actor_name={faber_issuer.tenant_name}"
+        )
+        assert actors_by_name.status_code == 200
+        assert_that(actors_by_name.json()[0]).contains(
+            "id", "name", "roles", "did", "didcomm_invitation"
+        )

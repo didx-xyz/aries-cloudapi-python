ee31754f8f68ee1027e07ebe5ca136368267f27c
cl0ete <cloete.dupreez@gmail.com>
2023-10-12 14:10:45 +0200

    imports needed for tests

---
+++ app/tests/e2e/test_trust_registry.py
@@ -0,0 +1,11 @@
+import pytest
+
+from assertpy import assert_that
+from fastapi import HTTPException
+
+from app.models.definitions import CredentialSchema
+from app.models.tenants import CreateTenantResponse
+from app.routes.trust_registry import router
+
+from shared.constants import CLOUDAPI_URL
+from shared.util.rich_async_client import RichAsyncClient

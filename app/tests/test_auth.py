b604cdf42f780cf0effdae82146d68ea30d6d22f
ff137 <ff137@proton.me>
2023-10-12 13:35:34 +0300

    :art: updated reference to base_url and headers of new AcaPyClient

--- app/tests/test_auth.py
+++ app/tests/test_auth.py
@@ -9,24 +9,29 @@ from shared import GOVERNANCE_ACAPY_API_KEY, TENANT_ACAPY_API_KEY
 @pytest.mark.anyio
 async def test_governance_agent(governance_acapy_client: AcaPyClient):
     assert isinstance(governance_acapy_client, AcaPyClient)
-    assert governance_acapy_client.base_url == Role.GOVERNANCE.agent_type.base_url
     assert (
-        governance_acapy_client.client.headers["x-api-key"] == GOVERNANCE_ACAPY_API_KEY
+        governance_acapy_client.configuration.host
+        == Role.GOVERNANCE.agent_type.base_url
     )
-    assert "Authorization" not in governance_acapy_client.client.headers
+    assert (
+        governance_acapy_client.default_headers["x-api-key"] == GOVERNANCE_ACAPY_API_KEY
+    )
+    assert "Authorization" not in governance_acapy_client.default_headers
 
 
 @pytest.mark.anyio
 async def test_tenant_agent():
     alice_acapy_client = get_tenant_acapy_client(token="Alice")
     assert isinstance(alice_acapy_client, AcaPyClient)
-    assert alice_acapy_client.base_url == Role.TENANT.agent_type.base_url
-    assert "Bearer " in alice_acapy_client.client.headers["Authorization"]
-    assert alice_acapy_client.client.headers["x-api-key"] == TENANT_ACAPY_API_KEY
+    assert alice_acapy_client.configuration.host == Role.TENANT.agent_type.base_url
+    assert "Bearer " in alice_acapy_client.default_headers["Authorization"]
+    assert alice_acapy_client.default_headers["x-api-key"] == TENANT_ACAPY_API_KEY
 
 
 @pytest.mark.anyio
 async def test_tenant_admin_agent(tenant_admin_acapy_client):
     assert isinstance(tenant_admin_acapy_client, AcaPyClient)
-    assert tenant_admin_acapy_client.client.headers["x-api-key"] == TENANT_ACAPY_API_KEY
-    assert "Authorization" not in tenant_admin_acapy_client.client.headers
+    assert (
+        tenant_admin_acapy_client.default_headers["x-api-key"] == TENANT_ACAPY_API_KEY
+    )
+    assert "Authorization" not in tenant_admin_acapy_client.default_headers

897961ff36d9265d2d928d305dbc47387ee611e1
ff137 <ff137@proton.me>
2023-10-16 20:34:00 +0200

    :white_check_mark: fix tests with new cloudcontroller config

--- app/tests/test_auth.py
+++ app/tests/test_auth.py
@@ -14,9 +14,10 @@ async def test_governance_agent(governance_acapy_client: AcaPyClient):
         == Role.GOVERNANCE.agent_type.base_url
     )
     assert (
-        governance_acapy_client.default_headers["x-api-key"] == GOVERNANCE_ACAPY_API_KEY
+        governance_acapy_client.api_client.default_headers["x-api-key"]
+        == GOVERNANCE_ACAPY_API_KEY
     )
-    assert "Authorization" not in governance_acapy_client.default_headers
+    assert "Authorization" not in governance_acapy_client.api_client.default_headers
 
 
 @pytest.mark.anyio
@@ -24,14 +25,18 @@ async def test_tenant_agent():
     alice_acapy_client = get_tenant_acapy_client(token="Alice")
     assert isinstance(alice_acapy_client, AcaPyClient)
     assert alice_acapy_client.configuration.host == Role.TENANT.agent_type.base_url
-    assert "Bearer " in alice_acapy_client.default_headers["Authorization"]
-    assert alice_acapy_client.default_headers["x-api-key"] == TENANT_ACAPY_API_KEY
+    assert "Bearer " in alice_acapy_client.api_client.default_headers["Authorization"]
+    assert (
+        alice_acapy_client.api_client.default_headers["x-api-key"]
+        == TENANT_ACAPY_API_KEY
+    )
 
 
 @pytest.mark.anyio
 async def test_tenant_admin_agent(tenant_admin_acapy_client):
     assert isinstance(tenant_admin_acapy_client, AcaPyClient)
     assert (
-        tenant_admin_acapy_client.default_headers["x-api-key"] == TENANT_ACAPY_API_KEY
+        tenant_admin_acapy_client.api_client.default_headers["x-api-key"]
+        == TENANT_ACAPY_API_KEY
     )
-    assert "Authorization" not in tenant_admin_acapy_client.default_headers
+    assert "Authorization" not in tenant_admin_acapy_client.api_client.default_headers

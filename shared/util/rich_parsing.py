e78ba4b502659cc5bdc0e448ccf79ab0738e3bfe
ff137 <ff137@proton.me>
2023-10-17 09:52:09 +0200

    :art: replace deprecated method with `model_validate_json`

--- shared/util/rich_parsing.py
+++ shared/util/rich_parsing.py
@@ -8,7 +8,7 @@ T = TypeVar("T", bound=BaseModel)
 
 def parse_with_error_handling(model: Type[T], data: str) -> T:
     try:
-        parsed_data = model.parse_raw(data)
+        parsed_data = model.model_validate_json(data)
     except ValidationError as e:
         raise ValidationError(
             f"Could not parse data into {model.__name__} object. Error: `{str(e)}`."

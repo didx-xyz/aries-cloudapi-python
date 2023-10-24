4d9fae5bc5e44d7da27cb7fe74b9e732724a086c
ff137 <ff137@proton.me>
2023-10-24 14:12:54 +0200

    :art: add wallet name as optional field with CreateTenantRequest. Add descriptions, and deduplicate definitions

--- app/models/tenants.py
+++ app/models/tenants.py
@@ -5,38 +5,54 @@ from pydantic import BaseModel, Field
 
 from app.models.trust_registry import TrustRegistryRole
 
+# Deduplicate some descriptions and field definitions
+name_description = "A required alias for the tenant, publicized to other agents when forming a connection. "
+"If the tenant is an issuer or verifier, this name will be displayed on the trust registry and must be unique."
+name_examples = ["A required alias for the tenant"]
+group_id_field = Field(
+    None,
+    description="An optional group identifier. Useful with `get_tenants` to fetch wallets by group id.",
+    examples=["Some Group Id"],
+)
+image_url_field = Field(
+    None,
+    examples=["https://upload.wikimedia.org/wikipedia/commons/7/70/Example.png"],
+)
+
 
 class CreateWalletRequestWithGroups(CreateWalletRequest):
-    group_id: Optional[str] = None
+    group_id: Optional[str] = group_id_field
 
 
-class TenantRequestBase(BaseModel):
-    image_url: Optional[str] = Field(
-        None, examples=["https://yoma.africa/images/sample.png"]
+class CreateTenantRequest(BaseModel):
+    name: str = Field(..., description=name_description, examples=name_examples)
+    wallet_name: Optional[str] = Field(
+        None,
+        description="An optional wallet name. Useful with `get_tenants` to fetch wallets by wallet name. "
+        "If selected, must be unique.",
+        examples=["An optional, unique wallet name"],
     )
-
-
-class CreateTenantRequest(TenantRequestBase):
-    name: str = Field(..., examples=["Yoma"])  # used as label and trust registry name
     roles: Optional[List[TrustRegistryRole]] = None
-    group_id: Optional[str] = Field(None, examples=["SomeGroupId"])
+    group_id: Optional[str] = group_id_field
+    image_url: Optional[str] = image_url_field
 
 
-class UpdateTenantRequest(TenantRequestBase):
+class UpdateTenantRequest(BaseModel):
     name: Optional[str] = Field(
-        None, examples=["Yoma"]
-    )  # used as label and trust registry name
+        None, description=name_description, examples=name_examples
+    )
     roles: Optional[List[TrustRegistryRole]] = None
-    group_id: Optional[str] = Field(None, examples=["SomeGroupId"])
+    group_id: Optional[str] = group_id_field
+    image_url: Optional[str] = image_url_field
 
 
 class Tenant(BaseModel):
     wallet_id: str = Field(..., examples=["545135a4-ecbc-4400-8594-bdb74c51c88d"])
     tenant_name: str = Field(..., examples=["Alice"])
-    image_url: Optional[str] = Field(None, examples=["https://yoma.africa/image.png"])
+    image_url: Optional[str] = image_url_field
     created_at: str = Field(...)
     updated_at: Optional[str] = Field(None)
-    group_id: Optional[str] = Field(None, examples=["SomeGroupId"])
+    group_id: Optional[str] = group_id_field
 
 
 class TenantAuth(BaseModel):

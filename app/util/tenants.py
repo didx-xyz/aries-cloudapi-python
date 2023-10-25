63069e6c1fb720eb3a5aa15cc4c7be20aa8f64d2
ff137 <ff137@proton.me>
2023-10-25 16:08:55 +0200

    :art: add wallet_name to tenant_from_wallet_record

--- app/util/tenants.py
+++ app/util/tenants.py
@@ -7,13 +7,18 @@ from app.models.tenants import Tenant
 
 def tenant_from_wallet_record(wallet_record: WalletRecordWithGroups) -> Tenant:
     label: str = wallet_record.settings["default_label"]
+    wallet_name: str = wallet_record.settings["wallet_name"]
     image_url: Optional[str] = wallet_record.settings.get("image_url")
+    group_id: Optional[str] = (
+        wallet_record.group_id if hasattr(wallet_record, "group_id") else None
+    )
 
     return Tenant(
         wallet_id=wallet_record.wallet_id,
         tenant_name=label,
-        image_url=image_url,
+        wallet_name=wallet_name,
         created_at=wallet_record.created_at,
         updated_at=wallet_record.updated_at,
-        group_id=wallet_record.group_id if hasattr(wallet_record, "group_id") else None,
+        image_url=image_url,
+        group_id=group_id,
     )

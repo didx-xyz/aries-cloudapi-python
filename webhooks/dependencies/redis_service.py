c76ba52b068ecc0ca134fe29f6a539720620f152
ff137 <ff137@proton.me>
2023-11-06 16:23:13 +0200

    :art: rearrange

--- webhooks/dependencies/redis_service.py
+++ webhooks/dependencies/redis_service.py
@@ -19,6 +19,12 @@ class RedisService:
     def __init__(self, redis: Redis) -> None:
         self._redis = redis
 
+    async def add_wallet_entry(self, wallet_id: str, event_json: str) -> None:
+        bound_logger = logger.bind(body={"wallet_id": wallet_id, "event": event_json})
+        bound_logger.debug("Write entry to redis")
+        await self._redis.sadd(wallet_id, event_json)
+        bound_logger.debug("Successfully wrote entry to redis.")
+
     async def get_json_webhook_events_by_wallet(self, wallet_id: str) -> List[str]:
         bound_logger = logger.bind(body={"wallet_id": wallet_id})
         bound_logger.debug("Fetching entries from redis by wallet id")
@@ -33,9 +39,3 @@ class RedisService:
         # Filter the json entry for our requested topic without deserialising
         topic_str = f'"topic":"{topic}"'
         return [entry for entry in entries if topic_str in entry]
-
-    async def add_wallet_entry(self, wallet_id: str, event_json: str) -> None:
-        bound_logger = logger.bind(body={"wallet_id": wallet_id, "event": event_json})
-        bound_logger.debug("Write entry to redis")
-        await self._redis.sadd(wallet_id, event_json)
-        bound_logger.debug("Successfully wrote entry to redis.")

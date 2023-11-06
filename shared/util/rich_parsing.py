31f35a0bdf4d060e0ff353871da4a1282cd49027
ff137 <ff137@proton.me>
2023-11-06 12:54:08 +0200

    :art:

--- shared/util/rich_parsing.py
+++ shared/util/rich_parsing.py
@@ -8,9 +8,8 @@ T = TypeVar("T", bound=BaseModel)
 
 def parse_with_error_handling(model: Type[T], data: str) -> T:
     try:
-        parsed_data = model.model_validate_json(data)
+        return model.model_validate_json(data)
     except ValidationError as e:
         raise ValidationError(
             f"Could not parse data into {model.__name__} object. Error: `{str(e)}`."
         )
-    return parsed_data

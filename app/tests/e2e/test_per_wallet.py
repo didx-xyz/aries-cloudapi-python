0badcac3d8c457c31c00a6b6a3b0374804ee264c
cl0ete <cloete.dupreez@gmail.com>
2023-11-08 10:14:35 +0200

    issue credential to holder

--- app/tests/e2e/test_per_wallet.py
+++ app/tests/e2e/test_per_wallet.py
@@ -79,3 +79,61 @@ async def test_extra_settings(
         desired_state="completed",
     )
     print("***Connection Made***")
+    cred_def = (
+        await issuer_client.post(
+            DEFINITIONS_BASE_PATH + "/credentials",
+            json={
+                "tag": "PerTenant",
+                "schema_id": schema_definition.id,
+                "support_revocation": False,
+            },
+        )
+    ).json()
+
+    holder_tenant_listener = SseListener(
+        topic="credentials", wallet_id=holder["wallet_id"]
+    )
+
+    issuer_credential_exchange = (
+        await issuer_client.post(
+            f"{ISSUER_BASE_PATH}",
+            json={
+                "protocol_version": "v1",
+                "connection_id": issuer_holder_connection_id,
+                "indy_credential_detail": {
+                    "credential_definition_id": cred_def["id"],
+                    "attributes": {"speed": "9001"},
+                },
+            },
+        )
+    ).json()
+
+    payload = await holder_tenant_listener.wait_for_event(
+        field="connection_id",
+        field_id=holder_issuer_connection_id,
+        desired_state="offer-received",
+    )
+
+    issuer_credential_exchange_id = issuer_credential_exchange["credential_id"]
+    holder_credential_exchange_id = payload["credential_id"]
+
+    issuer_tenant_cred_listener = SseListener(
+        topic="credentials", wallet_id=issuer["wallet_id"]
+    )
+
+    response = await holder_client.post(
+        f"{ISSUER_BASE_PATH}/{holder_credential_exchange_id}/request"
+    )
+    print("***Cred requested***")
+    # Wait for credential exchange to finish
+    await issuer_tenant_cred_listener.wait_for_event(
+        field="credential_id",
+        field_id=issuer_credential_exchange_id,
+        desired_state="done",
+    )
+
+    assert response.status_code == 200
+    assert False
+
+    #TODO test extra setting work
+    #TODO remove wallets after test done

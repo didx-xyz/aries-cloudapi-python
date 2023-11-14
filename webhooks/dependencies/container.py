25f74a1d2da33cba5516886e0c86544bcfd9d7d8
ff137 <ff137@proton.me>
2023-11-14 12:46:18 +0200

    :art: add docstrings

--- webhooks/dependencies/container.py
+++ webhooks/dependencies/container.py
@@ -5,24 +5,49 @@ from webhooks.dependencies.sse_manager import SseManager
 
 
 class Container(containers.DeclarativeContainer):
+    """
+    Dependency injection container for the webhooks service.
+
+    This container is responsible for creating and managing the lifecycle of
+    the Redis and SseManager services, used throughout the webhooks application.
+    It defines the configuration and instantiation of the services.
+    """
+
+    # Configuration provider for environment variables and other settings
     config = providers.Configuration()
+
+    # Resource provider for the Redis connection pool
     redis_pool = providers.Resource(
         init_redis_pool,
         host=config.redis_host,
         password=config.redis_password,
     )
+
+    # Singleton provider for the RedisService
     redis_service = providers.Singleton(
         RedisService,
         redis=redis_pool,
     )
+
+    # Singleton provider for the SseManager
     sse_manager = providers.Singleton(
         SseManager,
         redis_service=redis_service,
     )
 
 
-def get_container():
+def get_container() -> Container:
+    """
+    Creates and configures the dependency injection container for the application.
+
+    This function configures the Redis host and password. It prepares the container
+    to be wired into the application modules, enabling dependency injection throughout the app.
+
+    Returns:
+        An instance of the configured Container.
+    """
     configured_container = Container()
+    # Load configuration from environment variables with defaults
     configured_container.config.redis_host.from_env("REDIS_HOST", "localhost")
     configured_container.config.redis_password.from_env("REDIS_PASSWORD", "")
     return configured_container
--- webhooks/dependencies/event_generator_wrapper.py
+++ webhooks/dependencies/event_generator_wrapper.py
@@ -5,17 +5,47 @@ from shared.models.webhook_topics import CloudApiWebhookEventGeneric
 
 
 class EventGeneratorWrapper:
+    """
+    A wrapper class for managing an asynchronous generator alongside an asyncio task.
+
+    This class encapsulates an async generator and a related asyncio task, providing
+    a convenient way to manage their lifecycle together. It is used in the SseManager
+    and SSE routes.
+
+    Attributes:
+        generator: An asynchronous generator yielding CloudApiWebhookEventGeneric objects.
+        populate_task: An asyncio.Task object that populates the generator.
+    """
+
     def __init__(
         self,
         generator: AsyncGenerator[CloudApiWebhookEventGeneric, Any],
         populate_task: asyncio.Task,
     ):
+        """
+        Initializes the EventGeneratorWrapper with an async generator and a populate task.
+
+        Args:
+            generator: An asynchronous generator yielding CloudApiWebhookEventGeneric objects.
+            populate_task: An asyncio.Task object responsible for populating the generator.
+        """
         self.generator = generator
         self.populate_task = populate_task
 
     async def __aenter__(self):
+        """
+        Enters the asynchronous context, returning the generator.
+        """
         return self.generator
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
+        """
+        Exits the asynchronous context, closing the generator and canceling the populate task.
+
+        Args:
+            exc_type: The exception type, if any, that caused the context to be exited.
+            exc_val: The exception value, if any.
+            exc_tb: The traceback, if an exception occurred.
+        """
         await self.generator.aclose()  # Close the async generator
         self.populate_task.cancel()  # Ensure populate task is cancelled

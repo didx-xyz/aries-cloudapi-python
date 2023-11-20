cb00aaee82c85e00a80759e0fe48da1274e6920d
ff137 <ff137@proton.me>
2023-11-20 14:06:01 +0200

    :art: use api_key directly from AcaPyClient instead of reading from headers

--- app/tests/test_auth.py
+++ app/tests/test_auth.py
@@ -13,10 +13,7 @@ async def test_governance_agent(governance_acapy_client: AcaPyClient):
         governance_acapy_client.configuration.host
         == Role.GOVERNANCE.agent_type.base_url
     )
-    assert (
-        governance_acapy_client.api_client.default_headers["x-api-key"]
-        == GOVERNANCE_ACAPY_API_KEY
-    )
+    assert governance_acapy_client.api_key == GOVERNANCE_ACAPY_API_KEY
     assert "Authorization" not in governance_acapy_client.api_client.default_headers
 
 
@@ -26,17 +23,11 @@ async def test_tenant_agent():
     assert isinstance(alice_acapy_client, AcaPyClient)
     assert alice_acapy_client.configuration.host == Role.TENANT.agent_type.base_url
     assert "Bearer " in alice_acapy_client.api_client.default_headers["Authorization"]
-    assert (
-        alice_acapy_client.api_client.default_headers["x-api-key"]
-        == TENANT_ACAPY_API_KEY
-    )
+    assert alice_acapy_client.api_client.api_key == TENANT_ACAPY_API_KEY
 
 
 @pytest.mark.anyio
 async def test_tenant_admin_agent(tenant_admin_acapy_client):
     assert isinstance(tenant_admin_acapy_client, AcaPyClient)
-    assert (
-        tenant_admin_acapy_client.api_client.default_headers["x-api-key"]
-        == TENANT_ACAPY_API_KEY
-    )
+    assert tenant_admin_acapy_client.api_client.api_key == TENANT_ACAPY_API_KEY
     assert "Authorization" not in tenant_admin_acapy_client.api_client.default_headers

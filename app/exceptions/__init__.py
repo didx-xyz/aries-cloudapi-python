18ed604329183da44568d749f657137869cb54ac
ff137 <ff137@proton.me>
2023-11-20 16:09:20 +0200

    :art: imports

--- app/exceptions/__init__.py
+++ app/exceptions/__init__.py
@@ -1,3 +1,3 @@
 # pylint: disable=unused-import
-from cloudapi_exception import CloudApiException
-from trust_registry_exception import TrustRegistryException
+from app.exceptions.cloudapi_exception import CloudApiException
+from app.exceptions.trust_registry_exception import TrustRegistryException

f7039e515e78080039100c91a285131f122d9ba0
ff137 <ff137@proton.me>
2023-11-22 11:10:57 +0200

    :sparkles: make pool_recycle and pool_timeout configurable

--- trustregistry/database.py
+++ trustregistry/database.py
@@ -7,13 +7,15 @@ POSTGRES_DATABASE_URL = os.getenv(
     "POSTGRES_DATABASE_URL",
     "postgresql://trustregistry:trustregistry@trustregistry-db:5432/trustregistry",
 )
-POSTGRES_POOL_SIZE = int(os.getenv("POSTGRES_POOL_SIZE", "10"))
-POSTGRES_MAX_OVERFLOW = int(os.getenv("POSTGRES_MAX_OVERFLOW", "20"))
+POSTGRES_POOL_RECYCLE = int(os.getenv("POSTGRES_POOL_RECYCLE", "-1"))
+POSTGRES_POOL_TIMEOUT = float(os.getenv("POSTGRES_POOL_TIMEOUT", "30"))
 
 engine = create_engine(
     url=POSTGRES_DATABASE_URL,
     pool_size=POSTGRES_POOL_SIZE,
     max_overflow=POSTGRES_MAX_OVERFLOW,
+    pool_recycle=POSTGRES_POOL_RECYCLE,
+    pool_timeout=POSTGRES_POOL_TIMEOUT,
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 

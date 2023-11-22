0907f31775278c2e71d8cc23e37f8045f0428fe4
ff137 <ff137@proton.me>
2023-11-22 10:55:41 +0200

    :sparkles: add env variable config for pool_size and max_overflow

--- trustregistry/database.py
+++ trustregistry/database.py
@@ -7,7 +7,14 @@ POSTGRES_DATABASE_URL = os.getenv(
     "POSTGRES_DATABASE_URL",
     "postgresql://trustregistry:trustregistry@trustregistry-db:5432/trustregistry",
 )
-engine = create_engine(POSTGRES_DATABASE_URL)
+POSTGRES_POOL_SIZE = int(os.getenv("POSTGRES_POOL_SIZE", "10"))
+POSTGRES_MAX_OVERFLOW = int(os.getenv("POSTGRES_MAX_OVERFLOW", "20"))
+
+engine = create_engine(
+    url=POSTGRES_DATABASE_URL,
+    pool_size=POSTGRES_POOL_SIZE,
+    max_overflow=POSTGRES_MAX_OVERFLOW,
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()

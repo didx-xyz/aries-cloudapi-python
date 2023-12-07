976db5cc09dc0c8416f80c69fcd8b9ff71b177d5
ff137 <ff137@proton.me>
2023-12-07 14:10:53 +0200

    :white_check_mark: skip test if running in dev cluster

--- app/tests/e2e/test_wallet_dids.py
+++ app/tests/e2e/test_wallet_dids.py
@@ -1,3 +1,4 @@
+import os
 from typing import List
 
 import pytest
@@ -18,6 +19,10 @@ from app.tests.util.ledger import create_public_did, post_to_ledger
 from shared import RichAsyncClient
 
 WALLET_BASE_PATH = router.prefix
+
+# The setting public did test should be skipped in prod. This can be identified by the
+# existence of the seed env var. Writing a did with different seed causes conflicts
+skip_set_public_did = os.getenv("ACAPY_WALLET_SEED") is not None
 
 
 async def create_did_mock(governance_client: RichAsyncClient):
@@ -76,6 +81,10 @@ async def test_get_did_endpoint(governance_client: RichAsyncClient):
     assert response["did"] == did
 
 
+@pytest.mark.skipif(
+    skip_set_public_did,
+    reason="Avoid creating additional did for governance from different seed",
+)
 @pytest.mark.anyio
 async def test_set_public_did(
     governance_client: RichAsyncClient, governance_acapy_client: AcaPyClient

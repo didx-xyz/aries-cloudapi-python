c3d3101eb7cca4b9aac39c034accc19b0a727f38
ff137 <ff137@proton.me>
2023-12-11 16:12:20 +0200

    :white_check_mark: define basic test for fastapi app

---
+++ app/tests/test_main.py
@@ -0,0 +1,16 @@
+from unittest.mock import patch
+
+from app.main import create_app
+
+
+def test_create_app():
+    with patch("os.getenv") as mock_getenv:
+        mock_getenv.return_value = "False"  # Mock the 'prod' environment variable
+        app = create_app()
+        assert app.title == "OpenAPI"
+
+        # Verifying that all routes are included
+        routes = [route.path for route in app.routes]
+        expected_routes = ["/openapi.json", "/admin/tenants"]
+        for route in expected_routes:
+            assert route in routes

94d13cbe8dee84542d59ec6803b1a09fe94db895
cl0ete <cloete.dupreez@gmail.com>
2023-12-13 13:08:00 +0200

    add class to handle redis ops

--- app/util/wallet_map.py
+++ app/util/wallet_map.py
@@ -15,3 +15,20 @@ async def init_wallet_map_redis_pool(host: str, password: str) -> AsyncIterator[
     yield pool
 
     await pool.close()
+
+
+class ClientWalletMap:
+    def __init__(self, redis: Redis) -> None:
+        self.redis = redis
+
+    async def add_wallet_map(self, wallet_id: str, group: str) -> None:
+        try:
+            await self.redis.set(wallet_id, group)
+        except Exception as e:
+            raise e
+
+    async def get_wallet_map(self, wallet_id: str) -> str:
+        try:
+            return await self.redis.get(wallet_id)
+        except Exception as e:
+            raise e

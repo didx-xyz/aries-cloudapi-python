3cd1005bb7e887cc7a70eab333049e7e021e9756
cl0ete <cloete.dupreez@gmail.com>
2023-12-14 15:09:28 +0200

    models

--- app/clients_web.py
+++ app/clients_web.py
@@ -19,3 +19,21 @@ LAGO_URL = "http://localhost:3000/api/v1/events"
 LAGO_API_KEY = "cb131628-c605-49bd-8aa3-93fe0289e1a3"
 PORT = os.getenv("PORT", "3010")
 URL = os.getenv("WEBHOOKS_URL", "localhost")
+
+
+class Event(BaseModel):
+    payload: Dict[str, Any]
+    origin: str
+    wallet_id: str
+    topic: str
+
+
+class LagoEvent(BaseModel):
+    external_customer_id: str
+    transaction_id: str
+    code: str
+    # external_subscription_id: str
+
+
+class GetTransactionError(Exception):
+    """Raise when unable to get endorsement transaction"""

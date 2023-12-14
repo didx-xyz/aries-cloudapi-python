e6d64ba7e14031efc6c4c587a5a5380c51361a90
cl0ete <cloete.dupreez@gmail.com>
2023-12-14 15:09:04 +0200

    constants

--- app/clients_web.py
+++ app/clients_web.py
@@ -14,3 +14,8 @@ from shared.util.rich_parsing import parse_with_error_handling
 from shared.constants import GOVERNANCE_AGENT_API_KEY, GOVERNANCE_AGENT_URL
 
 from fastapi_websocket_pubsub import PubSubClient
+
+LAGO_URL = "http://localhost:3000/api/v1/events"
+LAGO_API_KEY = "cb131628-c605-49bd-8aa3-93fe0289e1a3"
+PORT = os.getenv("PORT", "3010")
+URL = os.getenv("WEBHOOKS_URL", "localhost")

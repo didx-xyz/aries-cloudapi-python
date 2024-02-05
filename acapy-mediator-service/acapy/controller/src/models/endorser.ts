ee0907a9b934ee7f2a3e38a3d31d9079207ee90f
wdbasson <wlmbasson@gmail.com>
2024-02-05 17:03:13 +0200

    ledger node IPs via tailscale

--- acapy-mediator-service/acapy/controller/src/models/endorser.ts
+++
@@ -1,3 +0,0 @@
-export interface EndorserMetadataServiceRequest {
-  connection_id: string;
-}
--- acapy-mediator-service/acapy/controller/test/services/webhooks.test.ts
+++
@@ -1,8 +0,0 @@
-import app from '../../src/app';
-
-describe('\'webhooks\' service', () => {
-  it('registered the service', () => {
-    const service = app.service('webhooks/topic/:topic');
-    expect(service).toBeTruthy();
-  });
-});

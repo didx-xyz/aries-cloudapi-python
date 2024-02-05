ebd8ff0541d7d05e0a82144db3a1cd131e8959de
cl0ete <cloete.dupreez@gmail.com>
2024-02-05 21:22:06 +0200

    test publish all for given rev reg id

--- app/tests/e2e/test_revocation.py
+++ app/tests/e2e/test_revocation.py
@@ -117,3 +117,36 @@ async def test_clear_pending_revokes_bad_payload(
         )
 
     assert_that(exc.value.status_code).is_equal_to(404)
+
+
+@pytest.mark.anyio
+async def test_publish_all_revocations_for_rev_reg_id(
+    faber_client: RichAsyncClient, issue_revocable_credentials_to_alice_and_revoke: list
+):
+
+    response = (
+        await faber_client.get(
+            f"{CREDENTIALS_BASE_PATH}/revocation/record"
+            + "?credential_exchange_id="
+            + issue_revocable_credentials_to_alice_and_revoke[0]["credential_id"][3:]
+        )
+    ).json()
+
+    rev_reg_id = response["rev_reg_id"]
+
+    await faber_client.post(
+        f"{CREDENTIALS_BASE_PATH}/publish-revocations",
+        json={"revocation_registry_credential_map": {rev_reg_id: []}},
+    )
+
+    for cred in issue_revocable_credentials_to_alice_and_revoke:
+
+        rev_record = (
+            await faber_client.get(
+                f"{CREDENTIALS_BASE_PATH}/revocation/record"
+                + "?credential_exchange_id="
+                + cred["credential_id"][3:]
+            )
+        ).json()
+
+        assert rev_record["state"] == "revoked"

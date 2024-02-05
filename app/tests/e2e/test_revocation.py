0e9244b8fba9ff60ea6ca146c674a7ab43f2a206
cl0ete <cloete.dupreez@gmail.com>
2024-02-05 21:20:47 +0200

    clear revocations empty payload

--- app/tests/e2e/test_revocation.py
+++ app/tests/e2e/test_revocation.py
@@ -52,3 +52,30 @@ async def test_clear_pending_revokes(
         ).json()
 
         assert rev_record["state"] == "issued"
+
+
+@pytest.mark.anyio
+async def test_clear_pending_revokes_no_map(
+    faber_client: RichAsyncClient, issue_revocable_credentials_to_alice_and_revoke: list
+):
+
+    clear_revoke_response = (
+        await faber_client.post(
+            f"{CREDENTIALS_BASE_PATH}/clear-pending-revocations",
+            json={"revocation_registry_credential_map": {}},
+        )
+    ).json()["revocation_registry_credential_map"]
+
+    assert clear_revoke_response == {}
+
+    for cred in issue_revocable_credentials_to_alice_and_revoke:
+
+        rev_record = (
+            await faber_client.get(
+                f"{CREDENTIALS_BASE_PATH}/revocation/record"
+                + "?credential_exchange_id="
+                + cred["credential_id"][3:]
+            )
+        ).json()
+
+        assert rev_record["state"] == "issued"

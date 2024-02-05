d09fb5a73c429bba57c2254c698af786c7fb0797
cl0ete <cloete.dupreez@gmail.com>
2024-02-05 21:22:51 +0200

    publish one revocation

--- app/tests/e2e/test_revocation.py
+++ app/tests/e2e/test_revocation.py
@@ -173,3 +173,39 @@ async def test_publish_all_revocations_no_payload(
         ).json()
 
         assert rev_record["state"] == "revoked"
+
+
+@pytest.mark.anyio
+async def test_publish_one_revocation(
+    faber_client: RichAsyncClient, issue_revocable_credentials_to_alice_and_revoke: list
+):
+
+    response = (
+        await faber_client.get(
+            f"{CREDENTIALS_BASE_PATH}/revocation/record"
+            + "?credential_exchange_id="
+            + issue_revocable_credentials_to_alice_and_revoke[0]["credential_id"][3:]
+        )
+    ).json()
+
+    rev_reg_id = response["rev_reg_id"]
+
+    await faber_client.post(
+        f"{CREDENTIALS_BASE_PATH}/publish-revocations",
+        json={"revocation_registry_credential_map": {rev_reg_id: ["1"]}},
+    )
+
+    for cred in issue_revocable_credentials_to_alice_and_revoke:
+
+        rev_record = (
+            await faber_client.get(
+                f"{CREDENTIALS_BASE_PATH}/revocation/record"
+                + "?credential_exchange_id="
+                + cred["credential_id"][3:]
+            )
+        ).json()
+        print(rev_record)
+        if rev_record["cred_rev_id"] == "1":
+            assert rev_record["state"] == "revoked"
+        else:
+            assert rev_record["state"] == "issued"

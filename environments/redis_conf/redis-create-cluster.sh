7eb300a7a1407c3f49117583e8723822325e6b95
ff137 <ff137@proton.me>
2024-02-23 14:40:53 +0200

    :fire:

--- environments/redis_conf/redis-create-cluster.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sleep 10
-
-node_1_ip=$(getent hosts redis-cluster-node-1 | awk '{ print $1 }')
-node_2_ip=$(getent hosts redis-cluster-node-2 | awk '{ print $1 }')
-node_3_ip=$(getent hosts redis-cluster-node-3 | awk '{ print $1 }')
-
-redis-cli --cluster create $node_1_ip:6379 $node_2_ip:6379 $node_3_ip:6379 --cluster-replicas 1 --cluster-yes

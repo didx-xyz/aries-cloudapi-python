649543101bb6b54786a8b9f944ec2b33679a0d0f
ff137 <ff137@proton.me>
2024-02-23 13:49:37 +0200

    :art: add more examples and descriptions

--- app/models/sd_jws.py
+++ app/models/sd_jws.py
@@ -6,13 +6,32 @@ from app.models.jws import JWSCreateRequest, JWSVerifyResponse
 
 
 class SDJWSCreateRequest(JWSCreateRequest):  # extend JWSCreateRequest model
-    non_sd_list: Optional[List[str]] = Field(default=[])
+    non_sd_list: Optional[List[str]] = Field(
+        default=[],
+        examples=[
+            [
+                "name",
+                "address",
+                "address.street_address",
+                "nationalities[1:3]",
+            ]
+        ],
+    )
 
 
 class SDJWSCreateResponse(BaseModel):
     sd_jws: str = Field(
         ...,
-        examples=[],
+        examples=[
+            (
+                "eyJhbGciOiJFZERTQSJ9."
+                "eyJhIjogIjAifQ."
+                "dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk"
+                "~WyJEM3BUSFdCYWNRcFdpREc2TWZKLUZnIiwgIkRFIl0"
+                "~WyJPMTFySVRjRTdHcXExYW9oRkd0aDh3IiwgIlNBIl0"
+                "~WyJkVmEzX1JlTGNsWTU0R1FHZm5oWlRnIiwgInVwZGF0ZWRfYXQiLCAxNTcwMDAwMDAwXQ"
+            )
+        ],
     )
 
 
@@ -21,4 +40,17 @@ class SDJWSVerifyRequest(SDJWSCreateResponse):
 
 
 class SDJWSVerifyResponse(JWSVerifyResponse):  # extend JWSVerifyResponse model
-    disclosures: List[List[Union[str, Dict]]] = Field(..., examples=[])
+    disclosures: List[List[Union[str, Dict]]] = Field(
+        ...,
+        description="Disclosure arrays associated with the SD-JWT",
+        examples=[
+            [
+                ["fx1iT_mETjGiC-JzRARnVg", "name", "Alice"],
+                [
+                    "n4-t3mlh8jSS6yMIT7QHnA",
+                    "street_address",
+                    {"_sd": ["kLZrLK7enwfqeOzJ9-Ss88YS3mhjOAEk9lr_ix2Heng"]},
+                ],
+            ]
+        ],
+    )

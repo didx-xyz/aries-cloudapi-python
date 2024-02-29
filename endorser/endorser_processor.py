9468184f85ce2432977b7b493426fc4aba4b4c19
ff137 <ff137@proton.me>
2024-02-29 14:10:52 +0200

    :truck: move transaction record methods to utils

--- endorser/endorser_processor.py
+++ endorser/endorser_processor.py
@@ -1,12 +1,16 @@
 import asyncio
-import json
-from typing import Any, Dict, Optional
+from typing import Any, Dict
 
-from aries_cloudcontroller import AcaPyClient, TransactionRecord
+from aries_cloudcontroller import AcaPyClient
 from fastapi import HTTPException
 from fastapi_websocket_pubsub import PubSubClient
 from pydantic import BaseModel
 
+from endorser.utils.transaction_record import (
+    get_did_and_schema_id_from_cred_def_attachment,
+    get_endorsement_request_attachment,
+    is_credential_definition_transaction,
+)
 from endorser.utils.trust_registry import is_valid_issuer
 from shared import GOVERNANCE_AGENT_API_KEY, GOVERNANCE_AGENT_URL, WEBHOOKS_PUBSUB_URL
 from shared.log_config import get_logger
@@ -167,97 +171,6 @@ async def should_accept_endorsement(
             else:
                 bound_logger.error("Max retries reached. Giving up.")
                 return False
-
-
-async def get_did_and_schema_id_from_cred_def_attachment(
-    client: AcaPyClient, attachment: Dict[str, Any]
-):
-    did = "did:sov:" + attachment["identifier"]
-    schema_seq_id = attachment["operation"]["ref"]
-
-    logger.debug("Fetching schema with seq id: `{}`", schema_seq_id)
-    schema = await client.schema.get_schema(schema_id=str(schema_seq_id))
-
-    if not schema.var_schema or not schema.var_schema.id:
-        raise Exception("Could not extract schema id from schema response.")
-
-    schema_id = schema.var_schema.id
-
-    return (did, schema_id)
-
-
-def get_endorsement_request_attachment(
-    transaction: TransactionRecord,
-) -> Optional[Dict[str, Any]]:
-    try:
-        if not transaction.messages_attach:
-            logger.debug("No message attachments in transaction")
-            return None
-
-        attachment: Dict = transaction.messages_attach[0]
-
-        if "data" not in attachment:
-            logger.debug(
-                "Attachment does not contain expected key `data`. Got attachment: `{}`.",
-                attachment,
-            )
-            return None
-
-        if not isinstance(attachment["data"], dict) or "json" not in attachment["data"]:
-            logger.debug(
-                "Attachment data does not contain expected keys `json`. Got attachment data: `{}`.",
-                attachment["data"],
-            )
-            return None
-
-        json_payload = attachment["data"]["json"]
-
-        # Both dict and str encoding have ocurred for the attachment data
-        # Parse to dict if payload is of type str
-        if isinstance(json_payload, str):
-            logger.debug("Try cast attachment payload to json")
-            try:
-                json_payload = json.loads(json_payload)
-                logger.debug("Payload is valid JSON.")
-            except json.JSONDecodeError:
-                logger.warning("Failed to decode attachment payload. Invalid JSON.")
-                json_payload = None
-
-        return json_payload
-    except (TypeError, KeyError):
-        logger.warning(f"Could not read attachment from transaction: `{transaction}`.")
-    except Exception:
-        logger.exception(
-            "Exception caught while running `get_endorsement_request_attachment`."
-        )
-    return None
-
-
-def is_credential_definition_transaction(attachment: Dict[str, Any]) -> bool:
-    try:
-        if "operation" not in attachment:
-            logger.debug("Key `operation` not in attachment: `{}`.", attachment)
-            return False
-
-        operation = attachment["operation"]
-
-        if "type" not in operation:
-            logger.debug("Key `type` not in operation attachment.")
-            return False
-
-        logger.debug(
-            "Endorsement request operation type: `{}`. Must be 102 for credential definition",
-            operation.get("type"),
-        )
-        # credential definition type is 102
-        # see https://github.com/hyperledger/indy-node/blob/master/docs/source/requests.md#common-request-structure
-
-        return operation.get("type") == "102"
-    except Exception:
-        logger.exception(
-            "Exception caught while running `is_credential_definition_transaction`."
-        )
-        return False
 
 
 async def accept_endorsement(client: AcaPyClient, endorsement: Endorsement):

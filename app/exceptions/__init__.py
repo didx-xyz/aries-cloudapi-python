ac539344f6d8f00c56caece33f486427fe41511b
ff137 <ff137@proton.me>
2024-03-05 11:24:21 +0200

    :art: import aries_cloudcontroller.exceptions

--- app/exceptions/__init__.py
+++ app/exceptions/__init__.py
@@ -1,3 +1,4 @@
 # pylint: disable=unused-import
 from app.exceptions.cloudapi_exception import CloudApiException
 from app.exceptions.trust_registry_exception import TrustRegistryException
+from aries_cloudcontroller.exceptions import *

e44bb7672c772b65fda1939d7e359e10949159ef
ff137 <ff137@proton.me>
2024-03-05 11:55:30 +0200

    :art:

--- app/exceptions/__init__.py
+++ app/exceptions/__init__.py
@@ -1,5 +1,6 @@
 # flake8: noqa
 # pylint: disable=unused-import
+from aries_cloudcontroller.exceptions import *
+
 from app.exceptions.cloudapi_exception import CloudApiException
 from app.exceptions.trust_registry_exception import TrustRegistryException
-from aries_cloudcontroller.exceptions import *

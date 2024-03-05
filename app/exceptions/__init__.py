69f7bd124562ddd5246f0b1e921321037a2e30b2
ff137 <ff137@proton.me>
2024-03-05 11:53:31 +0200

    :art: ignore warning

--- app/exceptions/__init__.py
+++ app/exceptions/__init__.py
@@ -1,3 +1,4 @@
+# flake8: noqa
 # pylint: disable=unused-import
 from app.exceptions.cloudapi_exception import CloudApiException
 from app.exceptions.trust_registry_exception import TrustRegistryException

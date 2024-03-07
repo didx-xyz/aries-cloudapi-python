11842fee6bfaa5b35dfde6cad9704f559307c312
ff137 <ff137@proton.me>
2024-03-07 18:20:22 +0200

    :white_check_mark: improve coverage

--- endorser/tests/test_util_transaction_record.py
+++ endorser/tests/test_util_transaction_record.py
@@ -1,5 +1,5 @@
 import json
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, Mock
 
 import pytest
 from aries_cloudcontroller import TransactionRecord
@@ -40,6 +40,47 @@ def test_get_endorsement_request_attachment_without_attachment():
     )
     assert attachment is None
 
+    transaction_record_data_key = TransactionRecord(
+        messages_attach=[{"not_the_expected_data_key": "abc"}]
+    )
+    attachment = get_endorsement_request_attachment(transaction_record_data_key)
+    assert attachment is None
+
+    transaction_record_without_json_key = TransactionRecord(
+        messages_attach=[{"data": {"not_the_expected_json_key": "abc"}}]
+    )
+    attachment = get_endorsement_request_attachment(transaction_record_without_json_key)
+    assert attachment is None
+
+
+def test_get_endorsement_request_attachment_invalid_json():
+    transaction_record_without_valid_json = TransactionRecord(
+        messages_attach=[{"data": {"json": "invalid_json"}}]
+    )
+    attachment = get_endorsement_request_attachment(
+        transaction_record_without_valid_json
+    )
+    assert attachment is None
+
+
+def test_get_endorsement_request_attachment_exception():
+    # Mock a TransactionRecord that raises an Exception when messages_attach is accessed
+    transaction_record_exception = MagicMock(spec=TransactionRecord)
+    transaction_record_exception.messages_attach = Mock(
+        side_effect=KeyError("Key error exception")
+    )
+
+    attachment = get_endorsement_request_attachment(transaction_record_exception)
+    assert attachment is None
+
+    transaction_record_exception = MagicMock(spec=TransactionRecord)
+    transaction_record_exception.messages_attach = Mock(
+        side_effect=Exception("Test exception")
+    )
+
+    attachment = get_endorsement_request_attachment(transaction_record_exception)
+    assert attachment is None
+
 
 def test_is_credential_definition_transaction_true():
     attachment = {"operation": {"type": "102"}}
@@ -48,6 +89,22 @@ def test_is_credential_definition_transaction_true():
 
 def test_is_credential_definition_transaction_false():
     attachment = {"operation": {"type": "100"}}
+    assert is_credential_definition_transaction(attachment) is False
+
+
+def test_is_credential_definition_transaction_no_operation():
+    attachment = {"bad_key": {"type": "102"}}
+    assert is_credential_definition_transaction(attachment) is False
+
+
+def test_is_credential_definition_transaction_no_type():
+    attachment = {"operation": {"bad_key": "102"}}
+    assert is_credential_definition_transaction(attachment) is False
+
+
+def test_is_credential_definition_transaction_exception():
+    attachment = MagicMock(side_effect=KeyError("Exception"))
+
     assert is_credential_definition_transaction(attachment) is False
 
 

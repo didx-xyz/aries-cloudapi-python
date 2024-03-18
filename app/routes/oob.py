ea1a3c8d991bad1aa5a8da8cb73c272e568e9874
ff137 <ff137@proton.me>
2024-03-18 16:11:29 +0200

    :sparkles: update oob route to use `handle_model_with_validation` for any models that can raise ValueErrors, and to use `handle_acapy_call` for all acapy calls

--- app/routes/oob.py
+++ app/routes/oob.py
@@ -1,10 +1,11 @@
 from typing import Optional
 
 from aries_cloudcontroller import InvitationCreateRequest, InvitationRecord, OobRecord
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 
 from app.dependencies.acapy_clients import client_from_auth
 from app.dependencies.auth import AcaPyAuth, acapy_auth
+from app.exceptions import handle_acapy_call, handle_model_with_validation
 from app.models.oob import AcceptOobInvitation, ConnectToPublicDid, CreateOobInvitation
 from app.util.credentials import strip_protocol_prefix
 from shared.log_config import get_logger
@@ -28,26 +29,34 @@ async def create_oob_invitation(
     if body is None:
         body = CreateOobInvitation()
 
-    handshake_protocols = [
-        "https://didcomm.org/didexchange/1.0",
-        "https://didcomm.org/connections/1.0",
-    ]
+    handshake_protocols = (
+        [
+            "https://didcomm.org/didexchange/1.0",
+            "https://didcomm.org/connections/1.0",
+        ]
+        if body.create_connection
+        else None
+    )
 
     if body.attachments:
         for item in body.attachments:
             if item.id:  # Optional field
                 item.id = strip_protocol_prefix(item.id)
 
-    oob_body = InvitationCreateRequest(
+    oob_body = handle_model_with_validation(
+        logger=bound_logger,
+        model_class=InvitationCreateRequest,
         alias=body.alias,
         attachments=body.attachments,
-        handshake_protocols=handshake_protocols if body.create_connection else None,
+        handshake_protocols=handshake_protocols,
         use_public_did=body.use_public_did,
     )
 
+    bound_logger.debug("Creating invitation")
     async with client_from_auth(auth) as aries_controller:
-        logger.bind(body=oob_body).debug("Creating invitation")
-        invitation = await aries_controller.out_of_band.create_invitation(
+        invitation = await handle_acapy_call(
+            logger=bound_logger,
+            acapy_call=aries_controller.out_of_band.create_invitation,
             multi_use=body.multi_use,
             body=oob_body,
             auto_accept=True,
@@ -71,8 +80,9 @@ async def accept_oob_invitation(
     bound_logger.info("POST request received: Accept OOB invitation")
 
     async with client_from_auth(auth) as aries_controller:
-        bound_logger.debug("Accepting invitation")
-        oob_record = await aries_controller.out_of_band.receive_invitation(
+        oob_record = await handle_acapy_call(
+            logger=bound_logger,
+            acapy_call=aries_controller.out_of_band.receive_invitation,
             auto_accept=True,
             use_existing_connection=body.use_existing_connection,
             alias=body.alias,
@@ -106,9 +116,10 @@ async def connect_to_public_did(
     bound_logger = logger.bind(body=body)
     bound_logger.info("POST request received: Connect to public DID")
     async with client_from_auth(auth) as aries_controller:
-        bound_logger.debug("Creating DID exchange request")
-        conn_record = await aries_controller.did_exchange.create_request(
-            their_public_did=body.public_did
+        conn_record = await handle_acapy_call(
+            logger=bound_logger,
+            acapy_call=aries_controller.did_exchange.create_request,
+            their_public_did=body.public_did,
         )
 
     result = conn_record_to_connection(conn_record)

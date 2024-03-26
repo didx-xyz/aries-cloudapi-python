2d4744902b75b603549345009fec591a73ebfb8e
ff137 <ff137@proton.me>
2024-03-26 10:37:51 +0200

    :art: reorder

--- app/services/webhooks.py
+++ app/services/webhooks.py
@@ -10,20 +10,6 @@ from shared.util.rich_async_client import RichAsyncClient
 logger = get_logger(__name__)
 
 
-async def get_hooks_for_wallet_by_topic(wallet_id: str, topic: CloudApiTopics) -> List:
-    bound_logger = logger.bind(body={"wallet_id": wallet_id, "topic": topic})
-    bound_logger.info("Fetching webhooks events from /webhooks/wallet_id/topic")
-    try:
-        async with RichAsyncClient() as client:
-            hooks = (
-                await client.get(f"{WEBHOOKS_URL}/webhooks/{wallet_id}/{topic}")
-            ).json()
-            return hooks if hooks else []
-    except HTTPError as e:
-        bound_logger.error("HTTP Error caught when fetching webhooks: {}.", e)
-        raise e
-
-
 async def get_hooks_for_wallet(wallet_id: str) -> List:
     """
     Gets webhooks for wallet. Only return the first 100 hooks to not overload OpenAPI interface
@@ -33,6 +19,20 @@ async def get_hooks_for_wallet(wallet_id: str) -> List:
     try:
         async with RichAsyncClient() as client:
             hooks = (await client.get(f"{WEBHOOKS_URL}/webhooks/{wallet_id}")).json()
+            return hooks if hooks else []
+    except HTTPError as e:
+        bound_logger.error("HTTP Error caught when fetching webhooks: {}.", e)
+        raise e
+
+
+async def get_hooks_for_wallet_by_topic(wallet_id: str, topic: CloudApiTopics) -> List:
+    bound_logger = logger.bind(body={"wallet_id": wallet_id, "topic": topic})
+    bound_logger.info("Fetching webhooks events from /webhooks/wallet_id/topic")
+    try:
+        async with RichAsyncClient() as client:
+            hooks = (
+                await client.get(f"{WEBHOOKS_URL}/webhooks/{wallet_id}/{topic}")
+            ).json()
             return hooks if hooks else []
     except HTTPError as e:
         bound_logger.error("HTTP Error caught when fetching webhooks: {}.", e)

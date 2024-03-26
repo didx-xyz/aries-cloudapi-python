f60b2df2d158e1ad2a4f3ac2ac5d60ac87125da2
ff137 <ff137@proton.me>
2024-03-26 15:49:56 +0200

    :sparkles: modify websocket topics to all include group. No longer publush to ALL TOPICS

--- webhooks/web/routers/websocket.py
+++ webhooks/web/routers/websocket.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter
 from fastapi_websocket_pubsub import PubSubEndpoint
 
 from shared.log_config import get_logger
-from shared.models.webhook_events import WEBHOOK_TOPIC_ALL
 
 logger = get_logger(__name__)
 
@@ -13,21 +12,28 @@ endpoint.register_route(router, "/pubsub")
 
 
 async def publish_event_on_websocket(
-    event_json: str, wallet_id: str, topic: str
+    event_json: str, group_id: str, wallet_id: str, topic: str
 ) -> None:
     """
     Publish the webhook to websocket subscribers on the following topics:
-        - current wallet id
-        - topic of the event
-        - topic and wallet id combined as topic-wallet_id
-        - 'all' topic, which allows to subscribe to all published events
+        - for the group_id
+        - for the group_id - wallet_id pair
+        - for the group_id - topic pair
+        - for the group_id - wallet_id - topic combo
 
     Args:
         event_json (str): Webhook event serialized as json
+        group_id (str): The group_id that wallet belongs to
         wallet_id (str): The wallet_id for this event
         topic (str): The cloudapi topic for the event
     """
+    group_wallet = f"{group_id}:{wallet_id}"
+    group_topic = f"{group_id}:{topic}"
+    group_wallet_topic = f"{group_id}:{wallet_id}:{topic}"
 
-    publish_topics = [topic, wallet_id, f"{topic}-{wallet_id}", WEBHOOK_TOPIC_ALL]
+    publish_topics = [group_wallet, group_topic, group_wallet_topic]
+    if group_id:  # don't publish on empty string if group_id is blank
+        publish_topics += [group_id]
+
     logger.trace("Publishing event on websocket: {}", event_json)
     await endpoint.publish(topics=publish_topics, data=event_json)

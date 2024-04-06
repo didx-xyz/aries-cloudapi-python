27a83ead68a6bde2d1b519ee8e4979bde9389d73
ff137 <ff137@proton.me>
2024-04-06 11:13:18 +0200

    :white_check_mark: assert method args

--- app/tests/routes/test_admin_tenants.py
+++ app/tests/routes/test_admin_tenants.py
@@ -1,12 +1,20 @@
+from secrets import token_urlsafe
 from unittest.mock import ANY, AsyncMock, Mock, patch
 
+import base58
 import pytest
 from aries_cloudcontroller import CreateWalletResponse, CreateWalletTokenRequest
 from fastapi import HTTPException
 
 from app.dependencies.acapy_clients import TENANT_ADMIN_AUTHED
 from app.exceptions import CloudApiException, TrustRegistryException
-from app.models.tenants import CreateTenantRequest, UpdateTenantRequest
+from app.models.tenants import (
+    CreateTenantRequest,
+    CreateWalletRequestWithGroups,
+    OnboardResult,
+    UpdateTenantRequest,
+)
+from app.models.trust_registry import Actor
 from app.routes.admin.tenants import (
     create_tenant,
     delete_tenant_by_id,
@@ -31,25 +39,52 @@ create_tenant_body = CreateTenantRequest(
     wallet_name=wallet_name,
     roles=["issuer"],
     group_id="test_group",
+    image_url="some_image_url",
 )
 
 
 @pytest.mark.anyio
 @pytest.mark.parametrize(
-    "roles", [[], ["issuer"], ["verifier"], ["issuer", "verifier"]]
+    "roles",
+    [[], ["issuer"], ["verifier"], ["issuer", "verifier"]],
 )
 async def test_create_tenant_success(roles):
+    # Create tenant request body
+    body = create_tenant_body.model_copy(update={"roles": roles})
+    wallet_label = create_tenant_body.wallet_label
+
     # Mock the dependencies
     mock_admin_controller = AsyncMock()
     mock_admin_controller.multitenancy.create_wallet = AsyncMock(
         return_value=create_wallet_response
     )
-    mock_onboard_tenant = AsyncMock()
+
+    did = "did:example:123"
+    didcomm_invitation = "invitation_string"
+    mock_onboard_tenant_result = OnboardResult(
+        did=did, didcomm_invitation=didcomm_invitation
+    )
+    mock_onboard_tenant = AsyncMock(return_value=mock_onboard_tenant_result)
+
     mock_register_actor = AsyncMock()
+
+    expected_wallet_body = CreateWalletRequestWithGroups(
+        image_url=body.image_url,
+        key_management_mode="managed",
+        label=wallet_label,
+        wallet_key=base58.b58encode(token_urlsafe(48)).decode(),
+        wallet_name=wallet_name,
+        wallet_type="askar",
+        group_id=body.group_id,
+        extra_settings=body.extra_settings,
+    )
 
     with patch(
         "app.routes.admin.tenants.get_tenant_admin_controller"
     ) as mock_get_admin_controller, patch(
+        "app.routes.admin.tenants.handle_model_with_validation",
+        return_value=expected_wallet_body,
+    ), patch(
         "app.routes.admin.tenants.onboard_tenant", mock_onboard_tenant
     ), patch(
         "app.routes.admin.tenants.register_actor", mock_register_actor
@@ -62,21 +97,41 @@ async def test_create_tenant_success(roles):
         )
 
         response = await create_tenant(
-            body=create_tenant_body.model_copy(update={"roles": roles}),
+            body=body,
             admin_auth=TENANT_ADMIN_AUTHED,
         )
 
-        assert response.wallet_label == create_tenant_body.wallet_label
+        assert response.wallet_label == wallet_label
         assert response.group_id == create_tenant_body.group_id
-        mock_admin_controller.multitenancy.create_wallet.assert_awaited()
+
+        mock_admin_controller.multitenancy.create_wallet.assert_awaited_once_with(
+            body=expected_wallet_body
+        )
 
         if roles:
-            mock_onboard_tenant.assert_awaited()
-            mock_register_actor.assert_awaited()
+            mock_onboard_tenant.assert_awaited_once_with(
+                tenant_label=wallet_label,
+                roles=roles,
+                wallet_auth_token=create_wallet_response.token,
+                wallet_id=create_wallet_response.wallet_id,
+            )
+            mock_register_actor.assert_awaited_once_with(
+                actor=Actor(
+                    id=create_wallet_response.wallet_id,
+                    name=create_tenant_body.wallet_label,
+                    roles=roles,
+                    did=did,
+                    didcomm_invitation=didcomm_invitation,
+                )
+            )
 
 
 @pytest.mark.anyio
-async def test_create_tenant_fail_trust_registry_error():
+@pytest.mark.parametrize(
+    "roles",
+    [[], ["issuer"], ["verifier"], ["issuer", "verifier"]],
+)
+async def test_create_tenant_fail_trust_registry_error(roles):
     with patch(
         "app.routes.admin.tenants.assert_actor_name",
         side_effect=TrustRegistryException("Error"),
@@ -84,7 +139,10 @@ async def test_create_tenant_fail_trust_registry_error():
         CloudApiException,
         match="An error occurred when trying to register actor. Please try again",
     ) as exc:
-        await create_tenant(body=Mock(), admin_auth=TENANT_ADMIN_AUTHED)
+        await create_tenant(
+            body=create_tenant_body.model_copy(update={"roles": roles}),
+            admin_auth=TENANT_ADMIN_AUTHED,
+        )
     assert exc.value.status_code == 500
 
 

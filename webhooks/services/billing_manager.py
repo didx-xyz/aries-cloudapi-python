f1b3fde7147f481268605effea3ecfdb753a7e1f
cl0ete <cloete.dupreez@gmail.com>
2024-04-09 14:53:36 +0200

    api key check not needed

--- webhooks/services/billing_manager.py
+++ webhooks/services/billing_manager.py
@@ -56,19 +56,18 @@ class BillingManager:
         """
         Wait for tasks to complete and stop the billing manager
         """
-        if self.lago_api_key:
-            for task in self._tasks:
-                task.cancel()  # Request cancellation of the task
-                try:
-                    await task  # Wait for the task to be cancelled
-                except asyncio.CancelledError:
-                    pass
-            self._tasks.clear()  # Clear the list of tasks
-            logger.info("Billing manager stopped")
-
-            if self._pubsub:
-                self._pubsub.disconnect()
-                logger.info("Billing pubsub disconnected")
+        
+        for task in self._tasks:
+            task.cancel()  # Request cancellation of the task
+            try:
+                await task  # Wait for the task to be cancelled
+            except asyncio.CancelledError:
+                pass
+        self._tasks.clear()  # Clear the list of tasks
+        logger.info("Billing manager stopped")
+        if self._pubsub:
+            self._pubsub.disconnect()
+            logger.info("Billing pubsub disconnected")
 
     def are_tasks_running(self) -> bool:
         """

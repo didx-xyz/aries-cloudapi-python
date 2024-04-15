0b74036c2f98373e9407e6b196cde0ad57eef7bc
ff137 <ff137@proton.me>
2024-04-15 20:05:11 +0200

    :art: replace SseListener

--- app/tests/e2e/issuer/test_credential_issue_on_deleted_connection.py
+++ app/tests/e2e/issuer/test_credential_issue_on_deleted_connection.py
@@ -3,11 +3,10 @@ from logging import getLogger
 
 import pytest
 
-from app.models.tenants import CreateTenantResponse
 from app.routes.connections import router as connections_router
 from app.routes.issuer import router as credentials_router
-from app.services.event_handling.sse_listener import SseListener
 from app.tests.util.ecosystem_connections import FaberAliceConnect
+from app.tests.util.webhooks import check_webhook_state
 from shared import RichAsyncClient
 from shared.models.credential_exchange import CredentialExchange
 
@@ -21,10 +20,9 @@ logger = getLogger()
 @pytest.mark.anyio
 async def test_credential_issue_on_deleted_connection(
     faber_client: RichAsyncClient,
-    credential_definition_id: str,  # pylint: disable=redefined-outer-name
+    credential_definition_id: str,
     faber_and_alice_connection: FaberAliceConnect,
     alice_member_client: RichAsyncClient,
-    alice_tenant: CreateTenantResponse,
 ) -> CredentialExchange:
     alice_connection_id = faber_and_alice_connection.alice_connection_id
     delete_response = await alice_member_client.delete(
@@ -33,17 +31,13 @@ async def test_credential_issue_on_deleted_connection(
     assert delete_response.status_code == 200
 
     credential = {
-        "protocol_version": "v1",
+        "protocol_version": "v2",
         "connection_id": faber_and_alice_connection.faber_connection_id,
         "indy_credential_detail": {
             "credential_definition_id": credential_definition_id,
             "attributes": {"speed": "10"},
         },
     }
-
-    alice_credentials_listener = SseListener(
-        topic="credentials", wallet_id=alice_tenant.wallet_id
-    )
 
     # create and send credential offer- issuer
     await faber_client.post(
@@ -57,21 +51,11 @@ async def test_credential_issue_on_deleted_connection(
 
     logger.warning(f"{faber_cred_ex_recs=}")
 
-    payload = await alice_credentials_listener.wait_for_event(
-        field="connection_id",
-        field_id=faber_and_alice_connection.alice_connection_id,
-        desired_state="offer-received",
+    assert await check_webhook_state(
+        client=alice_member_client,
+        topic="credentials",
+        state="offer-received",
+        filter_map={
+            "connection_id": faber_and_alice_connection.alice_connection_id,
+        },
     )
-
-    # alice_credential_id = payload["credential_id"]
-
-    # # send credential request - holder
-    # response = await alice_member_client.post(
-    #     f"{CREDENTIALS_BASE_PATH}/{alice_credential_id}/request", json={}
-    # )
-
-    # await alice_credentials_listener.wait_for_event(
-    #     field="credential_id", field_id=alice_credential_id, desired_state="done"
-    # )
-
-    # return response.json()

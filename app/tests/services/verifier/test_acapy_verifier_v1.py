591c19bceff4bd7b313a098f878da7eba0892291
ff137 <ff137@proton.me>
2024-04-15 20:47:25 +0200

    :white_check_mark: tests to assert exception handling

--- app/tests/services/verifier/test_acapy_verifier_v1.py
+++ app/tests/services/verifier/test_acapy_verifier_v1.py
@@ -1,5 +1,5 @@
 import pytest
-from aries_cloudcontroller import AcaPyClient, IndyPresSpec
+from aries_cloudcontroller import AcaPyClient, ApiException, IndyPresSpec
 from mockito import when
 
 from app.exceptions.cloudapi_exception import CloudApiException
@@ -30,12 +30,36 @@ async def test_create_proof_request(mock_agent_controller: AcaPyClient):
         controller=mock_agent_controller,
         create_proof_request=CreateProofRequest(
             indy_proof_request=indy_proof_request,
-            comment=None,
             protocol_version=PresentProofProtocolVersion.v1,
         ),
     )
 
     assert isinstance(created_proof_request, PresentationExchange)
+
+
+@pytest.mark.anyio
+@pytest.mark.parametrize("status_code", [400, 500])
+@pytest.mark.parametrize("error_detail", ["Error msg", "is dynamic"])
+async def test_create_proof_request_exception(
+    mock_agent_controller: AcaPyClient, status_code: int, error_detail: str
+):
+    when(mock_agent_controller.present_proof_v1_0).create_proof_request(...).thenRaise(
+        ApiException(reason=error_detail, status=status_code)
+    )
+
+    with pytest.raises(
+        CloudApiException,
+        match=f"Failed to create presentation request: {error_detail}.",
+    ) as exc:
+        await VerifierV1.create_proof_request(
+            controller=mock_agent_controller,
+            create_proof_request=CreateProofRequest(
+                indy_proof_request=indy_proof_request,
+                protocol_version=PresentProofProtocolVersion.v1,
+            ),
+        )
+
+    assert exc.value.status_code == status_code
 
 
 @pytest.mark.anyio
@@ -69,6 +93,32 @@ async def test_send_proof_request(mock_agent_controller: AcaPyClient):
 
 
 @pytest.mark.anyio
+@pytest.mark.parametrize("status_code", [400, 500])
+@pytest.mark.parametrize("error_detail", ["Error msg", "is dynamic"])
+async def test_send_proof_request_exception(
+    mock_agent_controller: AcaPyClient, status_code: int, error_detail: str
+):
+    when(mock_agent_controller.present_proof_v1_0).send_request_free(...).thenRaise(
+        ApiException(reason=error_detail, status=status_code)
+    )
+
+    with pytest.raises(
+        CloudApiException,
+        match=f"Failed to send presentation request: {error_detail}.",
+    ) as exc:
+        await VerifierV1.send_proof_request(
+            controller=mock_agent_controller,
+            send_proof_request=SendProofRequest(
+                indy_proof_request=indy_proof_request,
+                protocol_version=PresentProofProtocolVersion.v1,
+                connection_id="abc",
+            ),
+        )
+
+    assert exc.value.status_code == status_code
+
+
+@pytest.mark.anyio
 async def test_accept_proof_request(mock_agent_controller: AcaPyClient):
     when(mock_agent_controller.present_proof_v1_0).send_presentation(...).thenReturn(
         to_async(v10_presentation_exchange_records[0])
@@ -90,12 +140,38 @@ async def test_accept_proof_request(mock_agent_controller: AcaPyClient):
 
 
 @pytest.mark.anyio
+@pytest.mark.parametrize("status_code", [400, 500])
+@pytest.mark.parametrize("error_detail", ["Error msg", "is dynamic"])
+async def test_accept_proof_request_exception(
+    mock_agent_controller: AcaPyClient, status_code: int, error_detail: str
+):
+    when(mock_agent_controller.present_proof_v1_0).send_presentation(...).thenRaise(
+        ApiException(reason=error_detail, status=status_code)
+    )
+
+    with pytest.raises(
+        CloudApiException,
+        match=f"Failed to send proof presentation: {error_detail}.",
+    ) as exc:
+        await VerifierV1.accept_proof_request(
+            controller=mock_agent_controller,
+            accept_proof_request=AcceptProofRequest(
+                proof_id="v1-123",
+                indy_presentation_spec=IndyPresSpec(
+                    requested_attributes={},
+                    requested_predicates={},
+                    self_attested_attributes={},
+                ),
+            ),
+        )
+
+    assert exc.value.status_code == status_code
+
+
+@pytest.mark.anyio
 async def test_reject_proof_reject(mock_agent_controller: AcaPyClient):
     when(mock_agent_controller.present_proof_v1_0).delete_record(...).thenReturn(
         to_async({})
-    )
-    when(mock_agent_controller.present_proof_v1_0).report_problem(...).thenReturn(
-        {}  # todo: all of the mocks with to_async removed are unnecessary and have no effect
     )
     when(mock_agent_controller.present_proof_v1_0).get_record(...).thenReturn(
         v10_presentation_exchange_records[0]
@@ -122,6 +198,141 @@ async def test_reject_proof_reject(mock_agent_controller: AcaPyClient):
             controller=mock_agent_controller,
             reject_proof_request="v1-abc",
         )
+
+
+@pytest.mark.anyio
+@pytest.mark.parametrize("status_code", [400, 500])
+@pytest.mark.parametrize("error_detail", ["Error msg", "is dynamic"])
+async def test_reject_proof_reject_exception_report(
+    mock_agent_controller: AcaPyClient, status_code: int, error_detail: str
+):
+    when(mock_agent_controller.present_proof_v1_0).report_problem(...).thenRaise(
+        ApiException(reason=error_detail, status=status_code)
+    )
+
+    with pytest.raises(
+        CloudApiException,
+        match=f"Failed to send problem report: {error_detail}.",
+    ) as exc:
+        await VerifierV1.reject_proof_request(
+            controller=mock_agent_controller,
+            reject_proof_request=RejectProofRequest(
+                proof_id="v1-abc", problem_report="bad"
+            ),
+        )
+
+    assert exc.value.status_code == status_code
+
+
+@pytest.mark.anyio
+@pytest.mark.parametrize("status_code", [400, 500])
+@pytest.mark.parametrize("error_detail", ["Error msg", "is dynamic"])
+async def test_reject_proof_reject_exception_delete(
+    mock_agent_controller: AcaPyClient, status_code: int, error_detail: str
+):
+    when(mock_agent_controller.present_proof_v1_0).report_problem(...).thenReturn(
+        to_async({})
+    )
+    when(mock_agent_controller.present_proof_v1_0).delete_record(...).thenRaise(
+        ApiException(reason=error_detail, status=status_code)
+    )
+
+    with pytest.raises(
+        CloudApiException,
+        match=f"Failed to delete record: {error_detail}.",
+    ) as exc:
+        await VerifierV1.reject_proof_request(
+            controller=mock_agent_controller,
+            reject_proof_request=RejectProofRequest(
+                proof_id="v1-abc", problem_report="bad"
+            ),
+        )
+
+    assert exc.value.status_code == status_code
+
+
+@pytest.mark.anyio
+@pytest.mark.parametrize("status_code", [400, 500])
+@pytest.mark.parametrize("error_detail", ["Error msg", "is dynamic"])
+async def test_get_proof_records_exception(
+    mock_agent_controller: AcaPyClient, status_code: int, error_detail: str
+):
+    when(mock_agent_controller.present_proof_v1_0).get_records(...).thenRaise(
+        ApiException(reason=error_detail, status=status_code)
+    )
+
+    with pytest.raises(
+        CloudApiException,
+        match=f"Failed to get proof records: {error_detail}.",
+    ) as exc:
+        await VerifierV1.get_proof_records(
+            controller=mock_agent_controller,
+        )
+
+    assert exc.value.status_code == status_code
+
+
+@pytest.mark.anyio
+@pytest.mark.parametrize("status_code", [400, 500])
+@pytest.mark.parametrize("error_detail", ["Error msg", "is dynamic"])
+async def test_get_proof_record_exception(
+    mock_agent_controller: AcaPyClient, status_code: int, error_detail: str
+):
+    when(mock_agent_controller.present_proof_v1_0).get_record(...).thenRaise(
+        ApiException(reason=error_detail, status=status_code)
+    )
+    proof_id = "v1-abc"
+    with pytest.raises(
+        CloudApiException,
+        match=f"Failed to get proof record with proof id `{proof_id}`: {error_detail}.",
+    ) as exc:
+        await VerifierV1.get_proof_record(
+            controller=mock_agent_controller, proof_id=proof_id
+        )
+
+    assert exc.value.status_code == status_code
+
+
+@pytest.mark.anyio
+@pytest.mark.parametrize("status_code", [400, 500])
+@pytest.mark.parametrize("error_detail", ["Error msg", "is dynamic"])
+async def test_delete_proof_exception(
+    mock_agent_controller: AcaPyClient, status_code: int, error_detail: str
+):
+    when(mock_agent_controller.present_proof_v1_0).delete_record(...).thenRaise(
+        ApiException(reason=error_detail, status=status_code)
+    )
+    proof_id = "v1-abc"
+    with pytest.raises(
+        CloudApiException,
+        match=f"Failed to delete record with proof id `{proof_id}`: {error_detail}.",
+    ) as exc:
+        await VerifierV1.delete_proof(
+            controller=mock_agent_controller, proof_id=proof_id
+        )
+
+    assert exc.value.status_code == status_code
+
+
+@pytest.mark.anyio
+@pytest.mark.parametrize("status_code", [400, 500])
+@pytest.mark.parametrize("error_detail", ["Error msg", "is dynamic"])
+async def test_get_credentials_by_proof_id_exception(
+    mock_agent_controller: AcaPyClient, status_code: int, error_detail: str
+):
+    when(mock_agent_controller.present_proof_v1_0).get_matching_credentials(
+        ...
+    ).thenRaise(ApiException(reason=error_detail, status=status_code))
+    proof_id = "v1-abc"
+    with pytest.raises(
+        CloudApiException,
+        match=f"Failed to get credentials with proof id `{proof_id}`: {error_detail}.",
+    ) as exc:
+        await VerifierV1.get_credentials_by_proof_id(
+            controller=mock_agent_controller, proof_id=proof_id
+        )
+
+    assert exc.value.status_code == status_code
 
 
 @pytest.mark.anyio
@@ -163,9 +374,13 @@ async def test_accept_proof_request_is_indy_only(mock_agent_controller: AcaPyCli
         await VerifierV1.accept_proof_request(
             controller=mock_agent_controller,
             accept_proof_request=AcceptProofRequest(
+                proof_id="v1-123",
                 type=ProofRequestType.LD_PROOF,
-                protocol_version=PresentProofProtocolVersion.v1,
-                proof_id="abc",
+                indy_presentation_spec=IndyPresSpec(
+                    requested_attributes={},
+                    requested_predicates={},
+                    self_attested_attributes={},
+                ),
             ),
         )
     assert exc.value.status_code == 400

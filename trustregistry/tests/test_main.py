045eeaa5d9cfbf1993b3b7a0afdec61afa928e12
cl0ete <cloete.dupreez@gmail.com>
2024-04-17 19:05:16 +0200

    Add tests for `trustregistry/main.py` (#766)
    * test trustregistry main
    
    * :art:
    
    * use fixture the right way
    
    * disable=redefined-outer-name

---
+++ trustregistry/tests/test_main.py
@@ -0,0 +1,76 @@
+from unittest.mock import MagicMock, Mock, patch
+
+import pytest
+from fastapi import FastAPI
+from sqlalchemy.orm import Session
+
+from trustregistry import db
+from trustregistry.main import app, lifespan, registry, root
+
+
+@pytest.fixture
+def db_session_mock():
+    session = Mock(spec=Session)
+    return session
+
+
+def test_create_app():
+    assert app.title == "Trust Registry"
+
+    routes = [route.path for route in app.routes]
+
+    expected_routes = ["/", "/registry"]
+    for route in expected_routes:
+        assert route in routes
+
+
+@patch("trustregistry.main.inspect")
+@patch("trustregistry.main.engine")
+@pytest.mark.anyio
+async def test_lifespan(mock_engine, mock_inspect):
+    mock_inspector = MagicMock()
+    mock_inspect.return_value = mock_inspector
+    mock_inspector.get_table_names.return_value = ["table1", "table2"]
+    mock_connection = MagicMock()
+    mock_engine.connect.return_value.__enter__.return_value = mock_connection
+
+    async with lifespan(FastAPI()):
+        pass
+
+    mock_engine.dispose.assert_called_once()
+    mock_engine.connect.assert_called_once()
+    mock_inspect.assert_called_once_with(mock_connection)
+    mock_inspector.get_table_names.assert_called_once()
+
+
+@pytest.mark.anyio
+async def test_root(db_session_mock):  # pylint: disable=redefined-outer-name
+    schemas = [
+        db.Schema(id="123", did="did:123", name="schema1", version="1.0"),
+        db.Schema(id="456", did="did:123", name="schema2", version="1.0"),
+    ]
+    actors = [db.Actor(id="1", name="Alice"), db.Actor(id="2", name="Bob")]
+    with patch("trustregistry.main.crud.get_schemas") as mock_get_schemas, patch(
+        "trustregistry.main.crud.get_actors"
+    ) as mock_get_actors:
+        mock_get_schemas.return_value = schemas
+        mock_get_actors.return_value = actors
+
+        response = await root(db_session_mock)
+
+        assert response == {"actors": actors, "schemas": ["123", "456"]}
+
+        mock_get_schemas.assert_called_once_with(db_session_mock)
+        mock_get_actors.assert_called_once_with(db_session_mock)
+
+
+@pytest.mark.anyio
+async def test_registry(db_session_mock):  # pylint: disable=redefined-outer-name
+    with patch("trustregistry.main.root") as mock_root:
+        mock_root.return_value = {"actors": "actors", "schemas": "schemas"}
+
+        response = await registry(db_session_mock)
+
+        assert response == {"actors": "actors", "schemas": "schemas"}
+
+        mock_root.assert_called_once_with(db_session_mock)

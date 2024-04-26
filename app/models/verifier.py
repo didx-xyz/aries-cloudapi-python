93489cec0df62b605f87090361beb35dae53956b
cl0ete <cloete.dupreez@gmail.com>
2024-04-26 14:34:02 +0200

    update function name

--- app/models/verifier.py
+++ app/models/verifier.py
@@ -113,7 +113,7 @@ class RejectProofRequest(ProofId):
         description="Problem report to send with the rejection",
     )
     @field_validator("problem_report", mode="before")
-    def validate_my_field(cls, value):
+    def validate_problem_report(cls, value):
         if value is None:
             raise ValueError("problem_report cannot be None")
         return value

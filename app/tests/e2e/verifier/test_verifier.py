cc628085489771350f63892da431d483a7f619c5
ff137 <ff137@proton.me>
2024-04-29 15:47:33 +0200

    :truck: move oob test to own module

--- app/tests/e2e/verifier/test_verifier.py
+++ app/tests/e2e/verifier/test_verifier.py
@@ -11,12 +11,10 @@ from app.routes.issuer import router as issuer_router
 from app.routes.oob import router as oob_router
 from app.routes.verifier import AcceptProofRequest, RejectProofRequest
 from app.routes.verifier import router as verifier_router
-from app.services.trust_registry.actors import fetch_actor_by_id
 from app.tests.services.verifier.utils import indy_proof_request
 from app.tests.util.ecosystem_connections import AcmeAliceConnect, MeldCoAliceConnect
 from app.tests.util.verifier import send_proof_request
-from app.tests.util.webhooks import check_webhook_state, get_wallet_id_from_async_client
-from app.util.string import base64_to_json
+from app.tests.util.webhooks import check_webhook_state
 from shared import RichAsyncClient
 from shared.models.credential_exchange import CredentialExchange
 from shared.models.presentation_exchange import PresentationExchange
@@ -700,111 +698,3 @@ async def test_saving_of_presentation_exchange_records(
         assert len(acme_pres_ex_records) == 1  # Save record is True, should be 1 record
     else:
         assert len(acme_pres_ex_records) == 0  # default is to remove records
-
-
-@pytest.mark.anyio
-@pytest.mark.parametrize("protocol_version", ["v1", "v2"])
-async def test_accept_proof_request_verifier_no_public_did(
-    credential_definition_id: str,
-    issue_credential_to_alice: CredentialExchange,  # pylint: disable=unused-argument
-    acme_client: RichAsyncClient,
-    alice_member_client: RichAsyncClient,
-    protocol_version: str,
-):
-    # Create connection between holder and verifier
-    # We need to use the multi-use didcomm invitation from the trust registry
-    acme_wallet_id = get_wallet_id_from_async_client(acme_client)
-    verifier_actor = await fetch_actor_by_id(acme_wallet_id)
-
-    assert verifier_actor
-    assert verifier_actor["didcomm_invitation"]
-
-    invitation_json = base64_to_json(
-        verifier_actor["didcomm_invitation"].split("?oob=")[1]
-    )
-    invitation_response = (
-        await alice_member_client.post(
-            OOB_BASE_PATH + "/accept-invitation",
-            json={"invitation": invitation_json},
-        )
-    ).json()
-
-    payload = await check_webhook_state(
-        client=acme_client,
-        topic="connections",
-        state="completed",
-    )
-    holder_verifier_connection_id = invitation_response["connection_id"]
-    verifier_holder_connection_id = payload["connection_id"]
-
-    # Present proof from holder to verifier
-    request_body = {
-        "protocol_version": protocol_version,
-        "connection_id": verifier_holder_connection_id,
-        "indy_proof_request": {
-            "name": "Age Check",
-            "version": "1.0",
-            "requested_attributes": {
-                "name": {
-                    "name": "name",
-                    "restrictions": [{"cred_def_id": credential_definition_id}],
-                }
-            },
-            "requested_predicates": {
-                "age_over_21": {
-                    "name": "age",
-                    "p_type": ">=",
-                    "p_value": 21,
-                    "restrictions": [{"cred_def_id": credential_definition_id}],
-                }
-            },
-        },
-    }
-    send_proof_response = await send_proof_request(acme_client, request_body)
-
-    payload = await check_webhook_state(
-        client=alice_member_client,
-        topic="proofs",
-        state="request-received",
-        filter_map={
-            "connection_id": holder_verifier_connection_id,
-        },
-    )
-
-    verifier_proof_exchange_id = send_proof_response["proof_id"]
-    holder_proof_exchange_id = payload["proof_id"]
-
-    available_credentials = (
-        await alice_member_client.get(
-            f"{VERIFIER_BASE_PATH}/proofs/{holder_proof_exchange_id}/credentials",
-        )
-    ).json()
-
-    cred_id = available_credentials[0]["cred_info"]["referent"]
-
-    await alice_member_client.post(
-        VERIFIER_BASE_PATH + "/accept-request",
-        json={
-            "proof_id": holder_proof_exchange_id,
-            "indy_presentation_spec": {
-                "requested_attributes": {
-                    "name": {
-                        "cred_id": cred_id,
-                        "revealed": True,
-                    }
-                },
-                "requested_predicates": {"age_over_21": {"cred_id": cred_id}},
-                "self_attested_attributes": {},
-            },
-        },
-    )
-
-    event = await check_webhook_state(
-        client=acme_client,
-        topic="proofs",
-        state="done",
-        filter_map={
-            "proof_id": verifier_proof_exchange_id,
-        },
-    )
-    assert event["verified"]

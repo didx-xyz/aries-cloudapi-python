1e798c87c924e6c85675bcc606d04c30e6fe9c46
ff137 <ff137@proton.me>
2024-04-30 16:58:41 +0300

    :art:

--- app/tests/e2e/test_connections.py
+++ app/tests/e2e/test_connections.py
@@ -111,9 +111,9 @@ async def test_get_connections(
     alice_invitation_msg_id = alice_connections["invitation_msg_id"]
     alice_did = alice_connections["my_did"]
 
-    alice_connection_alias = (await alice_member_client.get(f"{BASE_PATH}?alias=AliceBobConnection")).json()[0][
-        "alias"
-    ]
+    alice_connection_alias = (
+        await alice_member_client.get(f"{BASE_PATH}?alias=AliceBobConnection")
+    ).json()[0]["alias"]
     assert alice_connection_alias == "AliceBobConnection"
 
     alice_state = (

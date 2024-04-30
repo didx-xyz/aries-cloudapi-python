0aacc1a17c5efa1504c47c3e803985cd4f0560a0
ff137 <ff137@proton.me>
2024-04-30 11:57:47 +0300

    :fire: delete unused fixtures

--- app/tests/util/ecosystem_connections.py
+++ app/tests/util/ecosystem_connections.py
@@ -1,8 +1,6 @@
 from dataclasses import dataclass
-from typing import Any, Dict, TypedDict
 
 import pytest
-from aries_cloudcontroller import AcaPyClient
 
 from app.models.tenants import CreateTenantResponse
 from app.routes.connections import CreateInvitation
@@ -10,9 +8,7 @@ from app.routes.connections import router as conn_router
 from app.routes.oob import router as oob_router
 from app.services.trust_registry import actors
 from app.services.trust_registry.actors import fetch_actor_by_id
-from app.tests.util.ledger import create_public_did
 from app.tests.util.webhooks import check_webhook_state
-from app.util.acapy_verifier_utils import ed25519_verkey_to_did_key
 from app.util.string import base64_to_json
 from shared import RichAsyncClient
 
@@ -294,123 +290,4 @@ async def meld_co_and_alice_connection(
     return MeldCoAliceConnect(
         alice_connection_id=alice_connection_id,
         meld_co_connection_id=meld_co_connection_id,
-    )
-
-
-@dataclass
-class BobAlicePublicDid:
-    alice_public_did: str
-    bob_public_did: str
-
-
-@pytest.fixture(scope="function")
-async def bob_and_alice_public_did(
-    alice_acapy_client: AcaPyClient,
-    bob_acapy_client: AcaPyClient,
-) -> BobAlicePublicDid:
-    bob_records = await bob_acapy_client.connection.get_connections()
-    alice_records = await alice_acapy_client.connection.get_connections()
-
-    await bob_acapy_client.connection.accept_invitation(
-        conn_id=bob_records.results[-1].connection_id
-    )
-    await alice_acapy_client.connection.accept_invitation(
-        conn_id=alice_records.results[-1].connection_id
-    )
-
-    bob_records = await bob_acapy_client.connection.get_connections()
-    alice_records = await alice_acapy_client.connection.get_connections()
-
-    bob_did = await create_public_did(bob_acapy_client)
-    alice_did = await create_public_did(alice_acapy_client)
-
-    if not bob_did.did or not alice_did.did:
-        raise Exception("Missing public did for alice or bob")
-
-    return BobAlicePublicDid(alice_public_did=alice_did, bob_public_did=bob_did)
-
-
-class InvitationResultDict(TypedDict):
-    invitation: Dict[str, Any]
-    connection_id: str
-
-
-class MultiInvite(TypedDict):
-    multi_use_invitation: InvitationResultDict
-    invitation_key: str
-    did_from_rec_key: str
-
-
-async def bob_multi_use_invitation(
-    bob_member_client: RichAsyncClient,
-) -> MultiInvite:
-    create_invite_json = CreateInvitation(
-        alias=None,
-        multi_use=True,
-        use_public_did=False,
-    ).model_dump()
-    # Create a multi-use invitation
-    invitation = (
-        await bob_member_client.post(
-            f"{CONNECTIONS_BASE_PATH}/create-invitation",
-            json=create_invite_json,
-        )
-    ).json()
-
-    recipient_key = invitation["invitation"]["recipientKeys"][0]
-    bob_multi_invite = MultiInvite(
-        multi_use_invitation=invitation,
-        invitation_key=recipient_key,
-        did_from_rec_key=ed25519_verkey_to_did_key(key=recipient_key),
-    )
-
-    return bob_multi_invite
-
-
-@pytest.fixture(scope="function")
-async def alice_bob_connect_multi(
-    bob_member_client: RichAsyncClient,
-    alice_member_client: RichAsyncClient,
-) -> BobAliceConnect:
-    multi_use_invite = bob_multi_use_invitation(bob_member_client)
-    invitation = multi_use_invite["multi_use_invitation"]["invitation"]
-
-    # accept invitation on alice side
-    invitation_response = (
-        await alice_member_client.post(
-            f"{CONNECTIONS_BASE_PATH}/accept-invitation",
-            json={"invitation": invitation},
-        )
-    ).json()
-
-    assert await check_webhook_state(
-        client=alice_member_client,
-        topic="connections",
-        state="request-sent",
-    )
-
-    bob_connection_id = multi_use_invite["multi_use_invitation"]["connection_id"]
-    alice_connection_id = invitation_response["connection_id"]
-
-    # fetch and validate
-    # both connections should be active - we have waited long enough for events to be exchanged
-    bob_connection_records = (await bob_member_client.get(CONNECTIONS_BASE_PATH)).json()
-
-    bob_connection_id = bob_connection_records[0]["connection_id"]
-
-    assert await check_webhook_state(
-        client=alice_member_client,
-        topic="connections",
-        state="completed",
-        look_back=5,
-    )
-    assert await check_webhook_state(
-        client=bob_member_client,
-        topic="connections",
-        state="completed",
-        look_back=5,
-    )
-
-    return BobAliceConnect(
-        alice_connection_id=alice_connection_id, bob_connection_id=bob_connection_id
     )

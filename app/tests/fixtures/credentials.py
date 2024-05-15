a06f8b64438b282e408f0fff78601c0d64512437
cl0ete <cloete.dupreez@gmail.com>
2024-05-15 15:19:08 +0200

    rename to exchange id

--- app/tests/fixtures/credentials.py
+++ app/tests/fixtures/credentials.py
@@ -217,7 +217,7 @@ async def revoke_alice_creds(
         await faber_client.post(
             f"{CREDENTIALS_BASE_PATH}/revoke",
             json={
-                "credential_exchange_id": cred.credential_id,
+                "credential_exchange_id": cred.credential_exchange_id,
             },
         )
 

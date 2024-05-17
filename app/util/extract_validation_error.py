1d5458d9901ed4f8aadef8fbe3bd6365797c916e
ff137 <ff137@proton.me>
2024-05-17 16:00:30 +0200

    :art: improve error handling

--- app/util/extract_validation_error.py
+++ app/util/extract_validation_error.py
@@ -4,7 +4,7 @@ from pydantic import ValidationError
 def extract_validation_error_msg(e: ValidationError):
     output = ""
     for error in e.errors():
-        loc = error.get(loc, ["Error:"])
+        loc = error.get("loc") or ["Error:"]
         field = loc[0]  # Gets the problematic field, or default string "Error:"
         msg = error["msg"]  # msg is prefaced with: "Value error, <what we want>"
         msg_without_value_error = msg.split(", ", maxsplit=1)[1] if ", " in msg else msg

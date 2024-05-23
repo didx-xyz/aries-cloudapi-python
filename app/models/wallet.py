9b1d96f02ebf1bedcff47efb6a4321d3a0b4e47f
cl0ete <cloete.dupreez@gmail.com>
2024-05-23 16:22:47 +0200

    extend models and add credential_id field

--- app/models/wallet.py
+++ app/models/wallet.py
@@ -1,5 +1,27 @@
+from typing import List, Optional
+
+from aries_cloudcontroller.models.indy_cred_info import IndyCredInfo
+from aries_cloudcontroller.models.vc_record import VCRecord
 from pydantic import BaseModel, Field
 
 
 class SetDidEndpointRequest(BaseModel):
     endpoint: str = Field(...)
+
+
+class ExtendedVCRecord(VCRecord):
+    credential_id: str = Field(..., alias="record_id")
+    record_id: str = Field(..., alias="credential_id")
+
+
+class VCRecordList(BaseModel):
+    results: Optional[List[ExtendedVCRecord]] = None
+
+
+class ExtendedIndyCredInfo(IndyCredInfo):
+    credential_id: str = Field(..., alias="referent")
+    referent: str = Field(..., alias="credential_id")
+
+
+class CredInfoList(BaseModel):
+    results: Optional[List[ExtendedIndyCredInfo]] = None

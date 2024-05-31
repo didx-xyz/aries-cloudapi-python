7369b1dfa54c0a581fb0dc1ea8e66fefc6d942a1
cl0ete <cloete.dupreez@gmail.com>
2024-05-31 14:08:43 +0200

    test setup

--- app/tests/e2e/verifier/test_predicate_proofs.py
+++ app/tests/e2e/verifier/test_predicate_proofs.py
@@ -11,3 +11,16 @@ from shared.models.credential_exchange import CredentialExchange
 from shared.models.presentation_exchange import PresentationExchange
 
 VERIFIER_BASE_PATH = router.prefix
+
+
+@pytest.mark.anyio
+@pytest.mark.parametrize("protocol_version", ["v1", "v2"])
+@pytest.mark.parametrize("predicate", ["<", ">", "<=", ">="])
+async def test_predicate_proofs(
+    protocol_version: str,
+    acme_client: RichAsyncClient,
+    acme_and_alice_connection: AcmeAliceConnect,
+    alice_member_client: RichAsyncClient,
+    issue_credential_to_alice: CredentialExchange,  # pylint: disable=unused-argument
+    predicate: str,
+):

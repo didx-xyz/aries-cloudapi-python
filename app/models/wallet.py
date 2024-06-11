2fbdfc5ea4ab20847d219909a01c2c0987d9cc61
cl0ete <cloete.dupreez@gmail.com>
2024-06-11 10:48:49 +0200

    rename models

--- app/models/wallet.py
+++ app/models/wallet.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from aries_cloudcontroller.models.indy_cred_info import IndyCredInfo
-from aries_cloudcontroller.models.vc_record import VCRecord
+from aries_cloudcontroller.models.indy_cred_info import IndyCredInfo as IndyCredInfoAcaPy
+from aries_cloudcontroller.models.vc_record import VCRecord as VCRecordAcaPy
 from pydantic import BaseModel, Field
 
 
@@ -9,19 +9,19 @@ class SetDidEndpointRequest(BaseModel):
     endpoint: str = Field(...)
 
 
-class ExtendedVCRecord(VCRecord):
+class VCRecord(VCRecordAcaPy):
     credential_id: str = Field(..., alias="record_id")
     record_id: str = Field(..., alias="credential_id")
 
 
 class VCRecordList(BaseModel):
-    results: Optional[List[ExtendedVCRecord]] = None
+    results: Optional[List[VCRecord]] = None
 
 
-class ExtendedIndyCredInfo(IndyCredInfo):
+class IndyCredInfo(IndyCredInfoAcaPy):
     credential_id: str = Field(..., alias="referent")
     referent: str = Field(..., alias="credential_id")
 
 
 class CredInfoList(BaseModel):
-    results: Optional[List[ExtendedIndyCredInfo]] = None
+    results: Optional[List[IndyCredInfo]] = None

403ab6f7fd9cd242806e3700450b24ed2c4bdabf
cl0ete <cloete.dupreez@gmail.com>
2024-06-11 11:54:19 +0200

    add response model

--- app/routes/admin/tenants.py
+++ app/routes/admin/tenants.py
@@ -226,12 +226,12 @@ async def create_tenant(
     return response
 
 
-@router.delete("/{wallet_id}", summary="Delete a Tenant by wallet ID")
+@router.delete("/{wallet_id}", summary="Delete a Tenant by wallet ID", status_code=204)
 async def delete_tenant_by_id(
     wallet_id: str,
     group_id: Optional[str] = group_id_query,
     admin_auth: AcaPyAuthVerified = Depends(acapy_auth_tenant_admin),
-):
+) -> None:
     """
     Delete tenant by ID
     ---
@@ -246,7 +246,7 @@ async def delete_tenant_by_id(
 
     Response body:
     ---
-        None
+        status_code: 204 No Content
     """
     bound_logger = logger.bind(body={"wallet_id": wallet_id})
     bound_logger.info("DELETE request received: Deleting tenant by id")

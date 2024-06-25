8e1c438a60458685f0de308d0939d3ae2257bf23
cl0ete <cloete.dupreez@gmail.com>
2024-06-25 14:51:40 +0200

    add wait for transaction acked util function

---
+++ app/util/transaction_acked.py
@@ -0,0 +1,34 @@
+import asyncio
+
+from aries_cloudcontroller import AcaPyClient
+
+from app.exceptions import CloudApiException
+from app.util.retry_method import coroutine_with_retry_until_value
+from shared.log_config import get_logger
+
+logger = get_logger(__name__)
+
+
+async def wait_for_transaction_ack(aries_controller: AcaPyClient, transaction_id: str):
+    """
+    Wait for the transaction to be acknowledged by the endorser.
+    """
+    bound_logger = logger.bind(transaction_id=transaction_id)
+    bound_logger.debug("Waiting for transaction to be acknowledged by the endorser")
+    try:
+        # Wait for transaction to be acknowledged and written to the ledger
+        await coroutine_with_retry_until_value(
+            coroutine_func=aries_controller.endorse_transaction.get_transaction,
+            args=(transaction_id,),
+            field_name="state",
+            expected_value="transaction_acked",
+            logger=bound_logger,
+            max_attempts=10,
+            retry_delay=2,
+        )
+    except asyncio.TimeoutError as e:
+        raise CloudApiException(
+            "Timeout waiting for endorser to accept the endorsement request.",
+            504,
+        ) from e
+    bound_logger.debug("Transaction has been acknowledged by the endorser")

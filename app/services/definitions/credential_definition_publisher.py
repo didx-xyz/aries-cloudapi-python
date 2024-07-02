ddb14c3d54321071b590d9edc4e26ab8f9ac217d
cl0ete <cloete.dupreez@gmail.com>
2024-07-02 11:04:05 +0200

    not used removed

--- app/services/definitions/credential_definition_publisher.py
+++ app/services/definitions/credential_definition_publisher.py
@@ -15,28 +15,6 @@ class CredentialDefinitionPublisher:
         self._logger = logger
         self._controller = controller
 
-    async def assert_public_did(self):
-        try:
-            self._logger.debug("Asserting client has public DID")
-            public_did = await acapy_wallet.assert_public_did(self._controller)
-        except CloudApiException as e:
-            log_message = f"Asserting public DID failed: {e}"
-
-            if e.status_code == 403:
-                self._logger.info(log_message)
-                client_error_message = (
-                    "Wallet making this request has no public DID. "
-                    "Only issuers with a public DID can make this request."
-                )
-            else:
-                self._logger.error(log_message)
-                client_error_message = (
-                    "Something went wrong while asserting if request is from a valid issuer. "
-                    "Please try again."
-                )
-            raise CloudApiException(client_error_message, e.status_code) from e
-        return public_did
-
     async def check_endorser_connection(self):
         has_connections = await check_endorser_connection(
             aries_controller=self._controller

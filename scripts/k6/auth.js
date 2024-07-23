ea538da8d9ed9d6b4fb561b1c8d2d6b9e5b468fd
wdbasson <wlmbasson@gmail.com>
2024-07-23 15:33:22 +0200

    Function to get governance-bearer token

--- scripts/k6/auth.js
+++ scripts/k6/auth.js
@@ -23,3 +23,27 @@ export function getBearerToken() {
     throw new Error("Failed to obtain bearer token");
   }
 }
+
+export function getGovernanceBearerToken() {
+  const url = `${__ENV.CLOUDAPI_URL}/${__ENV.GOVERNANCE_OAUTH_ENDPOINT}`;
+  const clientId = __ENV.GOVERNANCE_CLIENT_ID;
+  const clientSecret = __ENV.GOVERNANCE_CLIENT_SECRET;
+  const requestBody = `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`;
+
+  let response = http.post(url, requestBody, {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    }
+  });
+
+  if (response.status === 200) {
+    let responseData = JSON.parse(response.body);
+    let bearerToken = responseData.access_token;
+    return bearerToken;
+  } else {
+    console.error("Error:", response.status_text);
+    console.error("Response body:", response.body);
+    console.error("Error description:", response.json().error_description);
+    throw new Error("Failed to obtain bearer token");
+  }
+}

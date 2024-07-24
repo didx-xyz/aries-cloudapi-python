99359bced47c446851cd21e8976a79440b386d2e
Mourits de Beer <31511766+ff137@users.noreply.github.com>
2024-07-24 10:15:28 +0200

    :sparkles: better dependency management with Poetry (#863)
    * :fire:
    
    * :tada: replace requirements with pyproject.toml
    
    * Generated lock files
    
    * :sparkles: script to install all dependencies (for simultaneously testing all submodules)
    
    * :sparkles: script to generate lock files for each submodule
    
    * :sparkles: replace pip install requirements with poetry install
    
    * :arrow_up: use python 3.12.4
    
    * :art:
    
    * :art: set `package-mode = false`
    
    * :construction_worker: run uvicorn correctly
    
    * :construction_worker: configure dependabot for each submodule
    
    * :construction_worker: install poetry dependencies with writeable cache dir
    
    * :construction_worker: use poetry
    
    * :rewind: .isort.cfg is required
    
    * :construction_worker: use poetry to run pylint
    
    * :construction_worker: copy necessary files for dependency install
    
    * :art:
    
    * :tada: configuration file for cloudapi
    
    * :construction_worker: run pytest with poetry
    
    * :bug: Configure Poetry to not create virtual environments

---
+++ scripts/install_all_dependencies.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+POETRY_VERSION="1.8.3"
+
+# Function to check if a command exists
+command_exists() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+# Check if Poetry is installed
+if command_exists poetry; then
+  echo "Poetry is already installed."
+else
+  echo "Poetry is not installed. Installing Poetry..."
+  pip install "poetry==${POETRY_VERSION}"
+  echo "Poetry installed successfully."
+fi
+
+# Configure Poetry to not create virtual environments
+poetry config virtualenvs.create false
+
+# Array of submodules
+submodules=("app" "endorser" "trustregistry" "webhooks")
+
+# Install dependencies for each submodule
+for submodule in "${submodules[@]}"; do
+  echo "Installing dependencies for $submodule..."
+  cd "$submodule"
+  poetry install
+  cd ..
+done
+
+echo "All dependencies installed."

68ce3781119f11b01bba7bc5dcd5d769e5ca4f3b
ff137 <ff137@proton.me>
2024-07-24 16:21:43 +0300

    :arrow_up: Upgrade to ACA-Py 1.0.0rc5

--- shared/constants.py
+++ shared/constants.py
@@ -6,7 +6,7 @@ adminApiKey = "adminApiKey"
 
 # pylint: disable=invalid-name
 
-PROJECT_VERSION = os.getenv("PROJECT_VERSION", "1.0.0rc4")
+PROJECT_VERSION = os.getenv("PROJECT_VERSION", "1.0.0rc5")
 
 # the ACAPY_LABEL field with which the governance agent is initialised
 GOVERNANCE_LABEL = os.getenv("GOVERNANCE_ACAPY_LABEL", "Governance").lower()

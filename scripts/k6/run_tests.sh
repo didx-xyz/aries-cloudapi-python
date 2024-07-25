90153ba8eaacd1a24910b35e2936a46b7b4fe631
wdbasson <wlmbasson@gmail.com>
2024-07-25 14:54:13 +0200

    Interpret exit codes

--- scripts/k6/run_tests.sh
+++ scripts/k6/run_tests.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 source ./env.sh
 # ./k6 run --out output-statsd -e SKIP_DELETE_ISSUERS=true -e SKIP_DELETE_HOLDERS=true debug.js
 xk6 run -e SKIP_DELETE_ISSUERS=true -e SKIP_DELETE_HOLDERS=true create-holders.js
-xk6 run -e SKIP_DELETE_ISSUERS=true -e SKIP_DELETE_HOLDERS=true create-invitation.js
-xk6 run -e SKIP_DELETE_ISSUERS=true -e SKIP_DELETE_HOLDERS=true create-credentials.js
-xk6 run -e SKIP_DELETE_ISSUERS=true -e SKIP_DELETE_HOLDERS=true create-proof.js
-xk6 run -e SKIP_DELETE_ISSUERS=false -e SKIP_DELETE_HOLDERS=false delete-holders.js
+# xk6 run -e SKIP_DELETE_ISSUERS=true -e SKIP_DELETE_HOLDERS=true create-invitation.js
+# xk6 run -e SKIP_DELETE_ISSUERS=true -e SKIP_DELETE_HOLDERS=true create-credentials.js
+# xk6 run -e SKIP_DELETE_ISSUERS=true -e SKIP_DELETE_HOLDERS=true create-proof.js
+# xk6 run -e SKIP_DELETE_ISSUERS=false -e SKIP_DELETE_HOLDERS=false delete-holders.js

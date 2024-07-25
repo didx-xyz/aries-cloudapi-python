068b55208ede094a865ac4155b83c44bb9d9b023
wdbasson <wlmbasson@gmail.com>
2024-07-25 14:15:16 +0200

    simulate failed check

--- scripts/k6/env.sh
+++ scripts/k6/env.sh
@@ -6,9 +6,9 @@ fi
 
 export K6_STATSD_ENABLE_TAGS=true
 export SKIP_DELETE_ISSUERS=true
-export VUS=5
+export VUS=1
 export ITERATIONS=5
 export ISSUER_PREFIX=k6_issuer_dev2
-export HOLDER_PREFIX=k6_holder_dev
+export HOLDER_PREFIX=k6_holder_dev_a
 export SCHEMA_NAME="proof_of_person"
 export SCHEMA_VERSION="0.1.0"

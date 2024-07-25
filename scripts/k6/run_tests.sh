5e0a1e7aaab3831b4b2fba7dc9d5a2c379105e3d
wdbasson <wlmbasson@gmail.com>
2024-07-25 13:37:41 +0200

    Set entrypoint to /bin/sh

--- scripts/k6/run_tests.sh
+++ scripts/k6/run_tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source ./env.sh
 # ./k6 run --out output-statsd -e SKIP_DELETE_ISSUERS=true -e SKIP_DELETE_HOLDERS=true debug.js
-./k6 run -e SKIP_DELETE_ISSUERS=true -e SKIP_DELETE_HOLDERS=true create-holders.js
-./k6 run -e SKIP_DELETE_ISSUERS=true -e SKIP_DELETE_HOLDERS=true create-invitation.js
-./k6 run -e SKIP_DELETE_ISSUERS=true -e SKIP_DELETE_HOLDERS=true create-credentials.js
-./k6 run -e SKIP_DELETE_ISSUERS=true -e SKIP_DELETE_HOLDERS=true create-proof.js
-./k6 run -e SKIP_DELETE_ISSUERS=false -e SKIP_DELETE_HOLDERS=false delete-holders.js
+xk6 run -e SKIP_DELETE_ISSUERS=true -e SKIP_DELETE_HOLDERS=true create-holders.js
+xk6 run -e SKIP_DELETE_ISSUERS=true -e SKIP_DELETE_HOLDERS=true create-invitation.js
+xk6 run -e SKIP_DELETE_ISSUERS=true -e SKIP_DELETE_HOLDERS=true create-credentials.js
+xk6 run -e SKIP_DELETE_ISSUERS=true -e SKIP_DELETE_HOLDERS=true create-proof.js
+xk6 run -e SKIP_DELETE_ISSUERS=false -e SKIP_DELETE_HOLDERS=false delete-holders.js

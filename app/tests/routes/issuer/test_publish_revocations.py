84b8aac084e1bb43ad0bd72d57dd89fe1bc6cc25
cl0ete <cloete.dupreez@gmail.com>
2024-07-31 10:54:15 +0200

    move dummy txn record to utils

--- app/tests/routes/issuer/test_publish_revocations.py
+++ app/tests/routes/issuer/test_publish_revocations.py
@@ -7,74 +7,15 @@ from aries_cloudcontroller import TxnOrPublishRevocationsResult
 from app.exceptions.cloudapi_exception import CloudApiException
 from app.models.issuer import PublishRevocationsRequest
 from app.routes.issuer import publish_revocations
+from app.tests.util.models.dummy_txn_record_publish import txn_record
 
 
 @pytest.mark.anyio
 async def test_publish_revocations_success():
     mock_aries_controller = AsyncMock()
     mock_publish_revocations = AsyncMock(
-        return_value=TxnOrPublishRevocationsResult(
-            txn={  # todo: reduce size or move out
-                "state": "request_sent",
-                "created_at": "2024-07-16T07:09:37.807261Z",
-                "updated_at": "2024-07-16T07:09:37.812035Z",
-                "trace": False,
-                "transaction_id": "667f76bf-f950-4dd6-a2f2-b84a3b39b5e1",
-                "_type": "https://didcomm.org/sign-attachment/1.0/signature-request",
-                "signature_request": [
-                    {
-                        "context": "did:sov",
-                        "method": "add-signature",
-                        "signature_type": "default",
-                        "signer_goal_code": "aries.transaction.endorse",
-                        "author_goal_code": "aries.transaction.ledger.write",
-                    }
-                ],
-                "signature_response": [],
-                "timing": {"expires_time": None},
-                "formats": [
-                    {
-                        "attach_id": "dbeb9942-fe14-4960-a242-cdc469495ee8",
-                        "format": "dif/endorse-transaction/request@v1.0",
-                    }
-                ],
-                "messages_attach": [
-                    {
-                        "@id": "dbeb9942-fe14-4960-a242-cdc469495ee8",
-                        "mime-type": "application/json",
-                        "data": {
-                            "json": {
-                                "endorser": "CYJnoJA7fDaz6hWoDP4nj7",
-                                "identifier": "WYbzsbG3Nzx9ME3AC9poTD",
-                                "operation": {
-                                    "revocDefType": "CL_ACCUM",
-                                    "revocRegDefId": "WYbzsbG3Nzx9ME3AC9poTD:4:WYbzsbG3Nzx9ME3AC9poTD:3:CL:8:Epic:CL_ACCUM:7f7d0e58-a868-41b2-ae7d-1c69a9309c9c",
-                                    "type": "114",
-                                    "value": {
-                                        "accum": "...",
-                                        "prevAccum": "...",
-                                        "revoked": [7],
-                                    },
-                                },
-                                "protocolVersion": 2,
-                                "reqId": 1721113777802885400,
-                                "signature": "3KtmzZt2WnQTWXqipPBmLJQMjLYmy7ve83QGD9VdRYeg3TMxzbxerDRnDsASKyqXjkZMYbzemwPgz7QgxUSfAU3f",
-                                "taaAcceptance": {
-                                    "mechanism": "service_agreement",
-                                    "taaDigest": "0be4d87dec17a7901cb8ba8bb4239ee34d4f6e08906f3dad81d1d052dccc078f",
-                                    "time": 1721088000,
-                                },
-                            }
-                        },
-                    }
-                ],
-                "meta_data": {"context": {}, "processing": {}},
-                "connection_id": "64753329-ec71-4b19-b2e0-ee6c6e5a5cfd",
-                "endorser_write_txn": False,
-            }
-        )
+        return_value=TxnOrPublishRevocationsResult(txn=txn_record)
     )
-    # mock_publish_revocations.return_value.txn.transaction_id = "transaction_id"
 
     mock_get_transaction = AsyncMock()
 

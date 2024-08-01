44e24f085914f0b570c57685a805beaae2115fe7
cl0ete <cloete.dupreez@gmail.com>
2024-08-01 13:21:50 +0200

    import sort

--- app/tests/e2e/verifier/test_self_attested.py
+++ app/tests/e2e/verifier/test_self_attested.py
@@ -1,13 +1,13 @@
 import pytest
 from aries_cloudcontroller import IndyPresSpec
+from shared import RichAsyncClient
+from shared.models.credential_exchange import CredentialExchange
+from shared.models.presentation_exchange import PresentationExchange
 
 from app.routes.verifier import AcceptProofRequest, router
 from app.tests.util.connections import AcmeAliceConnect
 from app.tests.util.verifier import send_proof_request
 from app.tests.util.webhooks import check_webhook_state
-from shared import RichAsyncClient
-from shared.models.credential_exchange import CredentialExchange
-from shared.models.presentation_exchange import PresentationExchange
 
 VERIFIER_BASE_PATH = router.prefix
 

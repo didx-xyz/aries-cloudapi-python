0de58b6a53103b9fcf1f47e4cc948e018c3d3793
wdbasson <wlmbasson@gmail.com>
2024-08-21 08:27:47 +0200

    Add revoke credentials scenario

---
+++ scripts/k6/scenarios/revoke-credentials.js
@@ -0,0 +1,203 @@
+/* global __ENV, __ITER, __VU */
+/* eslint-disable no-undefined, no-console, camelcase */
+
+import { check, sleep } from "k6";
+import { SharedArray } from "k6/data";
+import { Counter, Trend } from "k6/metrics";
+import { getBearerToken } from "../libs/auth.js";
+import {
+  createCredentialDefinition,
+  deleteTenant,
+  getCredentialDefinitionId,
+  getWalletIdByWalletName,
+  revokeCredential,
+  checkRevoked,
+} from "../libs/functions.js";
+import { createIssuerIfNotExists } from "../libs/issuerUtils.js";
+import { createSchemaIfNotExists } from "../libs/schemaUtils.js";
+
+const vus = Number.parseInt(__ENV.VUS, 10);
+const iterations = Number.parseInt(__ENV.ITERATIONS, 10);
+const issuerPrefix = __ENV.ISSUER_PREFIX;
+
+export const options = {
+  scenarios: {
+    default: {
+      executor: "per-vu-iterations",
+      vus,
+      iterations,
+      maxDuration: "24h",
+    },
+  },
+  setupTimeout: "180s", // Increase the setup timeout to 120 seconds
+  teardownTimeout: "180s", // Increase the teardown timeout to 120 seconds
+  maxRedirects: 4,
+  thresholds: {
+    // https://community.grafana.com/t/ignore-http-calls-made-in-setup-or-teardown-in-results/97260/2
+    "http_req_duration{scenario:default}": ["max>=0"],
+    "http_reqs{scenario:default}": ["count >= 0"],
+    "iteration_duration{scenario:default}": ["max>=0"],
+    checks: ["rate==1"],
+  },
+  tags: {
+    test_run_id: "phased-issuance",
+    test_phase: "revoke-credentials",
+  },
+};
+
+const inputFilepath = "../output/create-credentials.json";
+const data = open(inputFilepath, "r");
+
+const testFunctionReqs = new Counter("test_function_reqs");
+
+const numIssuers = 1;
+
+export function setup() {
+  const issuers = [];
+  const ids = data.trim().split("\n").map(JSON.parse);
+  const bearerToken = getBearerToken();
+  for (let i = 0; i < numIssuers; i++) {
+    const walletName = `${issuerPrefix}_${i}`;
+    const credDefTag = walletName;
+
+    const issuerData = createIssuerIfNotExists(bearerToken, walletName);
+    check(issuerData, {
+      "Issuer data retrieved successfully": (data) => data !== null && data !== undefined,
+    });
+    if (!issuerData) {
+      console.error(`Failed to create or retrieve issuer for ${walletName}`);
+      continue;
+    }
+    const { issuerWalletId, issuerAccessToken } = issuerData;
+
+    const credentialDefinitionId = getCredentialDefinitionId(bearerToken, issuerAccessToken, credDefTag);
+    if (credentialDefinitionId) {
+      console.log(`Credential definition already exists for issuer ${walletName} - Skipping creation`);
+      issuers.push({
+        walletId: issuerWalletId,
+        accessToken: issuerAccessToken,
+        credentialDefinitionId,
+      });
+      continue;
+    }
+    console.warn(`Failed to get credential definition ID for issuer ${walletName}`);
+    // console.error(`Response body: ${credentialDefinitionId.body}`);
+
+    const schemaId = createSchemaIfNotExists(governanceBearerToken, schemaName, schemaVersion);
+    check(schemaId, {
+      "Schema ID is not null": (id) => id !== null && id !== undefined,
+    });
+
+    const createCredentialDefinitionResponse = createCredentialDefinition(
+      bearerToken,
+      issuerAccessToken,
+      credDefTag,
+      schemaId,
+    );
+    check(createCredentialDefinitionResponse, {
+      "Credential definition created successfully": (r) => r.status === 200,
+    });
+
+    if (createCredentialDefinitionResponse.status === 200) {
+      const { id: credentialDefinitionId } = JSON.parse(createCredentialDefinitionResponse.body);
+      console.log(`Credential definition created successfully for issuer ${walletName}`);
+      issuers.push({
+        walletId: issuerWalletId,
+        accessToken: issuerAccessToken,
+        credentialDefinitionId,
+      });
+    } else {
+      console.error(`Failed to create credential definition for issuer ${walletName}`);
+    }
+  }
+  return { bearerToken, ids, issuers }; // eslint-disable-line no-eval
+}
+
+const iterationsPerVU = options.scenarios.default.iterations;
+// Helper function to calculate the wallet index based on VU and iteration
+function getWalletIndex(vu, iter) {
+  const walletIndex = (vu - 1) * iterationsPerVU + (iter - 1);
+  return walletIndex;
+}
+
+export default function (data) {
+  const issuers = data.issuers;
+  const ids = data.ids;
+  const walletIndex = getWalletIndex(__VU, __ITER + 1); // __ITER starts from 0, adding 1 to align with the logic
+  const id = ids[walletIndex];
+
+  const issuerIndex = __ITER % numIssuers;
+  const issuer = issuers[issuerIndex];
+
+  const revokeCredentialResponse = revokeCredential(issuer.accessToken, id.credential_exchange_id);
+  check(revokeCredentialResponse, {
+    "Credential revoked sucessfully": (r) => {
+      if (r.status !== 200) {
+        throw new Error(`Unexpected response while revoking credential: ${r.response}`);
+      }
+      return true;
+    },
+  });
+
+  const checkRevokedCredentialResponse = checkRevoked(issuer.accessToken, id.credential_exchange_id);
+  check(checkRevokedCredentialResponse, {
+    "Credential state is revoked": (r) => {
+      if (r.status !== 200) {
+        throw new Error(`Unexpected response while checking if credential is revoked: ${r.status}`);
+      }
+      const responseBody = JSON.parse(r.body);
+      if (responseBody.state !== "revoked") {
+        throw new Error(`Credential state is not revoked. Current state: ${responseBody.state}`);
+      }
+      return true;
+    },
+  });
+
+
+  testFunctionReqs.add(1);
+}
+
+export function teardown(data) {
+  const bearerToken = data.bearerToken;
+  const issuers = data.issuers;
+  const wallets = data.holders;
+
+  // console.log(__ENV.SKIP_DELETE_ISSUERS)
+
+  if (__ENV.SKIP_DELETE_ISSUERS !== "true") {
+    for (const issuer of issuers) {
+      const deleteIssuerResponse = deleteTenant(bearerToken, issuer.walletId);
+      check(deleteIssuerResponse, {
+        "Delete Issuer Tenant Response status code is 200": (r) => {
+          if (r.status !== 200) {
+            console.error(`Unexpected response status while deleting issuer tenant ${issuer.walletId}: ${r.status}`);
+            return false;
+          }
+          console.log(`Deleted issuer tenant ${issuer.walletId} successfully.`);
+          return true;
+        },
+      });
+    }
+  } else {
+    console.log("Skipping deletion of issuer tenants.");
+  }
+  // // Delete holder tenants
+  if (__ENV.SKIP_DELETE_HOLDERS !== "true") {
+    for (const wallet of wallets) {
+      const walletId = getWalletIdByWalletName(bearerToken, wallet.wallet_name);
+      const deleteHolderResponse = deleteTenant(bearerToken, walletId);
+      check(deleteHolderResponse, {
+        "Delete Holder Tenant Response status code is 200": (r) => {
+          if (r.status !== 200) {
+            console.error(`Unexpected response status while deleting holder tenant ${walletId}: ${r.status}`);
+            return false;
+          }
+          console.log(`Deleted holder tenant ${walletId} successfully.`);
+          return true;
+        },
+      });
+    }
+  } else {
+    console.log("Skipping deletion of holder tenants.");
+  }
+}

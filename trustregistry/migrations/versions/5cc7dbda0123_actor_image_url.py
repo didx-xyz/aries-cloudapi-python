b51370aad6be1daec5f4a20c2f9d84a49deb2202
cl0ete <cloete.dupreez@gmail.com>
2024-08-21 16:10:45 +0200

    :art: import sort

--- trustregistry/migrations/versions/5cc7dbda0123_actor_image_url.py
+++ trustregistry/migrations/versions/5cc7dbda0123_actor_image_url.py
@@ -8,9 +8,8 @@ Create Date: 2024-08-21 15:52:01.005982
 
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "5cc7dbda0123"

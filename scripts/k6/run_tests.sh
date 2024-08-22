d829d9a39b9b60104ff2a3534ed9d13e229e7afb
wdbasson <wlmbasson@gmail.com>
2024-08-22 15:59:48 +0200

    Add revocation to run_tests.sh

--- scripts/k6/run_tests.sh
+++ scripts/k6/run_tests.sh
@@ -3,26 +3,43 @@
 source ./env.sh
 
 run_test() {
-    xk6 run -e SKIP_DELETE_ISSUERS="$1" -e SKIP_DELETE_HOLDERS="$2" "$3"
+    xk6 run "$1"
     local exit_code=$?
     if [ $exit_code -ne 0 ]; then
         echo "Test $3 failed with exit code $exit_code"
         echo "Deleting Holders"
-        xk6 run -e SKIP_DELETE_ISSUERS=true -e SKIP_DELETE_HOLDERS=false ./scenarios/delete-holders.js
+        xk6 run ./scenarios/delete-holders.js
+        if [ $MULTI_ISSUERS = false ]; then
+          export VUS=1 # delete single issuer
+          export ITERATIONS=1
+        fi
+        xk6 run ./scenarios/delete-issuers.js
         echo "Exiting with exit code $exit_code ..."
         exit $exit_code
     fi
 }
 
-run_test true true ./scenarios/create-holders.js
-run_test true true ./scenarios/create-invitation.js
-run_test true true ./scenarios/create-credentials.js
-run_test true true ./scenarios/create-proof.js
-run_test false true ./scenarios/revoke-credentials.js
-run_test true false ./scenarios/delete-holders.js
+# Single issuer, multiple holder tests
+export MULTI_ISSUERS=false
+run_test ./scenarios/create-holders.js
+run_test ./scenarios/create-invitation.js
+run_test ./scenarios/create-credentials.js
+run_test ./scenarios/create-proof.js
+export ITERATIONS=$((ITERATIONS * VUS)) # revoke sequentially
+export VUS=1
+run_test ./scenarios/revoke-credentials.js
+source ./env.sh # concurrent
+export IS_REVOKED=true
+run_test ./scenarios/create-proof.js
 
-run_test false true ./scenarios/create-issuers.js
+run_test ./scenarios/delete-holders.js
+export VUS=1 # delete single issuer - TODO: improve this
+export ITERATIONS=1
+run_test ./scenarios/delete-issuers.js
 
-echo "All tests completed successfully"
-
-# TODO: Better logic to handle deleting issuers and holders
+# Multiple issuers tests
+source ./env.sh # concurrent
+export MULTI_ISSUERS=true
+run_test ./scenarios/create-issuers.js
+run_test ./scenarios/create-creddef.js
+run_test ./scenarios/delete-issuers.js

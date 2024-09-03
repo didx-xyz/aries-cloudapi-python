791970356bb3c5bbcfadbdd925776dc4e015d883
cl0ete <cloete.dupreez@gmail.com>
2024-09-03 15:56:14 +0200

    init nats client and yield

--- waypoint/services/nats_service.py
+++ waypoint/services/nats_service.py
@@ -22,3 +22,26 @@ from shared.models.webhook_events import CloudApiWebhookEventGeneric
 from waypoint.util.event_generator_wrapper import EventGeneratorWrapper
 
 logger = get_logger(__name__)
+
+
+async def init_nats_client() -> AsyncGenerator[JetStreamContext, Any]:
+    """
+    Initialize a connection to the NATS server.
+    """
+    logger.info("Connecting to NATS server...")
+    try:
+        nats_client: NATS = await nats.connect(
+            servers=[NATS_SERVER], user_credentials=NATS_CREDS_FILE
+        )
+    except Exception as e:
+        logger.error(f"Error connecting to NATS server: {e}")
+        raise e
+    logger.debug("Connected to NATS server")
+
+    jetstream: JetStreamContext = nats_client.jetstream()
+    logger.debug("Yielding JetStream context...")
+    yield jetstream
+
+    logger.info("Closing NATS connection...")
+    nats_client.close()
+    logger.debug("NATS connection closed")

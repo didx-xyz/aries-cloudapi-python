fb6610861607807f1c8ece500c51681542b45569
cl0ete <cloete.dupreez@gmail.com>
2024-09-03 15:59:50 +0200

    event generator wrapper for async context

---
+++ waypoint/util/event_generator_wrapper.py
@@ -0,0 +1,22 @@
+from typing import Any, AsyncGenerator, Optional
+
+from shared.models.webhook_events import CloudApiWebhookEventGeneric
+
+
+class EventGeneratorWrapper:
+    def __init__(self, generator: AsyncGenerator[CloudApiWebhookEventGeneric, Any]):
+        self.generator = generator
+
+    async def __aenter__(self):
+        return self.generator
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        """
+        Exits the asynchronous context, closing the generator and cancelling the populate task.
+
+        Args:
+            exc_type: The exception type, if any, that caused the context to be exited.
+            exc_val: The exception value, if any.
+            exc_tb: The traceback, if an exception occurred.
+        """
+        await self.generator.aclose()

2a9f6eb82a137fe2a8ed29029ea8d501bab6ae1b
cl0ete <cloete.dupreez@gmail.com>
2024-09-06 12:57:50 +0200

    update imports

--- waypoint/routes/sse.py
+++ waypoint/routes/sse.py
@@ -1,14 +1,12 @@
 import asyncio
-from logging import Logger
-from typing import Any, AsyncGenerator, Optional
+from typing import AsyncGenerator, Optional
 
 from dependency_injector.wiring import Provide, inject
-from fastapi import BackgroundTasks, Depends, HTTPException, Query, Request
+from fastapi import BackgroundTasks, Depends, Query, Request
 from sse_starlette.sse import EventSourceResponse
 
 from shared import DISCONNECT_CHECK_PERIOD, SSE_TIMEOUT, APIRouter
 from shared.log_config import get_logger
-from shared.models.webhook_events import CloudApiWebhookEventGeneric
 from waypoint.services.dependency_injection.container import Container
 from waypoint.services.nats_service import NatsEventsProcessor
 from waypoint.util.event_generator_wrapper import EventGeneratorWrapper

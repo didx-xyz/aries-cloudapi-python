82164ed8fd34ae83a1bfd0fb358c71d94c152a11
cl0ete <cloete.dupreez@gmail.com>
2024-09-25 10:58:52 +0200

    remove nats init use one in shared

--- waypoint/services/nats_service.py
+++ waypoint/services/nats_service.py
@@ -1,7 +1,6 @@
 import asyncio
 import time
 from contextlib import asynccontextmanager
-from typing import Any, AsyncGenerator
 
 import nats
 import orjson
@@ -10,40 +9,11 @@ from nats.aio.errors import ErrConnectionClosed, ErrNoServers, ErrTimeout
 from nats.errors import BadSubscriptionError, Error, TimeoutError
 from nats.js.client import JetStreamContext
 
-from shared.constants import NATS_CREDS_FILE, NATS_SERVER, NATS_STREAM, NATS_SUBJECT
+from shared.constants import NATS_STREAM, NATS_SUBJECT
 from shared.log_config import get_logger
 from shared.models.webhook_events import CloudApiWebhookEventGeneric
 
 logger = get_logger(__name__)
-
-
-async def init_nats_client() -> AsyncGenerator[JetStreamContext, Any]:
-    """
-    Initialize a connection to the NATS server.
-    """
-    logger.debug("Connecting to NATS server...")
-    try:
-        connect_kwargs = {
-            "servers": [NATS_SERVER],
-        }
-        if NATS_CREDS_FILE:
-            connect_kwargs["user_credentials"] = NATS_CREDS_FILE
-        else:
-            logger.warning("No NATS credentials file found, assuming local development")
-        nats_client: NATS = await nats.connect(**connect_kwargs)
-
-    except (ErrConnectionClosed, ErrTimeout, ErrNoServers) as e:
-        logger.error("Error connecting to NATS server: {}", e)
-        raise e
-    logger.debug("Connected to NATS server")
-
-    jetstream: JetStreamContext = nats_client.jetstream()
-    logger.debug("Yielding JetStream context...")
-    yield jetstream
-
-    logger.debug("Closing NATS connection...")
-    await nats_client.close()
-    logger.debug("NATS connection closed")
 
 
 class NatsEventsProcessor:

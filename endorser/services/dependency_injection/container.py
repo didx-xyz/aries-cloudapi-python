b0e24aa77b2d43e8f404677a393e46d5286b66de
Robbie Blaine <4052340+rblaine95@users.noreply.github.com>
2024-10-09 15:29:43 +0200

    :fire: :coffin: Remove Redis and Webhooks Web from Tilt (#1099)
    * :fire: :coffin: Remove Redis and Webhooks Web from Tilt
    
    * :art: edit comments and remove unused imports
    
    * :construction_worker: Remove Webhooks and Redis Logs
    
    * Remove Webhooks and Redis Logs
    * Skip Webhooks tests
    
    * :arrow_up: Update Tilt/Local Dev Resources
    
    * `bitnami/nats` `8.4.2` -> `8.4.4
    * `bitnami/postgresql-ha` `14.2.28` -> `14.2.33`
    * `runix/pgadmin4` `1.29.0` -> `1.31.0`
    
    ---------
    
    Co-authored-by: ff137 <ff137@proton.me>

--- endorser/services/dependency_injection/container.py
+++ endorser/services/dependency_injection/container.py
@@ -1,8 +1,4 @@
-import os
-from typing import List
-
 from dependency_injector import containers, providers
-from redis.cluster import ClusterNode
 
 from endorser.services.endorsement_processor import EndorsementProcessor
 from shared.services.nats_jetstream import init_nats_client
@@ -13,12 +9,12 @@ class Container(containers.DeclarativeContainer):
     Dependency injection container for the Endorser service.
 
     This container is responsible for creating and managing the lifecycle of
-    the Redis and EndorsementProcessor services
+    the NATS Jetstream and EndorsementProcessor services
     """
 
     jetstream = providers.Resource(init_nats_client)
 
-    # Singleton provider for the ACA-Py Redis events processor
+    # Singleton provider for the ACA-Py Nats events processor
     endorsement_processor = providers.Singleton(
         EndorsementProcessor,
         jetstream=jetstream,

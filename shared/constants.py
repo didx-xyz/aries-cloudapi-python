0cc97f00d0dc883a5b7fc563d77574de9652f8bd
ff137 <ff137@proton.me>
2024-10-10 00:12:16 +0300

    :art: remove billing service env vars

--- shared/constants.py
+++ shared/constants.py
@@ -62,10 +62,6 @@ MAX_NUM_RETRIES = int(os.getenv("MAX_NUM_RETRIES", "3"))
 REGISTRY_CREATION_TIMEOUT = int(os.getenv("REGISTRY_CREATION_TIMEOUT", "60"))
 REGISTRY_SIZE = int(os.getenv("REGISTRY_SIZE", "32767"))
 
-# Billing Service
-LAGO_URL = os.getenv("LAGO_URL", "")
-LAGO_API_KEY = os.getenv("LAGO_API_KEY", "")
-
 # NATS
 NATS_SERVER = os.getenv("NATS_SERVER", "nats://nats:4222")
 NATS_SUBJECT = os.getenv("NATS_SUBJECT", "cloudapi.aries.events")

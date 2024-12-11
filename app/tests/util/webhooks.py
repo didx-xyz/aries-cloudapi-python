6567a2e081e4a57c7bcd204aad57a4f096ce40df
ff137 <ff137@proton.me>
2024-12-11 17:50:13 +0200

    :wrench: Increase default max duration for awaiting webhooks

--- app/tests/util/webhooks.py
+++ app/tests/util/webhooks.py
@@ -28,7 +28,7 @@ async def check_webhook_state(
     topic: CloudApiTopics,
     state: str,
     filter_map: Optional[Dict[str, str]] = None,
-    max_duration: int = 30,
+    max_duration: int = 45,
     max_tries: int = 2,
     delay: float = 0.5,
 ) -> Dict[str, Any]:

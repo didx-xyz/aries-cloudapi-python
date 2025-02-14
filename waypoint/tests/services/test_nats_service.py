417062378b30661e79ad7874f9f6ee4365abd4f8
cl0ete <cloete.dupreez@gmail.com>
2025-02-14 12:10:38 +0200

    :art:

--- waypoint/tests/services/test_nats_service.py
+++ waypoint/tests/services/test_nats_service.py
@@ -31,7 +31,7 @@ async def mock_consumer_info():
     consumer_info = ConsumerInfo(
         name="consumer_name", stream_name="stream_name", config="SomeConfig"
     )
-    mock_consumer_info = AsyncMock( # pylint: disable=redefined-outer-name
+    mock_consumer_info = AsyncMock(  # pylint: disable=redefined-outer-name
         return_value=consumer_info
     )
     return mock_consumer_info
